efb2e2ec19962e29d0af6455d3fc68154e0f2eeb
Mike Rapp <jordy.rascal@protonmail.com>
2021-06-16 00:44:44 -0500

    Added tests for NumberSignSymbolDto.CopyIn().

--- strmech/zzzt_numsignsymboldto_0001000_test.go
+++ strmech/zzzt_numsignsymboldto_0001000_test.go
@@ -1,1 +1,309 @@
 package strmech
+
+import (
+	ePref "github.com/MikeAustin71/errpref"
+	"testing"
+)
+
+func TestNumberSignSymbolDto_CopyIn_000100(t *testing.T) {
+
+	ePrefix := "TestNumberSignSymbolDto_CopyIn_000100()"
+
+	nSignSymOne,
+		err := NumberSignSymbolDto{}.New(
+		"+",
+		"",
+		false,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v", err.Error())
+		return
+	}
+
+	nSignSymTwo := NumberSignSymbolDto{}
+
+	err = nSignSymTwo.CopyIn(
+		&nSignSymOne,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v\n", err.Error())
+		return
+	}
+
+	if !nSignSymTwo.Equal(&nSignSymOne) {
+		t.Errorf("%v - Error:\n"+
+			"Expected nSignSymTwo == nSignSymOne .\n"+
+			"HOWEVER, THEY ARE NOT EQUAL!\n",
+			ePrefix)
+
+		return
+	}
+
+}
+
+func TestNumberSignSymbolDto_CopyIn_000200(t *testing.T) {
+
+	ePrefix := "TestNumberSignSymbolDto_CopyIn_000200()"
+
+	nSignSymOne,
+		err := NumberSignSymbolDto{}.New(
+		"-",
+		"",
+		true,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v", err.Error())
+		return
+	}
+
+	nSignSymTwo := NumberSignSymbolDto{}
+
+	err = nSignSymTwo.CopyIn(
+		&nSignSymOne,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v\n", err.Error())
+		return
+	}
+
+	if !nSignSymTwo.Equal(&nSignSymOne) {
+		t.Errorf("%v - Error:\n"+
+			"Expected nSignSymTwo == nSignSymOne .\n"+
+			"HOWEVER, THEY ARE NOT EQUAL!\n",
+			ePrefix)
+
+		return
+	}
+
+}
+
+func TestNumberSignSymbolDto_CopyIn_000300(t *testing.T) {
+
+	ePrefix := "TestNumberSignSymbolDto_CopyIn_000300()"
+
+	nSignSymOne,
+		err := NumberSignSymbolDto{}.New(
+		"",
+		"+",
+		false,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v", err.Error())
+		return
+	}
+
+	nSignSymTwo := NumberSignSymbolDto{}
+
+	err = nSignSymTwo.CopyIn(
+		&nSignSymOne,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v\n", err.Error())
+		return
+	}
+
+	if !nSignSymTwo.Equal(&nSignSymOne) {
+		t.Errorf("%v - Error:\n"+
+			"Expected nSignSymTwo == nSignSymOne .\n"+
+			"HOWEVER, THEY ARE NOT EQUAL!\n",
+			ePrefix)
+
+		return
+	}
+
+}
+
+func TestNumberSignSymbolDto_CopyIn_000400(t *testing.T) {
+
+	ePrefix := "TestNumberSignSymbolDto_CopyIn_000400()"
+
+	nSignSymOne,
+		err := NumberSignSymbolDto{}.New(
+		"",
+		"-",
+		true,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v", err.Error())
+		return
+	}
+
+	nSignSymTwo := NumberSignSymbolDto{}
+
+	err = nSignSymTwo.CopyIn(
+		&nSignSymOne,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v\n", err.Error())
+		return
+	}
+
+	if !nSignSymTwo.Equal(&nSignSymOne) {
+		t.Errorf("%v - Error:\n"+
+			"Expected nSignSymTwo == nSignSymOne .\n"+
+			"HOWEVER, THEY ARE NOT EQUAL!\n",
+			ePrefix)
+
+		return
+	}
+
+}
+
+func TestNumberSignSymbolDto_CopyIn_000500(t *testing.T) {
+
+	ePrefix := "TestNumberSignSymbolDto_CopyIn_000500()"
+
+	nSignSymOne,
+		err := NumberSignSymbolDto{}.New(
+		"(",
+		")",
+		true,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v", err.Error())
+		return
+	}
+
+	nSignSymTwo := NumberSignSymbolDto{}
+
+	err = nSignSymTwo.CopyIn(
+		&nSignSymOne,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v\n", err.Error())
+		return
+	}
+
+	if !nSignSymTwo.Equal(&nSignSymOne) {
+		t.Errorf("%v - Error:\n"+
+			"Expected nSignSymTwo == nSignSymOne .\n"+
+			"HOWEVER, THEY ARE NOT EQUAL!\n",
+			ePrefix)
+
+		return
+	}
+
+}
+
+func TestNumberSignSymbolDto_CopyIn_000600(t *testing.T) {
+
+	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
+		"TestNumberSignSymbolDto_CopyIn_000600()",
+		"")
+
+	nSignSymMolecule := numberSignSymbolDtoMolecule{}
+
+	nSignSymOne,
+		err := NumberSignSymbolDto{}.New(
+		"+",
+		"",
+		false,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v", err.Error())
+		return
+	}
+
+	err = nSignSymMolecule.copyIn(
+		&nSignSymOne,
+		nil,
+		&ePrefix)
+
+	if err == nil {
+		t.Errorf("%v - Error:\n"+
+			"Expected an error return from nSignSymMolecule.copyIn()\n"+
+			"because 'incomingNumSignSymbol' is a nil pointer.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.String())
+		return
+	}
+
+}
+
+func TestNumberSignSymbolDto_CopyIn_000700(t *testing.T) {
+
+	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
+		"TestNumberSignSymbolDto_CopyIn_000700()",
+		"")
+
+	nSignSymMolecule := numberSignSymbolDtoMolecule{}
+
+	nSignSymTwo,
+		err := NumberSignSymbolDto{}.New(
+		"+",
+		"",
+		false,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v", err.Error())
+		return
+	}
+
+	err = nSignSymMolecule.copyIn(
+		nil,
+		&nSignSymTwo,
+		&ePrefix)
+
+	if err == nil {
+		t.Errorf("%v - Error:\n"+
+			"Expected an error return from nSignSymMolecule.copyIn()\n"+
+			"because 'targetNumSignSymbol' is a nil pointer.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.String())
+		return
+	}
+
+}
+
+func TestNumberSignSymbolDto_CopyIn_000800(t *testing.T) {
+
+	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
+		"TestNumberSignSymbolDto_CopyIn_000800()",
+		"")
+
+	nSignSymOne := NumberSignSymbolDto{}
+
+	nSignSymMolecule := numberSignSymbolDtoMolecule{}
+
+	nSignSymTwo,
+		err := NumberSignSymbolDto{}.New(
+		"+",
+		"",
+		false,
+		ePrefix)
+
+	if err != nil {
+		t.Errorf("%v", err.Error())
+		return
+	}
+
+	nSignSymTwo.leadingNumSignChars = nil
+	nSignSymTwo.trailingNumSignChars = nil
+
+	err = nSignSymMolecule.copyIn(
+		&nSignSymOne,
+		&nSignSymTwo,
+		&ePrefix)
+
+	if err == nil {
+		t.Errorf("%v - Error:\n"+
+			"Expected an error return from nSignSymMolecule.copyIn()\n"+
+			"because 'incomingNumSignSymbol' is invalid.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.String())
+		return
+	}
+
+}
