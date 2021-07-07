843227995f45e45bae9c40edc51edc5a3c014fa2
Mike Rapp <jordy.rascal@protonmail.com>
2021-07-07 04:11:02 -0500

    Added method textLineSpecBlankLinesMolecule.copyIn()

--- strmech/textlinespecblanklinesmolecule.go
+++ strmech/textlinespecblanklinesmolecule.go
@@ -10,6 +10,78 @@ type textLineSpecBlankLinesMolecule struct {
 	lock *sync.Mutex
 }
 
+// copyIn - Copies all data from input parameter 'incomingBlkLines'
+// to input parameter 'targetBlkLines'.
+//
+// Be advised that the data fields in 'targetBlkLines' will be
+// overwritten.
+//
+func (txtBlankLinesMolecule textLineSpecBlankLinesMolecule) copyIn(
+	targetBlkLines *TextLineSpecBlankLines,
+	incomingBlkLines *TextLineSpecBlankLines,
+	errPrefDto *ePref.ErrPrefixDto) error {
+
+	if txtBlankLinesMolecule.lock == nil {
+		txtBlankLinesMolecule.lock = new(sync.Mutex)
+	}
+
+	txtBlankLinesMolecule.lock.Lock()
+
+	defer txtBlankLinesMolecule.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
+		errPrefDto,
+		"textLineSpecBlankLinesMolecule.copyOut()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	if targetBlkLines == nil {
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'targetBlkLines' is a nil pointer!\n",
+			ePrefix.String())
+
+		return err
+	}
+
+	if incomingBlkLines == nil {
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'incomingBlkLines' is a nil pointer!\n",
+			ePrefix.String())
+
+		return err
+	}
+
+	if len(incomingBlkLines.newLineChars) == 0 {
+		incomingBlkLines.newLineChars = []rune{'\n'}
+	}
+
+	if incomingBlkLines.numBlankLines < 0 {
+		incomingBlkLines.numBlankLines = 0
+	}
+
+	targetBlkLines.numBlankLines =
+		incomingBlkLines.numBlankLines
+
+	lenIncomingChars := len(incomingBlkLines.newLineChars)
+
+	targetBlkLines.newLineChars =
+		make([]rune, lenIncomingChars)
+
+	for i := 0; i < lenIncomingChars; i++ {
+		targetBlkLines.newLineChars[i] =
+			incomingBlkLines.newLineChars[i]
+	}
+
+	return nil
+}
+
 // copyOut - Returns a deep copy of the TextLineSpecBlankLines
 // input parameter 'blkLines'.
 //
