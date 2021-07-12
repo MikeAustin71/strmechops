6be250e52c9ed238975272ea44c4ceed94d7311f
Mike Rapp <jordy.rascal@protonmail.com>
2021-07-12 06:04:58 -0500

    Added new method TextFieldSpecLabel.SetFieldLength()

--- strmech/textfieldspeclabel.go
+++ strmech/textfieldspeclabel.go
@@ -921,14 +921,91 @@ func (txtFieldLabel TextFieldSpecLabel) NewPtr(
 // field length in the current instance of TextFieldSpecLabel.
 //
 // The field length specifies the length of the text field in which
-// the text label string will be positioned.
+// the text label string will be positioned. If the field length is
+// greater than the Text Label string length, the field will be
+// padded with spaces according to the Text Justification setting.
 //
 // If the field length is less than the length of the text label
 // string, it will automatically reset to the length of the text
 // label string.
 //
+//
+// ------------------------------------------------------------------------
+//
+// Input Parameters
+//
+//  fieldLen                   int
+//     - The length of the field in which the 'textLabel' will be
+//       displayed.
+//
+//       If this parameter is submitted with a value less than
+//       minus one (-1) or greater than 1-million (1,000,000), an
+//       error will be returned.
+//
+//
+//  errorPrefix                interface{}
+//     - This object encapsulates error prefix text which is
+//       included in all returned error messages. Usually, it
+//       contains the name of the calling method or methods
+//       listed as a method or function chain of execution.
+//
+//       If no error prefix information is needed, set this parameter
+//       to 'nil'.
+//
+//       This empty interface must be convertible to one of the
+//       following types:
+//
+//
+//       1. nil - A nil value is valid and generates an empty
+//                collection of error prefix and error context
+//                information.
+//
+//       2. string - A string containing error prefix information.
+//
+//       3. []string A one-dimensional slice of strings containing
+//                   error prefix information
+//
+//       4. [][2]string A two-dimensional slice of strings containing
+//                      error prefix and error context information.
+//
+//       5. ErrPrefixDto - An instance of ErrPrefixDto. The
+//                         ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       6. *ErrPrefixDto - A pointer to an instance of ErrPrefixDto.
+//                          ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       7. IBasicErrorPrefix - An interface to a method generating
+//                              a two-dimensional slice of strings
+//                              containing error prefix and error
+//                              context information.
+//
+//       If parameter 'errorPrefix' is NOT convertible to one of
+//       the valid types listed above, it will be considered
+//       invalid and trigger the return of an error.
+//
+//       Types ErrPrefixDto and IBasicErrorPrefix are included in
+//       the 'errpref' software package, "github.com/MikeAustin71/errpref".
+//
+//
+// ------------------------------------------------------------------------
+//
+// Return Values
+//
+//  error
+//     - If this method completes successfully and no errors are
+//       encountered this return value is set to 'nil'. Otherwise,
+//       if errors are encountered, this return value will contain
+//       an appropriate error message.
+//
+//       If an error message is returned, the text value of input
+//       parameter 'errorPrefix' will be inserted or prefixed at
+//       the beginning of the error message.
+//
 func (txtFieldLabel *TextFieldSpecLabel) SetFieldLength(
-	fieldLen int) {
+	fieldLen int,
+	errorPrefix interface{}) error {
 
 	if txtFieldLabel.lock == nil {
 		txtFieldLabel.lock = new(sync.Mutex)
@@ -938,13 +1015,50 @@ func (txtFieldLabel *TextFieldSpecLabel) SetFieldLength(
 
 	defer txtFieldLabel.lock.Unlock()
 
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextFieldSpecFiller.SetFieldLength()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	if fieldLen < -1 {
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'fieldLen' is less than minus one (-1)!\n"+
+			"'fieldLen' controls the length of the Text Label Field.\n",
+			ePrefix.String())
+
+		return err
+	}
+
+	if fieldLen > 1000000 {
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'fieldLen' is greater than one-million (1,000,000)!\n"+
+			"'fieldLen' controls the length of the Text Label Field.\n",
+			ePrefix.String())
+		return err
+	}
+
 	txtFieldLabel.fieldLen = fieldLen
+
+	return nil
 }
 
 // SetTextJustification - Sets and replaces the current value of
-// the text justification enumeration specification. This
+// the Text Justification enumeration specification. This
 // specification will be used to position the text label string
 // within a text field.
+//
+// The field length specifies the length of the text field in which
+// the text label string will be positioned. If the field length is
+// greater than the Text Label string length, the field will be
+// padded with spaces according to the Text Justification setting.
 //
 //
 // ------------------------------------------------------------------------
