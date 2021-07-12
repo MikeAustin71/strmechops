0c1881f98c97b7ae6aa13607970f7aa0954103e6
Mike Rapp <jordy.rascal@protonmail.com>
2021-07-12 05:25:34 -0500

    Added new method TextFieldSpecFiller.NewPtr()

--- strmech/textfieldspecfiller.go
+++ strmech/textfieldspecfiller.go
@@ -141,7 +141,7 @@ func (txtFillerField TextFieldSpecFiller) NewConstructor(
 	ePrefix,
 		err = ePref.ErrPrefixDto{}.NewIEmpty(
 		errorPrefix,
-		"TextFieldSpecLabel.CopyIn()",
+		"TextFieldSpecFiller.NewConstructor()",
 		"")
 
 	if err != nil {
@@ -201,4 +201,160 @@ func (txtFillerField TextFieldSpecFiller) NewEmpty() TextFieldSpecFiller {
 
 	return textFieldSpecFillerMolecule{}.ptr().
 		newEmpty()
+}
+
+// NewPtr - Creates and returns a pointer to a new, fully populated
+// instance of TextFieldSpecFiller. This method is identical to
+// method TextFieldSpecFiller.NewConstructor() with the sole
+// distinction being that this method returns a pointer to a new
+// instance of TextFieldSpecFiller.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  fillerCharacters    string
+//     - A string containing the text characters which will be
+//       included in the Text Filler Field. The final Text Filler
+//       Field will be constructed from the filler characters
+//       repeated one or more times as specified by the
+//       'fillerCharsCount' parameter.
+//
+//
+//  fillerCharsCount    int
+//     - Controls the number of times 'fillerCharacters' is
+//       repeated when constructing the final Text Filler Field
+//       returned by this method. The actual length of the string
+//       which will populated the completed Text Filler Field is
+//       equal to the length of 'fillerCharacters' times the value
+//       of 'fillerCharsCount'.
+//
+//
+//  errorPrefix                interface{}
+//     - This object encapsulates error prefix text which is
+//       included in all returned error messages. Usually, it
+//       contains the name of the calling method or methods
+//       listed as a method or function chain of execution.
+//
+//       If no error prefix information is needed, set this parameter
+//       to 'nil'.
+//
+//       This empty interface must be convertible to one of the
+//       following types:
+//
+//
+//       1. nil - A nil value is valid and generates an empty
+//                collection of error prefix and error context
+//                information.
+//
+//       2. string - A string containing error prefix information.
+//
+//       3. []string A one-dimensional slice of strings containing
+//                   error prefix information
+//
+//       4. [][2]string A two-dimensional slice of strings containing
+//                      error prefix and error context information.
+//
+//       5. ErrPrefixDto - An instance of ErrPrefixDto. The
+//                         ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       6. *ErrPrefixDto - A pointer to an instance of ErrPrefixDto.
+//                          ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       7. IBasicErrorPrefix - An interface to a method generating
+//                              a two-dimensional slice of strings
+//                              containing error prefix and error
+//                              context information.
+//
+//       If parameter 'errorPrefix' is NOT convertible to one of
+//       the valid types listed above, it will be considered
+//       invalid and trigger the return of an error.
+//
+//       Types ErrPrefixDto and IBasicErrorPrefix are included in
+//       the 'errpref' software package, "github.com/MikeAustin71/errpref".
+//
+//
+// ------------------------------------------------------------------------
+//
+// Return Values
+//
+//  *TextFieldSpecFiller
+//     - If this method completes successfully, this parameter will
+//       return a pointer to a new, valid and fully populated Text
+//       Filler Field.
+//
+//
+//  error
+//     - If this method completes successfully and no errors are
+//       encountered this return value is set to 'nil'. Otherwise,
+//       if errors are encountered, this return value will contain
+//       an appropriate error message.
+//
+//       If an error message is returned, the text value of input
+//       parameter 'errorPrefix' will be inserted or prefixed at
+//       the beginning of the error message.
+//
+func (txtFillerField TextFieldSpecFiller) NewPtr(
+	fillerCharacters string,
+	fillerCharsCount int,
+	errorPrefix interface{}) (
+	*TextFieldSpecFiller,
+	error) {
+
+	if txtFillerField.lock == nil {
+		txtFillerField.lock = new(sync.Mutex)
+	}
+
+	txtFillerField.lock.Lock()
+
+	defer txtFillerField.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextFieldSpecFiller.NewConstructor()",
+		"")
+
+	if err != nil {
+		return &TextFieldSpecFiller{}, err
+	}
+
+	if len(fillerCharacters) == 0 {
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'fillerCharacters' is a zero length string!\n",
+			ePrefix.String())
+		return &TextFieldSpecFiller{}, err
+	}
+
+	if fillerCharsCount < 1 {
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'fillerCharsCount' is less than one (1)!\n"+
+			"'fillerCharsCount' controls the number of repetitions of 'fillerCharacters'\n"+
+			"in the Filler Text Field.\n",
+			ePrefix.String())
+		return &TextFieldSpecFiller{}, err
+	}
+
+	fillerCharsRunes := []rune(fillerCharacters)
+
+	lenFillerChars := len(fillerCharsRunes)
+
+	newTxtFillerField := textFieldSpecFillerMolecule{}.ptr().newEmpty()
+
+	newTxtFillerField.fillerCharacters =
+		make([]rune, lenFillerChars)
+
+	copy(newTxtFillerField.fillerCharacters,
+		fillerCharsRunes)
+
+	newTxtFillerField.fillerCharsCount =
+		fillerCharsCount
+
+	return &newTxtFillerField, nil
 }
