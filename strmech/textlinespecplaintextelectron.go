3e21e361f50000439dbd29731db8c056cc56d635
Mike Rapp <jordy.rascal@protonmail.com>
2021-09-04 16:24:23 -0500

    Added new method textLineSpecPlainTextElectron.equal()

--- strmech/textlinespecplaintextelectron.go
+++ strmech/textlinespecplaintextelectron.go
@@ -46,6 +46,66 @@ func (txtLinePlainTextElectron *textLineSpecPlainTextElectron) empty(
 	return
 }
 
+// equal - Receives pointers to two TextLineSpecPlainText
+// instances and proceeds to compare the member data elements to
+// determine whether they are equal.
+//
+// If the data elements of both input parameters 'plainTxtLineOne'
+// and 'plainTxtLineTwo' are equal in all respects, this method
+// returns a boolean value of 'true'. Otherwise, this method
+// returns 'false'.
+//
+func (txtLinePlainTextElectron *textLineSpecPlainTextElectron) equal(
+	plainTxtLineOne *TextLineSpecPlainText,
+	plainTxtLineTwo *TextLineSpecPlainText) bool {
+
+	if txtLinePlainTextElectron.lock == nil {
+		txtLinePlainTextElectron.lock = new(sync.Mutex)
+	}
+
+	txtLinePlainTextElectron.lock.Lock()
+
+	defer txtLinePlainTextElectron.lock.Unlock()
+
+	if plainTxtLineOne == nil ||
+		plainTxtLineTwo == nil {
+
+		return false
+	}
+
+	sMechPreon := strMechPreon{}
+
+	if !sMechPreon.equalRuneArrays(
+		plainTxtLineOne.leftMarginChars,
+		plainTxtLineOne.leftMarginChars) {
+		return false
+	}
+
+	if !sMechPreon.equalRuneArrays(
+		plainTxtLineOne.rightMarginChars,
+		plainTxtLineOne.rightMarginChars) {
+		return false
+	}
+
+	if plainTxtLineOne.textString !=
+		plainTxtLineTwo.textString {
+		return false
+	}
+
+	if plainTxtLineOne.turnLineTerminatorOff !=
+		plainTxtLineTwo.turnLineTerminatorOff {
+		return false
+	}
+
+	if !sMechPreon.equalRuneArrays(
+		plainTxtLineOne.newLineChars,
+		plainTxtLineOne.newLineChars) {
+		return false
+	}
+
+	return true
+}
+
 // ptr - Returns a pointer to a new instance of
 // textLineSpecPlainTextElectron.
 //
