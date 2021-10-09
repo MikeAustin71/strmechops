7714fe2ef26c1313fce5c7b46e3f7cfd81ddbc03
Mike Rapp <jordy.rascal@protonmail.com>
2021-10-08 22:26:15 -0500

    Added new tests for textFieldSpecDateTimeAtom.equal().

--- strmech/zzzt_textfielddatetime_0001000_test.go
+++ strmech/zzzt_textfielddatetime_0001000_test.go
@@ -33,7 +33,7 @@ func TestTextFieldSpecDateTimeAtom_empty_000100(t *testing.T) {
 
 	txtFieldDateTime.dateTime = time.Date(
 		2021,
-		time.Month(20),
+		time.Month(10),
 		6,
 		23,
 		55,
@@ -130,7 +130,7 @@ func TestTextFieldSpecDateTime_Empty_000100(t *testing.T) {
 
 	txtFieldDateTime.dateTime = time.Date(
 		2021,
-		time.Month(20),
+		time.Month(10),
 		6,
 		23,
 		55,
@@ -194,6 +194,249 @@ func TestTextFieldSpecDateTime_Empty_000100(t *testing.T) {
 	txtFieldDateTime2 := TextFieldSpecDateTime{}
 
 	txtFieldDateTime2.Empty()
+
+	return
+}
+
+func TestTextFieldSpecDateTimeAtom_equal_000100(t *testing.T) {
+
+	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
+		"TestTextFieldSpecDateTimeAtom_equal_000100()",
+		"")
+
+	txtFieldDateTimeOne := TextFieldSpecDateTime{}
+
+	timeZoneName := "America/Chicago"
+
+	tzLocPtr, err := time.LoadLocation(timeZoneName)
+
+	if err != nil {
+		t.Errorf("%v\n"+
+			"Error returned by time.LoadLocation(timeZoneName)\n"+
+			"timeZoneName='%v'\n"+
+			"Error='%v'\n",
+			ePrefix.String(),
+			timeZoneName,
+			err.Error())
+
+		return
+
+	}
+
+	txtFieldDateTimeOne.dateTime = time.Date(
+		2021,
+		time.Month(10),
+		6,
+		23,
+		55,
+		0,
+		0,
+		tzLocPtr)
+
+	txtFieldDateTimeOne.dateTimeFormat =
+		"Monday January 2, 2006 15:04:05.000000000 -0700 MST"
+
+	txtFieldDateTimeOne.fieldLen = -1
+
+	txtFieldDateTimeOne.textJustification = TxtJustify.Left()
+
+	txtFieldDateTimeAtom := textFieldSpecDateTimeAtom{}
+
+	areEqual := txtFieldDateTimeAtom.equal(
+		nil,
+		&txtFieldDateTimeOne)
+
+	if areEqual == true {
+		t.Errorf("%v Series #1\n"+
+			"Error: Expected txtFieldDateTimeAtom.equal() to\n"+
+			"return 'false'.\n"+
+			"Instead, txtFieldDateTimeAtom.equal() returned\n"+
+			"'true'\n",
+			ePrefix.String())
+
+		return
+
+	}
+
+	areEqual = txtFieldDateTimeAtom.equal(
+		&txtFieldDateTimeOne,
+		nil)
+
+	if areEqual == true {
+		t.Errorf("%v Series #2\n"+
+			"Error: Expected txtFieldDateTimeAtom.equal() to\n"+
+			"return 'false'.\n"+
+			"Instead, txtFieldDateTimeAtom.equal() returned\n"+
+			"'true'\n",
+			ePrefix.String())
+
+		return
+	}
+
+	areEqual = txtFieldDateTimeAtom.equal(
+		nil,
+		nil)
+
+	if areEqual == true {
+		t.Errorf("%v Series #3\n"+
+			"Error: Expected txtFieldDateTimeAtom.equal() to\n"+
+			"return 'false'.\n"+
+			"Instead, txtFieldDateTimeAtom.equal() returned\n"+
+			"'true'\n",
+			ePrefix.String())
+
+		return
+	}
+
+	txtFieldDateTimeTwo := TextFieldSpecDateTime{}
+
+	timeZoneName = "America/Chicago"
+
+	tzLocPtr, err = time.LoadLocation(timeZoneName)
+
+	if err != nil {
+		t.Errorf("%v Series #4\n"+
+			"Error returned by time.LoadLocation(timeZoneName)\n"+
+			"timeZoneName='%v'\n"+
+			"Error='%v'\n",
+			ePrefix.String(),
+			timeZoneName,
+			err.Error())
+
+		return
+
+	}
+
+	txtFieldDateTimeTwo.dateTime = time.Date(
+		2021,
+		time.Month(10),
+		6,
+		23,
+		55,
+		0,
+		0,
+		tzLocPtr)
+
+	txtFieldDateTimeTwo.dateTimeFormat =
+		"Monday January 2, 2006 15:04:05.000000000 -0700 MST"
+
+	txtFieldDateTimeTwo.fieldLen = -1
+
+	txtFieldDateTimeTwo.textJustification = TxtJustify.Left()
+
+	txtFieldDateTimeAtom2 := textFieldSpecDateTimeAtom{}
+
+	areEqual = txtFieldDateTimeAtom2.equal(
+		&txtFieldDateTimeOne,
+		&txtFieldDateTimeTwo)
+
+	if areEqual == false {
+		t.Errorf("%v Series #5\n"+
+			"Error: Expected txtFieldDateTimeAtom.equal() to\n"+
+			"return 'true'.\n"+
+			"Instead, txtFieldDateTimeAtom.equal() returned\n"+
+			"'false'\n",
+			ePrefix.String())
+
+		return
+	}
+
+	txtFieldDateTimeThree := TextFieldSpecDateTime{}
+
+	txtFieldDateTimeThree.dateTime =
+		txtFieldDateTimeTwo.dateTime
+
+	txtFieldDateTimeThree.dateTimeFormat =
+		txtFieldDateTimeTwo.dateTimeFormat
+
+	txtFieldDateTimeThree.fieldLen =
+		txtFieldDateTimeTwo.fieldLen
+
+	txtFieldDateTimeThree.textJustification =
+		TxtJustify.None()
+
+	areEqual = txtFieldDateTimeAtom2.equal(
+		&txtFieldDateTimeTwo,
+		&txtFieldDateTimeThree)
+
+	if areEqual == true {
+		t.Errorf("%v Series #6\n"+
+			"Error: Expected txtFieldDateTimeAtom.equal() to return\n"+
+			"'false' because Text Justifcations are NOT equal.\n"+
+			"Instead, txtFieldDateTimeAtom.equal() returned\n"+
+			"'true'\n",
+			ePrefix.String())
+
+		return
+	}
+
+	txtFieldDateTimeThree.textJustification =
+		txtFieldDateTimeTwo.textJustification
+
+	txtFieldDateTimeThree.fieldLen = 927
+
+	areEqual = txtFieldDateTimeAtom2.equal(
+		&txtFieldDateTimeTwo,
+		&txtFieldDateTimeThree)
+
+	if areEqual == true {
+		t.Errorf("%v Series #7\n"+
+			"Error: Expected txtFieldDateTimeAtom.equal() to return\n"+
+			"'false' because Field Lengths are NOT equal.\n"+
+			"Instead, txtFieldDateTimeAtom.equal() returned\n"+
+			"'true'\n",
+			ePrefix.String())
+
+		return
+	}
+
+	txtFieldDateTimeThree.fieldLen =
+		txtFieldDateTimeTwo.fieldLen
+
+	txtFieldDateTimeThree.dateTimeFormat = ""
+
+	areEqual = txtFieldDateTimeAtom2.equal(
+		&txtFieldDateTimeTwo,
+		&txtFieldDateTimeThree)
+
+	if areEqual == true {
+		t.Errorf("%v Series #7\n"+
+			"Error: Expected txtFieldDateTimeAtom.equal() to return\n"+
+			"'false' because Date Time Formats are NOT equal.\n"+
+			"Instead, txtFieldDateTimeAtom.equal() returned\n"+
+			"'true'\n",
+			ePrefix.String())
+
+		return
+	}
+
+	txtFieldDateTimeThree.dateTimeFormat =
+		txtFieldDateTimeTwo.dateTimeFormat
+
+	txtFieldDateTimeThree.dateTime = time.Date(
+		2021,
+		time.Month(10),
+		8,
+		22,
+		24,
+		0,
+		0,
+		tzLocPtr)
+
+	areEqual = txtFieldDateTimeAtom2.equal(
+		&txtFieldDateTimeTwo,
+		&txtFieldDateTimeThree)
+
+	if areEqual == true {
+		t.Errorf("%v Series #7\n"+
+			"Error: Expected txtFieldDateTimeAtom.equal() to return\n"+
+			"'false' because Date Time Values are NOT equal.\n"+
+			"Instead, txtFieldDateTimeAtom.equal() returned\n"+
+			"'true'\n",
+			ePrefix.String())
+
+		return
+	}
 
 	return
 }
