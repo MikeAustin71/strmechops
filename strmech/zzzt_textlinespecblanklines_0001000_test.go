7136b488b5528b8aef4f1c1fc9c17593ba190379
Mike Rapp <jordy.rascal@protonmail.com>
2021-11-07 23:51:33 -0600

    Added tests for textLineSpecBlankLinesMolecule.copyOut()

--- strmech/zzzt_textlinespecblanklines_0001000_test.go
+++ strmech/zzzt_textlinespecblanklines_0001000_test.go
@@ -11,9 +11,11 @@ func TestTextLineSpecBlankLines_copyIn_000100(t *testing.T) {
 		"TestTextLineSpecBlankLines_copyIn_000100()",
 		"")
 
+	numOfBlankLines := 3
+
 	incomingBlankLines,
 		err := TextLineSpecBlankLines{}.NewBlankLines(
-		3,
+		numOfBlankLines,
 		ePrefix.XCtx(
 			"incomingBlankLines"))
 
@@ -65,6 +67,63 @@ func TestTextLineSpecBlankLines_copyIn_000100(t *testing.T) {
 			"Expected targetBlankLines==incomingBlankLines\n"+
 			"HOWEVER, THESE INSTANCES ARE NOT EQUAL!\n",
 			ePrefix.XCtxEmpty().String())
+		return
+	}
+
+	targetBlankLinesTwo := TextLineSpecBlankLines{}
+
+	err =
+		txtBlankLinesMolecule.copyIn(
+			&targetBlankLinesTwo,
+			nil,
+			ePrefix.XCtx(
+				"'nil'->targetBlankLinesTwo"))
+
+	if err == nil {
+		t.Errorf("%v\n"+
+			"Expected an error return from "+
+			"txtBlankLinesMolecule.copyIn()\n"+
+			"because input parameter 'incomingBlkLines' is 'nil'.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtx("Missing Error Return"))
+		return
+	}
+
+	txtBlankLinesMolecule2 := textLineSpecBlankLinesMolecule{}
+
+	err =
+		txtBlankLinesMolecule2.copyIn(
+			nil,
+			&incomingBlankLines,
+			ePrefix.XCtx(
+				"'nil'->targetBlankLinesTwo"))
+
+	if err == nil {
+		t.Errorf("%v\n"+
+			"Expected an error return from "+
+			"txtBlankLinesMolecule.copyIn()\n"+
+			"because input parameter 'targetBlkLines' is 'nil'.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtx("Missing Error Return"))
+		return
+	}
+
+	incomingBlankLines.numBlankLines = -9999
+
+	err = textLineSpecBlankLinesMolecule{}.ptr().
+		copyIn(
+			&targetBlankLinesTwo,
+			&incomingBlankLines,
+			ePrefix.XCtx(
+				"incomingBlankLines is invalid!"))
+
+	if err == nil {
+		t.Errorf("%v\n"+
+			"Expected an error return from "+
+			"txtBlankLinesMolecule.copyIn()\n"+
+			"because input parameter 'incomingBlankLines' is invalid.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtx("Missing Error Return"))
 		return
 	}
 
@@ -146,6 +205,110 @@ func TestTextLineSpecBlankLines_CopyIn_000100(t *testing.T) {
 			"HOWEVER, NO ERROR WAS RETURNED!\n",
 			ePrefix.XCtxEmpty().String())
 
+		return
+	}
+
+	return
+}
+
+func TestTextLineSpecBlankLines_copyOut_000100(t *testing.T) {
+
+	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
+		"TestTextLineSpecBlankLines_copyOut_000100()",
+		"")
+
+	numOfBlankLines := 3
+
+	txtBlankLinesBase,
+		err := TextLineSpecBlankLines{}.NewBlankLines(
+		numOfBlankLines,
+		ePrefix.XCtx(
+			"txtBlankLinesBase"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	err = txtBlankLinesBase.IsValidInstanceError(
+		ePrefix.XCtx(
+			"txtBlankLinesBase"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	txtBlankLinesMolecule := textLineSpecBlankLinesMolecule{}
+
+	var txtBlankLinesOut TextLineSpecBlankLines
+
+	txtBlankLinesOut,
+		err =
+		txtBlankLinesMolecule.copyOut(
+			&txtBlankLinesBase,
+			ePrefix.XCtx(
+				"txtBlankLinesBase->txtBlankLinesOut"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	err = txtBlankLinesOut.IsValidInstanceError(
+		ePrefix.XCtx(
+			"txtBlankLinesOut"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	if !txtBlankLinesOut.Equal(&txtBlankLinesBase) {
+		t.Errorf("%v - ERROR\n"+
+			"Expected txtBlankLinesOut==txtBlankLinesBase\n"+
+			"HOWEVER, THESE INSTANCES ARE NOT EQUAL!\n",
+			ePrefix.XCtxEmpty().String())
+		return
+	}
+
+	_,
+		err =
+		txtBlankLinesMolecule.copyOut(
+			nil,
+			ePrefix.XCtx(
+				"'nil'->txtBlankLinesOutTwo"))
+
+	if err == nil {
+		t.Errorf("%v\n"+
+			"Expected an error return from "+
+			"txtBlankLinesMolecule.copyOut()\n"+
+			"because input parameter 'txtBlankLines' is 'nil'.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtx("Missing Error Return"))
+		return
+	}
+
+	txtBlankLinesBase.numBlankLines = -9999
+
+	_,
+		err = textLineSpecBlankLinesMolecule{}.ptr().
+		copyOut(
+			&txtBlankLinesBase,
+			ePrefix.XCtx(
+				"txtBlankLinesBase is invalid!"))
+
+	if err == nil {
+		t.Errorf("%v\n"+
+			"Expected an error return from "+
+			"txtBlankLinesMolecule.copyOut()\n"+
+			"because input parameter 'txtBlankLinesBase' is invalid.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtx("Missing Error Return"))
 		return
 	}
 
