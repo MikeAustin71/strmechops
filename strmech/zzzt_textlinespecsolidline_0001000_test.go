34efc8d451bba738b30c46bd9660f36efc14d1f7
Mike Rapp <jordy.rascal@protonmail.com>
2021-12-07 03:29:41 -0600

    Added tests for TextLineSpecSolidLine.CopyIn()

---
+++ strmech/zzzt_textlinespecsolidline_0001000_test.go
@@ -0,0 +1,152 @@
+package strmech
+
+import (
+	ePref "github.com/MikeAustin71/errpref"
+	"strings"
+	"testing"
+)
+
+func TestTextLineSpecSolidLine_CopyIn_000100(t *testing.T) {
+
+	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
+		"TestTextLineSpecSolidLine_CopyIn_000100()",
+		"")
+
+	solidLineChars := "-"
+	solidLineCharsRepeatCount := 35
+
+	expectedSolidLineStr :=
+		strings.Repeat(
+			solidLineChars,
+			solidLineCharsRepeatCount) +
+			"\n"
+
+	txtSolidLine01,
+		err := TextLineSpecSolidLine{}.NewDefaultSolidLine(
+		solidLineChars,
+		solidLineCharsRepeatCount,
+		ePrefix.XCtx(
+			"txtSolidLine01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	var txtSolidLine01FmtText, txtSolidLine02FmtText string
+
+	txtSolidLine01FmtText,
+		err = txtSolidLine01.GetFormattedText(
+		ePrefix.XCtx(
+			"txtSolidLine01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	txtSolidLine02 := TextLineSpecSolidLine{}
+
+	err =
+		txtSolidLine02.CopyIn(
+			&txtSolidLine01,
+			ePrefix.XCtx(
+				"txtSolidLine02<-txtSolidLine01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	txtSolidLine02FmtText,
+		err = txtSolidLine02.GetFormattedText(
+		ePrefix.XCtx(
+			"txtSolidLine02"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	sMech := StrMech{}
+
+	printableExpectedStr :=
+		sMech.ConvertNonPrintableChars(
+			[]rune(txtSolidLine01FmtText),
+			true)
+
+	printableActualStr :=
+		sMech.ConvertNonPrintableChars(
+			[]rune(txtSolidLine02FmtText),
+			true)
+
+	if printableExpectedStr != printableActualStr {
+
+		t.Errorf("%v - ERROR\n"+
+			"Test #1"+
+			"Expected Formatted Text String 01 DOES NOT match\n"+
+			"Actual Formatted Text String 02.\n"+
+			"Expected Formatted Text String = '%v'\n"+
+			"Instead, Formatted Text String = '%v'\n",
+			ePrefix.XCtxEmpty().String(),
+			printableExpectedStr,
+			printableActualStr)
+
+		return
+	}
+
+	printableExpectedStr =
+		sMech.ConvertNonPrintableChars(
+			[]rune(expectedSolidLineStr),
+			true)
+
+	if printableExpectedStr != printableActualStr {
+
+		t.Errorf("%v - ERROR\n"+
+			"Test #2"+
+			"Expected Formatted Text String DOES NOT match\n"+
+			"Actual Formatted Text String 02.\n"+
+			"Expected Formatted Text String = '%v'\n"+
+			"Instead, Formatted Text String = '%v'\n",
+			ePrefix.XCtxEmpty().String(),
+			printableExpectedStr,
+			printableActualStr)
+
+		return
+	}
+
+	if !txtSolidLine02.Equal(&txtSolidLine01) {
+		t.Errorf("%v - ERROR\n"+
+			"Test #3"+
+			"Expected Text Solid Line Object #1 would be\n"+
+			"Equal to Text Solid Line Object #2.\n"+
+			"HOWEVER, THEY ARE NOT EQUAL!!!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	txtSolidLine03 := TextLineSpecSolidLine{}
+
+	err =
+		txtSolidLine03.CopyIn(
+			&txtSolidLine02,
+			StrMech{})
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from txtSolidLine03."+
+			"CopyIn()\n"+
+			"because 'errorPrefix' is invalid.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+}
