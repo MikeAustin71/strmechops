2f19afb06aa612001e6364d45f2725c51b0e383a
Mike Rapp <jordy.rascal@protonmail.com>
2021-12-08 16:30:13 -0600

    Added new test for TextLineSpecSolidLine.CopyOutPtr().

--- strmech/zzzt_textlinespecsolidline_0001000_test.go
+++ strmech/zzzt_textlinespecsolidline_0001000_test.go
@@ -554,3 +554,202 @@ func TestTextLineSpecSolidLine_CopyOutITextLine_000100(t *testing.T) {
 
 	return
 }
+
+func TestTextLineSpecSolidLine_CopyOutPtr_000100(t *testing.T) {
+
+	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
+		"TestTextLineSpecSolidLine_CopyOutPtr_000100()",
+		"")
+
+	leftMargin := 2
+	rightMargin := 2
+	solidLineChars := "-"
+	solidLineCharsRepeatCount := 35
+	newLineChars := "\n"
+
+	expectedSolidLineStr :=
+		strings.Repeat(" ", leftMargin) +
+			strings.Repeat(
+				solidLineChars,
+				solidLineCharsRepeatCount) +
+			strings.Repeat(" ", rightMargin) +
+			newLineChars
+
+	txtSolidLine01,
+		err := TextLineSpecSolidLine{}.NewSolidLine(
+		leftMargin,
+		rightMargin,
+		solidLineChars,
+		solidLineCharsRepeatCount,
+		ePrefix.XCtx(
+			"txtSolidLine01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	err = txtSolidLine01.IsValidInstanceError(
+		ePrefix.XCtx("txtSolidLine01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	var txtSolidLine01FmtText, txtSolidLine02FmtText string
+
+	txtSolidLine01FmtText,
+		err = txtSolidLine01.GetFormattedText(
+		ePrefix.XCtx(
+			"txtSolidLine01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	var txtSolidLine02 *TextLineSpecSolidLine
+
+	txtSolidLine02,
+		err =
+		txtSolidLine01.CopyOutPtr(
+			ePrefix.XCtx(
+				"txtSolidLine02<-txtSolidLine01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	txtSolidLine02FmtText,
+		err = txtSolidLine02.GetFormattedText(
+		ePrefix.XCtx(
+			"txtSolidLine02"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	sMech := StrMech{}
+
+	printableExpectedStr :=
+		sMech.ConvertNonPrintableChars(
+			[]rune(txtSolidLine01FmtText),
+			true)
+
+	printableActualStr :=
+		sMech.ConvertNonPrintableChars(
+			[]rune(txtSolidLine02FmtText),
+			true)
+
+	if printableExpectedStr != printableActualStr {
+
+		t.Errorf("%v - ERROR\n"+
+			"Test #1\n"+
+			"Expected Formatted Text String 01 DOES NOT match\n"+
+			"Actual Formatted Text String 02.\n"+
+			"Expected Formatted Text String = '%v'\n"+
+			"Instead, Formatted Text String = '%v'\n",
+			ePrefix.XCtxEmpty().String(),
+			printableExpectedStr,
+			printableActualStr)
+
+		return
+	}
+
+	printableExpectedStr =
+		sMech.ConvertNonPrintableChars(
+			[]rune(expectedSolidLineStr),
+			true)
+
+	if printableExpectedStr != printableActualStr {
+
+		t.Errorf("%v - ERROR\n"+
+			"Test #2\n"+
+			"Expected Formatted Text String DOES NOT match\n"+
+			"Actual Formatted Text String 02.\n"+
+			"Expected Formatted Text String = '%v'\n"+
+			"Instead, Formatted Text String = '%v'\n",
+			ePrefix.XCtxEmpty().String(),
+			printableExpectedStr,
+			printableActualStr)
+
+		return
+	}
+
+	if !txtSolidLine02.Equal(&txtSolidLine01) {
+		t.Errorf("%v - ERROR\n"+
+			"Test #3\n"+
+			"Expected Text Solid Line Object #1 would be\n"+
+			"Equal to Text Solid Line Object #2.\n"+
+			"HOWEVER, THEY ARE NOT EQUAL!!!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	txtSolidLine03 := TextLineSpecSolidLine{}
+
+	_,
+		err =
+		txtSolidLine03.CopyOutPtr(
+			StrMech{})
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from txtSolidLine03."+
+			"CopyOutPtr()\n"+
+			"because 'errorPrefix' is invalid.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	_,
+		err =
+		txtSolidLine01.CopyOutPtr(
+			StrMech{})
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from txtSolidLine01."+
+			"CopyOutPtr()\n"+
+			"because 'errorPrefix' is invalid.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	txtSolidLine04 := TextLineSpecSolidLine{}
+
+	_,
+		err =
+		txtSolidLine04.CopyOutPtr(
+			ePrefix.XCtx(
+				"txtSolidLine04"))
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from txtSolidLine04."+
+			"CopyOutPtr()\n"+
+			"because 'txtSolidLine04' is empty.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	return
+}
