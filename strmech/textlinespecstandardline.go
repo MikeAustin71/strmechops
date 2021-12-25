ec1e9b38e6d418ad0dd71def5b21dec96ee253c2
Mike Rapp <jordy.rascal@protonmail.com>
2021-12-25 16:47:43 -0600

    Added new method TextLineSpecStandardLine.AddTextFieldSpacer()

--- strmech/textlinespecstandardline.go
+++ strmech/textlinespecstandardline.go
@@ -646,6 +646,110 @@ func (stdLine *TextLineSpecStandardLine) AddTextFieldLabel(
 	return indexId, err
 }
 
+// AddTextFieldSpacer - This method will append a Spacer text field
+// object to the end of the current array of text field objects
+// maintained by the current instance of TextLineSpecStandardLine.
+//
+// This operation will create a new TextFieldSpecSpacer object
+// based on the input parameters passed by the calling function.
+// This TextFieldSpecSpacer object will then be added to the text
+// field objects collection for the current
+// TextLineSpecStandardLine instance.
+//
+// If the method completes successfully, the internal array index
+// of the new Text Field Filler Object will be returned to the
+// calling function.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  fieldLen                   int
+//     - An integer value which specifies the number of white space
+//       characters to be included in the spacer text field.
+//
+//       Examples:
+//          fieldLen = 1 produces text field " "
+//          fieldLen = 2 produces text field "  "
+//          fieldLen = 5 produces text field "     "
+//
+//       If 'fieldLen' is less than one (+1), an error will be
+//       returned.
+//
+//       If 'fieldLen' is greater than one-million (+1,000,000), an
+//       error will be returned.
+//
+//
+//  errorPrefix                interface{}
+//     - This object encapsulates error prefix text which is
+//       included in all returned error messages. Usually, it
+//       contains the name of the calling method or methods
+//       listed as a method or function chain of execution.
+//
+//       If no error prefix information is needed, set this parameter
+//       to 'nil'.
+//
+//       This empty interface must be convertible to one of the
+//       following types:
+//
+//
+//       1. nil - A nil value is valid and generates an empty
+//                collection of error prefix and error context
+//                information.
+//
+//       2. string - A string containing error prefix information.
+//
+//       3. []string A one-dimensional slice of strings containing
+//                   error prefix information
+//
+//       4. [][2]string A two-dimensional slice of strings containing
+//                      error prefix and error context information.
+//
+//       5. ErrPrefixDto - An instance of ErrPrefixDto. The
+//                         ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       6. *ErrPrefixDto - A pointer to an instance of ErrPrefixDto.
+//                          ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       7. IBasicErrorPrefix - An interface to a method generating
+//                              a two-dimensional slice of strings
+//                              containing error prefix and error
+//                              context information.
+//
+//       If parameter 'errorPrefix' is NOT convertible to one of
+//       the valid types listed above, it will be considered
+//       invalid and trigger the return of an error.
+//
+//       Types ErrPrefixDto and IBasicErrorPrefix are included in
+//       the 'errpref' software package, "github.com/MikeAustin71/errpref".
+//
+//
+// ------------------------------------------------------------------------
+//
+// Return Values
+//
+//  indexId                    int
+//     - If this method completes successfully, the internal array
+//       index of the new text label object will be returned as an
+//       integer value.
+//
+//       In the event of an error, 'indexId' will be set to a value
+//       of minus one (-1).
+//
+//
+//  err                        error
+//     - If this method completes successfully and no errors are
+//       encountered, this return value is set to 'nil'. Otherwise,
+//       if errors are encountered, this return value will contain
+//       an appropriate error message.
+//
+//       If an error message is returned, the text value of input
+//       parameter 'errorPrefix' will be inserted or prefixed at
+//       the beginning of the error message.
+//
 func (stdLine *TextLineSpecStandardLine) AddTextFieldSpacer(
 	fieldLen int,
 	errorPrefix interface{}) (
@@ -667,7 +771,7 @@ func (stdLine *TextLineSpecStandardLine) AddTextFieldSpacer(
 	ePrefix,
 		err = ePref.ErrPrefixDto{}.NewIEmpty(
 		errorPrefix,
-		"TextLineSpecStandardLine.AddTextFieldLabel()",
+		"TextLineSpecStandardLine.AddTextFieldSpacer()",
 		"")
 
 	if err != nil {
