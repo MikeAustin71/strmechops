f6c80b3fe99df1ebe840b1d11a0e007b8c42828c
Mike Rapp <jordy.rascal@protonmail.com>
2021-12-26 15:58:55 -0600

    Added new method TextLineSpecStandardLine.PeekAtLastTextField()

--- strmech/textlinespecstandardline.go
+++ strmech/textlinespecstandardline.go
@@ -2521,6 +2521,143 @@ func (stdLine *TextLineSpecStandardLine) PeekAtFirstIndex(
 	return iTxtFieldSpec, err
 }
 
+// PeekAtLastTextField - Returns a deep copy of the last Text Field
+// ('ITextFieldSpecification') object in the Text Field Collection
+// ('stdLine.textFields[lastIdx]').
+//
+// As a 'Peek' method, the original Text Field object
+// ('stdLine.textFields[lastIdx]') WILL NOT be deleted from the Text
+// Field Collection encapsulated by this instance of
+// TextLineSpecStandardLine.
+//
+// After completion of this method, the Text Field Collection array
+// will remain unchanged.
+//
+//
+// ------------------------------------------------------------------------
+//
+// Input Parameters
+//
+//  errorPrefix                interface{}
+//     - This object encapsulates error prefix text which is
+//       included in all returned error messages. Usually, it
+//       contains the name of the calling method or methods
+//       listed as a method or function chain of execution.
+//
+//       If no error prefix information is needed, set this parameter
+//       to 'nil'.
+//
+//       This empty interface must be convertible to one of the
+//       following types:
+//
+//
+//       1. nil - A nil value is valid and generates an empty
+//                collection of error prefix and error context
+//                information.
+//
+//       2. string - A string containing error prefix information.
+//
+//       3. []string A one-dimensional slice of strings containing
+//                   error prefix information
+//
+//       4. [][2]string A two-dimensional slice of strings containing
+//                      error prefix and error context information.
+//
+//       5. ErrPrefixDto - An instance of ErrPrefixDto. The
+//                         ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       6. *ErrPrefixDto - A pointer to an instance of ErrPrefixDto.
+//                          ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       7. IBasicErrorPrefix - An interface to a method generating
+//                              a two-dimensional slice of strings
+//                              containing error prefix and error
+//                              context information.
+//
+//       If parameter 'errorPrefix' is NOT convertible to one of
+//       the valid types listed above, it will be considered
+//       invalid and trigger the return of an error.
+//
+//       Types ErrPrefixDto and IBasicErrorPrefix are included in
+//       the 'errpref' software package, "github.com/MikeAustin71/errpref".
+//
+//
+// ------------------------------------------------------------------------
+//
+// Return Values
+//
+//  iTxtFieldSpec              ITextFieldSpecification
+//     - If this method completes successfully, a deep copy of
+//       the designated member of the Text Fields Collection
+//       will be returned to the calling function. The returned
+//       object will implement the ITextFieldSpecification
+//       interface.
+//
+//
+//  err                        error
+//     - If this method completes successfully and no errors are
+//       encountered, this return value is set to 'nil'. Otherwise,
+//       if errors are encountered, this return value will contain
+//       an appropriate error message.
+//
+//       If an error message is returned, the text value of input
+//       parameter 'errorPrefix' will be inserted or prefixed at
+//       the beginning of the error message.
+//
+func (stdLine *TextLineSpecStandardLine) PeekAtLastTextField(
+	errorPrefix interface{}) (
+	iTxtFieldSpec ITextFieldSpecification,
+	err error) {
+
+	if stdLine.lock == nil {
+		stdLine.lock = new(sync.Mutex)
+	}
+
+	stdLine.lock.Lock()
+
+	defer stdLine.lock.Unlock()
+
+	iTxtFieldSpec = nil
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextLineSpecStandardLine."+
+			"PeekAtTextFieldAtIndex()",
+		"")
+
+	if err != nil {
+		return iTxtFieldSpec, err
+	}
+
+	lastIdx := len(stdLine.textFields) - 1
+
+	if lastIdx < 0 {
+		err = fmt.Errorf("%v - ERROR\n"+
+			"The Text Field Collection is empty!\n",
+			ePrefix.String())
+
+		return
+	}
+
+	iTxtFieldSpec,
+		err = textLineSpecStandardLineAtom{}.ptr().
+		peekPopTextField(
+			stdLine,
+			lastIdx,
+			false,
+			ePrefix.XCtx(
+				fmt.Sprintf(
+					"stdLine.textFields[%v]",
+					lastIdx)))
+
+	return iTxtFieldSpec, err
+}
+
 // PeekAtTextFieldAtIndex - Returns a deep copy of the Text Field
 // ('ITextFieldSpecification') object located at index, 'indexId',
 // in the Text Field Collection ('stdLine.textFields').
