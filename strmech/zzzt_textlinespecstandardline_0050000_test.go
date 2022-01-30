f90e81d5480b592be02f110e44e038a04f691942
Mike Rapp <jordy.rascal@protonmail.com>
2022-01-29 22:35:33 -0600

    Added tests for method textLineSpecStandardLineNanobot.copyOut()

--- strmech/zzzt_textlinespecstandardline_0050000_test.go
+++ strmech/zzzt_textlinespecstandardline_0050000_test.go
@@ -150,7 +150,7 @@ func TestTextLineSpecStandardLine_copyIn_000100(t *testing.T) {
 			&stdLine02,
 			nil,
 			ePrefix.XCtx(
-				"stdLine02<-nil"))
+				"incomingStdLine==nil"))
 
 	if err == nil {
 
@@ -243,4 +243,222 @@ func TestTextLineSpecStandardLine_copyIn_000100(t *testing.T) {
 	}
 
 	return
+}
+
+func TestTextLineSpecStandardLine_copyOut_000100(t *testing.T) {
+
+	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
+		"TestTextLineSpecStandardLine_copyOut_000100()",
+		"")
+
+	stdLine01 := TextLineSpecStandardLine{}.NewPtr()
+
+	rightMarginLen := 5
+
+	rightMarginSpec,
+		err := TextFieldSpecSpacer{}.NewSpacer(
+		rightMarginLen,
+		ePrefix.XCtx(
+			"rightMarginSpec"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	var leftMarginSpec TextFieldSpecSpacer
+
+	leftMarginLen := 6
+
+	leftMarginSpec,
+		err = TextFieldSpecSpacer{}.NewSpacer(
+		leftMarginLen,
+		ePrefix.XCtx(
+			"leftMarginSpec"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	label := "How Now Brown Cow!"
+	fieldLen := len(label) + 4
+	txtJustify := TxtJustify.Center()
+
+	var labelSpec TextFieldSpecLabel
+
+	labelSpec,
+		err = TextFieldSpecLabel{}.NewTextLabel(
+		label,
+		fieldLen,
+		txtJustify,
+		ePrefix.XCtx(
+			"labelSpec"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	_,
+		err = stdLine01.AddTextField(
+		&leftMarginSpec,
+		ePrefix.XCtx(
+			"stdLine01<-leftMarginSpec"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	_,
+		err = stdLine01.AddTextField(
+		&labelSpec,
+		ePrefix.XCtx(
+			"stdLine01<-labelSpec"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	_,
+		err = stdLine01.AddTextField(
+		&rightMarginSpec,
+		ePrefix.XCtx(
+			"stdLine01<-rightMarginSpec"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	err = stdLine01.IsValidInstanceError(
+		ePrefix.XCtx(
+			"stdLine01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	stdLine02 := TextLineSpecStandardLine{}
+
+	txtStdLineNanobot := textLineSpecStandardLineNanobot{}
+
+	stdLine02,
+		err =
+		txtStdLineNanobot.copyOut(
+			stdLine01,
+			ePrefix.XCtx(
+				"stdLine02<-stdLine01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	err = stdLine02.IsValidInstanceError(
+		ePrefix.XCtx(
+			"stdLine02"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	if !stdLine02.Equal(stdLine01) {
+		t.Errorf("%v\n"+
+			"Error: Expected stdLine02 == stdLine01\n"+
+			"HOWEVER, THEY ARE NOT EQUAL!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	_,
+		err =
+		txtStdLineNanobot.copyOut(
+			nil,
+			ePrefix.XCtx(
+				"txtStdLine==nil"))
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from txtStdLineNanobot{}."+
+			"copyOut()\n"+
+			"because 'txtStdLine' is 'nil'.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	stdLine01.numOfStdLines = -47
+	_,
+		err =
+		txtStdLineNanobot.copyOut(
+			stdLine01,
+			ePrefix.XCtx(
+				"stdLine01 is invalid"))
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from txtStdLineNanobot{}."+
+			"copyOut()\n"+
+			"because 'stdLine01.numOfStdLines' is invalid.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	stdLine01.numOfStdLines = 1
+	stdLine01.newLineChars = nil
+	// nil newLinChars defaults to '\n'
+
+	_,
+		err =
+		txtStdLineNanobot.copyOut(
+			stdLine01,
+			ePrefix.XCtx(
+				"stdLine01.newLineChars==nil"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	stdLine01.newLineChars = []rune{'\n', 0, '\n', 0}
+	_,
+		err =
+		txtStdLineNanobot.copyOut(
+			stdLine01,
+			ePrefix.XCtx(
+				"stdLine01 is invalid"))
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from txtStdLineNanobot{}."+
+			"copyOut()\n"+
+			"because 'stdLine01.newLineChars' contains invalid characters.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
 }
