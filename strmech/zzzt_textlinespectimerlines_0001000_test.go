7574ef405506c5c57dbb1ebb62f83ed56ed1244d
Mike Rapp <jordy.rascal@protonmail.com>
2022-04-09 00:02:03 -0500

    Added more tests TextLineSpecTimerLines.CopyIn()

--- strmech/zzzt_textlinespectimerlines_0001000_test.go
+++ strmech/zzzt_textlinespectimerlines_0001000_test.go
@@ -5,6 +5,134 @@ import (
 	"testing"
 )
 
+func TestTextLineSpecTimerLines_CopyIn_000100(t *testing.T) {
+
+	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
+		"TestTextLineSpecTimerLines_CopyIn_000100()",
+		"")
+
+	outputStr,
+		timerLines01,
+		err := createTestTextLineSpecTimerLines01(
+		ePrefix.XCtx(
+			"timerLines01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	var timerLines02 *TextLineSpecTimerLines
+
+	_,
+		timerLines02,
+		err = createTestTextLineSpecTimerLines02(
+		ePrefix.XCtx(
+			"timerLines02"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	err = timerLines02.CopyIn(
+		timerLines01,
+		ePrefix.XCtx(
+			"timerLines02<-timerLines01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	var actualStr string
+
+	actualStr,
+		err = timerLines02.GetFormattedText(
+		ePrefix.XCtx(
+			"timerLines02"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	sMech := StrMech{}
+
+	printableActualStr :=
+		sMech.ConvertNonPrintableChars(
+			[]rune(actualStr),
+			true)
+
+	if outputStr != printableActualStr {
+
+		t.Errorf("%v - ERROR\n"+
+			"timerLines02.GetFormattedText()"+
+			"Expected string DOES NOT match Actual string\n"+
+			"Expected string = '%v'\n"+
+			"  Actual string = '%v'\n",
+			ePrefix.XCtxEmpty().String(),
+			outputStr,
+			printableActualStr)
+
+		return
+
+	}
+
+	var timerLines03 *TextLineSpecTimerLines
+
+	_,
+		timerLines03,
+		err = createTestTextLineSpecTimerLines02(
+		ePrefix.XCtx(
+			"timerLines03"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	err = timerLines03.CopyIn(
+		nil,
+		ePrefix.XCtx(
+			"timerLines03<-nil"))
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from timerLines03."+
+			"CopyIn()\n"+
+			"because 'incomingTimerLines' is 'nil'.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	err = timerLines03.CopyIn(
+		timerLines02,
+		StrMech{})
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from timerLines03."+
+			"CopyIn()\n"+
+			"because 'errorPrefix' is invalid.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	return
+}
+
 func TestTextLineSpecTimerLines_GetFormattedText_000100(t *testing.T) {
 
 	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
--- strmech/zzzt_textlinespectimerlines_0050000_test.go
+++ strmech/zzzt_textlinespectimerlines_0050000_test.go
@@ -5,6 +5,102 @@ import (
 	"testing"
 )
 
+func TestTextLineSpecTimerLinesNanobot_copyIn_000100(t *testing.T) {
+
+	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
+		"TestTextLineSpecTimerLinesNanobot_copyIn_000100()",
+		"")
+
+	_,
+		timerLines01,
+		err := createTestTextLineSpecTimerLines02(
+		ePrefix.XCtx(
+			"timerLines01"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	var timerLines02 *TextLineSpecTimerLines
+
+	_,
+		timerLines02,
+		err = createTestTextLineSpecTimerLines02(
+		ePrefix.XCtx(
+			"timerLines02"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	txtTimerLinesNanobot := textLineSpecTimerLinesNanobot{}
+
+	err =
+		txtTimerLinesNanobot.copyIn(
+			timerLines02,
+			nil,
+			ePrefix.XCtx(
+				"timerLines02<-nil"))
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from txtTimerLinesNanobot."+
+			"copyIn()\n"+
+			"because 'incomingTimerLines' is 'nil'.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	err =
+		txtTimerLinesNanobot.copyIn(
+			nil,
+			timerLines01,
+			ePrefix.XCtx(
+				"nil<-timerLines01"))
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from txtTimerLinesNanobot."+
+			"copyIn()\n"+
+			"because 'targetTimerLines' is 'nil'.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	timerLines03 := TextLineSpecTimerLines{}
+
+	err =
+		txtTimerLinesNanobot.copyIn(
+			timerLines02,
+			&timerLines03,
+			ePrefix.XCtx(
+				"timerLines02<-timerLines03 (invalid)"))
+
+	if err == nil {
+
+		t.Errorf("%v - ERROR\n"+
+			"Expected an error return from txtTimerLinesNanobot."+
+			"copyIn()\n"+
+			"because 'incomingTimerLines' is invalid.\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.XCtxEmpty().String())
+
+		return
+	}
+
+	return
+}
+
 func TestTextLineSpecTimerLinesMolecule_getFormattedText_000100(t *testing.T) {
 
 	ePrefix := ePref.ErrPrefixDto{}.NewEPrefCtx(
