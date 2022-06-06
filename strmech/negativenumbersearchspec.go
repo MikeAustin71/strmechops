cfd4a94bac62f9dbd26d50ad86f5fb2db930018b
Mike Rapp <jordy.rascal@protonmail.com>
2022-06-05 23:48:02 -0500

    Added new method NegativeNumberSearchSpec.CopyIn().

--- strmech/negativenumbersearchspec.go
+++ strmech/negativenumbersearchspec.go
@@ -47,6 +47,130 @@ type NegativeNumberSearchSpec struct {
 	lock                         *sync.Mutex
 }
 
+// CopyIn - Copies the data fields from an incoming instance of
+// NegativeNumberSearchSpec ('incomingNegNumSearchSpec') to the
+// data fields of the current NegativeNumberSearchSpec instance
+// ('negNumSearchSpec').
+//
+// IMPORTANT
+// All the data fields in current NegativeNumberSearchSpec instance
+// ('negNumSearchSpec') will be modified and overwritten.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  incomingNegNumSearchSpec   *NegativeNumberSearchSpec
+//     - A pointer to an instance of NegativeNumberSearchSpec. This
+//       method will NOT change the values of internal member
+//       variables contained in this instance.
+//
+//       All data values in this NegativeNumberSearchSpec instance
+//       will be copied to current NegativeNumberSearchSpec
+//       instance ('negNumSearchSpec').
+//
+//       If parameter 'incomingNegNumSearchSpec' is determined to
+//       be invalid, an error will be returned.
+//
+//
+//  errorPrefix                interface{}
+//     - This object encapsulates error prefix text which is
+//       included in all returned error messages. Usually, it
+//       contains the name of the calling method or methods
+//       listed as a method or function chain of execution.
+//
+//       If no error prefix information is needed, set this parameter
+//       to 'nil'.
+//
+//       This empty interface must be convertible to one of the
+//       following types:
+//
+//
+//       1. nil - A nil value is valid and generates an empty
+//                collection of error prefix and error context
+//                information.
+//
+//       2. string - A string containing error prefix information.
+//
+//       3. []string A one-dimensional slice of strings containing
+//                   error prefix information
+//
+//       4. [][2]string A two-dimensional slice of strings containing
+//                      error prefix and error context information.
+//
+//       5. ErrPrefixDto - An instance of ErrPrefixDto. The
+//                         ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       6. *ErrPrefixDto - A pointer to an instance of ErrPrefixDto.
+//                          ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       7. IBasicErrorPrefix - An interface to a method generating
+//                              a two-dimensional slice of strings
+//                              containing error prefix and error
+//                              context information.
+//
+//       If parameter 'errorPrefix' is NOT convertible to one of
+//       the valid types listed above, it will be considered
+//       invalid and trigger the return of an error.
+//
+//       Types ErrPrefixDto and IBasicErrorPrefix are included in
+//       the 'errpref' software package, "github.com/MikeAustin71/errpref".
+//
+//
+// ------------------------------------------------------------------------
+//
+// Return Values
+//
+//  error
+//     - If this method completes successfully and no errors are
+//       encountered this return value is set to 'nil'. Otherwise,
+//       if errors are encountered, this return value will contain
+//       an appropriate error message.
+//
+//       If an error message is returned, the text value of input
+//       parameter 'errorPrefix' will be inserted or prefixed at
+//       the beginning of the error message.
+//
+func (negNumSearchSpec *NegativeNumberSearchSpec) CopyIn(
+	incomingNegNumSearchSpec *NegativeNumberSearchSpec,
+	errorPrefix interface{}) (
+	err error) {
+
+	if negNumSearchSpec.lock == nil {
+		negNumSearchSpec.lock = new(sync.Mutex)
+	}
+
+	negNumSearchSpec.lock.Lock()
+
+	defer negNumSearchSpec.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"NegativeNumberSearchSpec."+
+			"CopyOut()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	negNumSearchNanobot := negNumSignSearchNanobot{}
+
+	err = negNumSearchNanobot.copyIn(
+		negNumSearchSpec,
+		incomingNegNumSearchSpec,
+		ePrefix.XCpy(
+			"negNumSearchSpec<-incomingNegNumSearchSpec"))
+
+	return err
+}
+
 // CopyOut - Returns a deep copy of the current
 // NegativeNumberSearchSpec instance.
 //
