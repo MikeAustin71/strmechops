43cb45295f2792d1b2298e60bf6b52c236817c84
Mike Rapp <jordy.rascal@protonmail.com>
2022-07-03 02:00:20 -0500

    Added method CharSearchResultsDto.CopyIn().

--- strmech/charsearchresultsdto.go
+++ strmech/charsearchresultsdto.go
@@ -1,6 +1,7 @@
 package strmech
 
 import (
+	ePref "github.com/MikeAustin71/errpref"
 	"sync"
 )
 
@@ -281,6 +282,134 @@ type CharSearchResultsDto struct {
 	//  CharSearchType.LinearEndOfString()
 
 	lock *sync.Mutex
+}
+
+// CopyIn - Copies the data fields from an incoming instance of
+// CharSearchResultsDto ('incomingSearchResults') to the data
+// fields of the current CharSearchResultsDto instance
+// ('charSearchResults').
+//
+// ----------------------------------------------------------------
+//
+// IMPORTANT
+//
+// All the data fields in current CharSearchResultsDto instance
+// ('charSearchResults') will be deleted and overwritten.
+//
+// No Data Validation will be performed on 'incomingSearchResults'.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  incomingSearchResults     *CharSearchResultsDto
+//     - A pointer to an instance of CharSearchResultsDto. This
+//       method will NOT change the data values of member variables
+//       contained in this instance.
+//
+//       All data values in this CharSearchResultsDto instance
+//       ('incomingSearchResults') will be copied to the current
+//       CharSearchResultsDto instance ('charSearchResults').
+//
+//       No Data Validation will be performed on
+//       'incomingSearchResults'.
+//
+//
+//  errorPrefix                interface{}
+//     - This object encapsulates error prefix text which is
+//       included in all returned error messages. Usually, it
+//       contains the name of the calling method or methods
+//       listed as a method or function chain of execution.
+//
+//       If no error prefix information is needed, set this
+//       parameter to 'nil'.
+//
+//       This empty interface must be convertible to one of the
+//       following types:
+//
+//
+//       1. nil - A nil value is valid and generates an empty
+//                collection of error prefix and error context
+//                information.
+//
+//       2. string - A string containing error prefix information.
+//
+//       3. []string A one-dimensional slice of strings containing
+//                   error prefix information
+//
+//       4. [][2]string A two-dimensional slice of strings
+//          containing error prefix and error context information.
+//
+//       5. ErrPrefixDto - An instance of ErrPrefixDto. The
+//                         ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       6. *ErrPrefixDto - A pointer to an instance of
+//                          ErrPrefixDto. ErrorPrefixInfo from this
+//                          object will be copied to 'errPrefDto'.
+//
+//       7. IBasicErrorPrefix - An interface to a method generating
+//                              a two-dimensional slice of strings
+//                              containing error prefix and error
+//                              context information.
+//
+//       If parameter 'errorPrefix' is NOT convertible to one of
+//       the valid types listed above, it will be considered
+//       invalid and trigger the return of an error.
+//
+//       Types ErrPrefixDto and IBasicErrorPrefix are included in
+//       the 'errpref' software package,
+//       "github.com/MikeAustin71/errpref".
+//
+//
+// ----------------------------------------------------------------
+//
+// Return Values
+//
+//  error
+//     - If the method completes successfully and no errors are
+//       encountered this return value is set to 'nil'. Otherwise,
+//       if errors are encountered, this return value will contain
+//       an appropriate error message.
+//
+//       If an error message is returned, the text value of input
+//       parameter 'errorPrefix' will be inserted or prefixed at
+//       the beginning of the error message.
+//
+func (charSearchResults *CharSearchResultsDto) CopyIn(
+	incomingSearchResults *CharSearchResultsDto,
+	errorPrefix interface{}) error {
+
+	if charSearchResults.lock == nil {
+		charSearchResults.lock = new(sync.Mutex)
+	}
+
+	charSearchResults.lock.Lock()
+
+	defer charSearchResults.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"CharSearchResultsDto."+
+			"CopyIn()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	return charSearchResultsDtoNanobot{}.ptr().
+		copyIn(
+			charSearchResults,
+			incomingSearchResults,
+			ePrefix.XCpy(
+				"charSearchResults<-"+
+					"incomingSearchResults"))
 }
 
 func (charSearchResults *CharSearchResultsDto) Empty() {
