fa0260818fd26772624f32fe9ec777d79e07afff
Mike Rapp <jordy.rascal@protonmail.com>
2022-07-05 01:26:39 -0500

    Added new method charSearchTestConfigDtoNanobot.copyOut()

--- strmech/charsearchtestconfigdtonanobot.go
+++ strmech/charsearchtestconfigdtonanobot.go
@@ -183,6 +183,154 @@ func (searchTestConfigNanobot *charSearchTestConfigDtoNanobot) copyIn(
 	return err
 }
 
+// copyOut - Returns a deep copy of the input parameter
+// 'searchTestCfgDto', a pointer to an instance of
+// CharSearchTestConfigDto.
+//
+// ----------------------------------------------------------------
+//
+// IMPORTANT
+//
+//
+// NO validation is performed on 'searchTestCfgDto'.
+//
+// ------------------------------------------------------------------------
+//
+// Input Parameters
+//
+//  searchTestCfgDto                *CharSearchTestConfigDto
+//     - A pointer to an instance of CharSearchTestConfigDto. A
+//       deep copy of the internal member variables will be created
+//       and returned in a new instance of CharSearchTestConfigDto.
+//
+//       No data validation is performed on 'searchTestCfgDto'.
+//
+//
+//  errPrefDto                      *ePref.ErrPrefixDto
+//     - This object encapsulates an error prefix string which is
+//       included in all returned error messages. Usually, it
+//       contains the name of the calling method or methods listed
+//       as a function chain.
+//
+//       If no error prefix information is needed, set this parameter
+//       to 'nil'.
+//
+//       Type ErrPrefixDto is included in the 'errpref' software
+//       package, "github.com/MikeAustin71/errpref".
+//
+//
+// ------------------------------------------------------------------------
+//
+// Return Values
+//
+//  deepCopySearchTestCfgDto        CharSearchTestConfigDto
+//     - If this method completes successfully, a deep copy of
+//       input parameter 'searchTestCfgDto' will be created and
+//       returned in a new instance of CharSearchTestConfigDto.
+//
+//
+//  err                             error
+//     - If this method completes successfully, this returned error
+//       Type is set equal to 'nil'. If errors are encountered during
+//       processing, the returned error Type will encapsulate an error
+//       message.
+//
+//       If an error message is returned, the text value for input
+//       parameter 'errPrefDto' (error prefix) will be prefixed or
+//       attached at the beginning of the error message.
+//
+func (searchTestConfigNanobot *charSearchTestConfigDtoNanobot) copyOut(
+	searchTestCfgDto *CharSearchTestConfigDto,
+	errPrefDto *ePref.ErrPrefixDto) (
+	deepCopySearchTestCfgDto CharSearchTestConfigDto,
+	err error) {
+
+	if searchTestConfigNanobot.lock == nil {
+		searchTestConfigNanobot.lock = new(sync.Mutex)
+	}
+
+	searchTestConfigNanobot.lock.Lock()
+
+	defer searchTestConfigNanobot.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
+		errPrefDto,
+		"charSearchTargetInputParametersDtoNanobot."+
+			"copyOut()",
+		"")
+
+	if err != nil {
+
+		return deepCopySearchTestCfgDto, err
+
+	}
+
+	if searchTestCfgDto == nil {
+
+		err = fmt.Errorf("%v\n"+
+			"ERROR: Input parameter 'searchTestCfgDto' is a nil pointer!\n",
+			ePrefix.String())
+
+		return deepCopySearchTestCfgDto, err
+	}
+
+	charSearchTestConfigDtoAtom{}.ptr().empty(
+		&deepCopySearchTestCfgDto)
+
+	deepCopySearchTestCfgDto.TestInputParametersName =
+		searchTestCfgDto.TestInputParametersName
+
+	deepCopySearchTestCfgDto.TestStringName =
+		searchTestCfgDto.TestStringName
+
+	deepCopySearchTestCfgDto.TestStringLengthName =
+		searchTestCfgDto.TestStringLengthName
+
+	deepCopySearchTestCfgDto.TestStringStartingIndex =
+		searchTestCfgDto.TestStringStartingIndex
+
+	deepCopySearchTestCfgDto.TestStringStartingIndexName =
+		searchTestCfgDto.TestStringStartingIndexName
+
+	deepCopySearchTestCfgDto.TestStringDescription1 =
+		searchTestCfgDto.TestStringDescription1
+
+	deepCopySearchTestCfgDto.TestStringDescription2 =
+		searchTestCfgDto.TestStringDescription2
+
+	deepCopySearchTestCfgDto.CollectionTestObjIndex =
+		searchTestCfgDto.CollectionTestObjIndex
+
+	deepCopySearchTestCfgDto.NumValueType =
+		searchTestCfgDto.NumValueType
+
+	deepCopySearchTestCfgDto.NumStrFormatType =
+		searchTestCfgDto.NumStrFormatType
+
+	deepCopySearchTestCfgDto.NumSymbolLocation =
+		searchTestCfgDto.NumSymbolLocation
+
+	deepCopySearchTestCfgDto.NumSymbolClass =
+		searchTestCfgDto.NumSymbolClass
+
+	deepCopySearchTestCfgDto.NumSignValue =
+		searchTestCfgDto.NumSignValue
+
+	deepCopySearchTestCfgDto.PrimaryNumSignPosition =
+		searchTestCfgDto.PrimaryNumSignPosition
+
+	deepCopySearchTestCfgDto.SecondaryNumSignPosition =
+		searchTestCfgDto.SecondaryNumSignPosition
+
+	deepCopySearchTestCfgDto.TextCharSearchType =
+		searchTestCfgDto.TextCharSearchType
+
+	return deepCopySearchTestCfgDto, err
+}
+
 // ptr - Returns a pointer to a new instance of
 // charSearchTestConfigDtoNanobot.
 //
