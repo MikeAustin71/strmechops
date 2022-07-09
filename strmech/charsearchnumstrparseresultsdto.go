9b03164f0a0c40c7ef8d22bcab07aef4432097c3
Mike Rapp <jordy.rascal@protonmail.com>
2022-07-09 02:09:54 -0500

    Added new method CharSearchNumStrParseResultsDto.CopyOut()

--- strmech/charsearchnumstrparseresultsdto.go
+++ strmech/charsearchnumstrparseresultsdto.go
@@ -64,7 +64,7 @@ import (
 //
 // The Character Search Number String Results Data Transfer
 // type (CharSearchNumStrParseResultsDto) is used to
-// identify, sumarize, and transmit the results or outcomes of a
+// identify, summarize, and transmit the results or outcomes of a
 // search operation for numeric digit characters within a number
 // string.
 //
@@ -288,4 +288,120 @@ func (searchNumStrParseResults *CharSearchNumStrParseResultsDto) CopyIn(
 			ePrefix.XCpy(
 				"searchNumStrParseResults<-"+
 					"incomingNumStrParseResults"))
+}
+
+// CopyOut - Returns a deep copy of the current
+// CharSearchNumStrParseResultsDto instance.
+//
+// ----------------------------------------------------------------
+//
+// IMPORTANT
+//
+// No Data Validation will be performed on the current instance
+// of CharSearchNumStrParseResultsDto.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  errorPrefix                interface{}
+//     - This object encapsulates error prefix text which is
+//       included in all returned error messages. Usually, it
+//       contains the name of the calling method or methods
+//       listed as a method or function chain of execution.
+//
+//       If no error prefix information is needed, set this
+//       parameter to 'nil'.
+//
+//       This empty interface must be convertible to one of the
+//       following types:
+//
+//
+//       1. nil - A nil value is valid and generates an empty
+//                collection of error prefix and error context
+//                information.
+//
+//       2. string - A string containing error prefix information.
+//
+//       3. []string A one-dimensional slice of strings containing
+//                   error prefix information
+//
+//       4. [][2]string A two-dimensional slice of strings
+//          containing error prefix and error context information.
+//
+//       5. ErrPrefixDto - An instance of ErrPrefixDto. The
+//                         ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       6. *ErrPrefixDto - A pointer to an instance of
+//                          ErrPrefixDto. ErrorPrefixInfo from this
+//                          object will be copied to 'errPrefDto'.
+//
+//       7. IBasicErrorPrefix - An interface to a method generating
+//                              a two-dimensional slice of strings
+//                              containing error prefix and error
+//                              context information.
+//
+//       If parameter 'errorPrefix' is NOT convertible to one of
+//       the valid types listed above, it will be considered
+//       invalid and trigger the return of an error.
+//
+//       Types ErrPrefixDto and IBasicErrorPrefix are included in
+//       the 'errpref' software package,
+//       "github.com/MikeAustin71/errpref".
+//
+//
+// ----------------------------------------------------------------
+//
+// Return Values
+//
+//  CharSearchNumStrParseResultsDto
+//     - If this method completes successfully and no errors are
+//       encountered, this parameter will return a deep copy of the
+//       current CharSearchNumStrParseResultsDto instance.
+//
+//
+//  error
+//     - If the method completes successfully and no errors are
+//       encountered this return value is set to 'nil'. Otherwise,
+//       if errors are encountered, this return value will contain
+//       an appropriate error message.
+//
+//       If an error message is returned, the text value of input
+//       parameter 'errorPrefix' will be inserted or prefixed at
+//       the beginning of the error message.
+//
+func (searchNumStrParseResults *CharSearchNumStrParseResultsDto) CopyOut(
+	errorPrefix interface{}) (
+	CharSearchNumStrParseResultsDto,
+	error) {
+
+	if searchNumStrParseResults.lock == nil {
+		searchNumStrParseResults.lock = new(sync.Mutex)
+	}
+
+	searchNumStrParseResults.lock.Lock()
+
+	defer searchNumStrParseResults.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"CharSearchNumStrParseResultsDto."+
+			"CopyOut()",
+		"")
+
+	if err != nil {
+		return CharSearchNumStrParseResultsDto{}, err
+	}
+
+	return charSearchNumStrParseResultsDtoNanobot{}.ptr().
+		copyOut(
+			searchNumStrParseResults,
+			ePrefix.XCpy(
+				"<-searchNumStrParseResults"))
 }
