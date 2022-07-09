af9e3e707deb6c9023b9d5ab17c725725cf01cc1
Mike Rapp <jordy.rascal@protonmail.com>
2022-07-08 21:38:32 -0500

    Added new method CharSearchDecimalSeparatorResultsDto.GetParameterTextListing()

--- strmech/charsearchdecimalseparatorresultsdto.go
+++ strmech/charsearchdecimalseparatorresultsdto.go
@@ -2,6 +2,7 @@ package strmech
 
 import (
 	ePref "github.com/MikeAustin71/errpref"
+	"strings"
 	"sync"
 )
 
@@ -619,6 +620,121 @@ func (decSepSearchResultsDto *CharSearchDecimalSeparatorResultsDto) Equal(
 		equal(
 			decSepSearchResultsDto,
 			incomingDecSepResults)
+}
+
+// GetParameterTextListing - Returns formatted text output
+// detailing the member variable names and their corresponding
+// values contained in the current instance of
+// CharSearchDecimalSeparatorResultsDto ('decSepSearchResultsDto').
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  errorPrefix                interface{}
+//     - This object encapsulates error prefix text which is
+//       included in all returned error messages. Usually, it
+//       contains the name of the calling method or methods
+//       listed as a method or function chain of execution.
+//
+//       If no error prefix information is needed, set this
+//       parameter to 'nil'.
+//
+//       This empty interface must be convertible to one of the
+//       following types:
+//
+//
+//       1. nil - A nil value is valid and generates an empty
+//                collection of error prefix and error context
+//                information.
+//
+//       2. string - A string containing error prefix information.
+//
+//       3. []string A one-dimensional slice of strings containing
+//                   error prefix information
+//
+//       4. [][2]string A two-dimensional slice of strings
+//          containing error prefix and error context information.
+//
+//       5. ErrPrefixDto - An instance of ErrPrefixDto. The
+//                         ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       6. *ErrPrefixDto - A pointer to an instance of
+//                          ErrPrefixDto. ErrorPrefixInfo from this
+//                          object will be copied to 'errPrefDto'.
+//
+//       7. IBasicErrorPrefix - An interface to a method generating
+//                              a two-dimensional slice of strings
+//                              containing error prefix and error
+//                              context information.
+//
+//       If parameter 'errorPrefix' is NOT convertible to one of
+//       the valid types listed above, it will be considered
+//       invalid and trigger the return of an error.
+//
+//       Types ErrPrefixDto and IBasicErrorPrefix are included in
+//       the 'errpref' software package,
+//       "github.com/MikeAustin71/errpref".
+//
+//
+// ----------------------------------------------------------------
+//
+// Return Values
+//
+//  strings.Builder
+//     - If this method completes successfully, an instance of
+//       strings.Builder will be returned. This instance contains
+//       the formatted text output listing the member variable
+//       names and their corresponding values for the current
+//       instance of CharSearchDecimalSeparatorResultsDto. This
+//       formatted text can then be used for text displays, file
+//       output or printing.
+//
+//
+//  error
+//     - If this method completes successfully, this returned error
+//       Type is set equal to 'nil'. If errors are encountered during
+//       processing, the returned error Type will encapsulate an error
+//       message.
+//
+//       If an error message is returned, the text value for input
+//       parameter 'errPrefDto' (error prefix) will be prefixed or
+//       attached at the beginning of the error message.
+//
+func (decSepSearchResultsDto *CharSearchDecimalSeparatorResultsDto) GetParameterTextListing(
+	errorPrefix interface{}) (
+	strings.Builder,
+	error) {
+
+	if decSepSearchResultsDto.lock == nil {
+		decSepSearchResultsDto.lock = new(sync.Mutex)
+	}
+
+	decSepSearchResultsDto.lock.Lock()
+
+	defer decSepSearchResultsDto.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"CharSearchDecimalSeparatorResultsDto."+
+			"CopyIn()",
+		"")
+
+	if err != nil {
+		return strings.Builder{}, err
+	}
+
+	return charSearchDecimalSeparatorResultsDtoNanobot{}.ptr().
+		getParameterTextListing(
+			decSepSearchResultsDto,
+			ePrefix.XCpy(
+				"decSepSearchResultsDto"))
 }
 
 // LoadTargetBaseInputParameters - Receives Target String data from
