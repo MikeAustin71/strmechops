f98c89a324b3e91492f10341c5f80e3d4cefb17d
Mike Rapp <jordy.rascal@protonmail.com>
2022-07-08 19:00:33 -0500

    Added new method CharSearchDecimalSeparatorResultsDto.CopyIn()

--- strmech/charsearchdecimalseparatorresultsdto.go
+++ strmech/charsearchdecimalseparatorresultsdto.go
@@ -1,6 +1,9 @@
 package strmech
 
-import "sync"
+import (
+	ePref "github.com/MikeAustin71/errpref"
+	"sync"
+)
 
 // CharSearchDecimalSeparatorResultsDto  - Contains parameters
 // detailing the results of a text character search for decimal
@@ -264,6 +267,138 @@ type CharSearchDecimalSeparatorResultsDto struct {
 	// of DecimalSeparatorSpec
 
 	lock *sync.Mutex
+}
+
+// CopyIn - Copies the data fields from an incoming instance of
+// CharSearchDecimalSeparatorResultsDto ('incomingDecSepResults')
+// to the data fields of the current
+// CharSearchDecimalSeparatorResultsDto instance
+// ('decSepSearchResultsDto').
+//
+// ----------------------------------------------------------------
+//
+// IMPORTANT
+//
+// All the data fields in the current
+// CharSearchDecimalSeparatorResultsDto instance
+// ('decSepSearchResultsDto') will be deleted and overwritten.
+//
+// No Data Validation will be performed on 'incomingDecSepResults'.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  incomingDecSepResults   *CharSearchDecimalSeparatorResultsDto
+//     - A pointer to an instance of
+//       CharSearchDecimalSeparatorResultsDto. This method will NOT
+//       change the data values of member variables contained in
+//       this instance.
+//
+//       All data values in this CharSearchDecimalSeparatorResultsDto
+//       instance ('incomingDecSepResults') will be copied to
+//       the current CharSearchDecimalSeparatorResultsDto instance
+//       ('decSepSearchResultsDto').
+//
+//       No Data Validation will be performed on
+//       'incomingDecSepResults'.
+//
+//
+//  errorPrefix                interface{}
+//     - This object encapsulates error prefix text which is
+//       included in all returned error messages. Usually, it
+//       contains the name of the calling method or methods
+//       listed as a method or function chain of execution.
+//
+//       If no error prefix information is needed, set this
+//       parameter to 'nil'.
+//
+//       This empty interface must be convertible to one of the
+//       following types:
+//
+//
+//       1. nil - A nil value is valid and generates an empty
+//                collection of error prefix and error context
+//                information.
+//
+//       2. string - A string containing error prefix information.
+//
+//       3. []string A one-dimensional slice of strings containing
+//                   error prefix information
+//
+//       4. [][2]string A two-dimensional slice of strings
+//          containing error prefix and error context information.
+//
+//       5. ErrPrefixDto - An instance of ErrPrefixDto. The
+//                         ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       6. *ErrPrefixDto - A pointer to an instance of
+//                          ErrPrefixDto. ErrorPrefixInfo from this
+//                          object will be copied to 'errPrefDto'.
+//
+//       7. IBasicErrorPrefix - An interface to a method generating
+//                              a two-dimensional slice of strings
+//                              containing error prefix and error
+//                              context information.
+//
+//       If parameter 'errorPrefix' is NOT convertible to one of
+//       the valid types listed above, it will be considered
+//       invalid and trigger the return of an error.
+//
+//       Types ErrPrefixDto and IBasicErrorPrefix are included in
+//       the 'errpref' software package,
+//       "github.com/MikeAustin71/errpref".
+//
+//
+// ----------------------------------------------------------------
+//
+// Return Values
+//
+//  error
+//     - If the method completes successfully and no errors are
+//       encountered this return value is set to 'nil'. Otherwise,
+//       if errors are encountered, this return value will contain
+//       an appropriate error message.
+//
+//       If an error message is returned, the text value of input
+//       parameter 'errorPrefix' will be inserted or prefixed at
+//       the beginning of the error message.
+//
+func (decSepSearchResultsDto *CharSearchDecimalSeparatorResultsDto) CopyIn(
+	incomingDecSepResults *CharSearchDecimalSeparatorResultsDto,
+	errorPrefix interface{}) error {
+
+	if decSepSearchResultsDto.lock == nil {
+		decSepSearchResultsDto.lock = new(sync.Mutex)
+	}
+
+	decSepSearchResultsDto.lock.Lock()
+
+	defer decSepSearchResultsDto.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"CharSearchDecimalSeparatorResultsDto."+
+			"CopyIn()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	return charSearchDecimalSeparatorResultsDtoNanobot{}.ptr().
+		copyIn(
+			decSepSearchResultsDto,
+			incomingDecSepResults,
+			ePrefix.XCpy(
+				"decSepSearchResultsDto<-"+
+					"incomingDecSepResults"))
 }
 
 // Empty - Resets all internal member variables for the current
