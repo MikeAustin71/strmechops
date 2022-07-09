51ca74adb014445009a5d965c211c4f388b84eef
Mike Rapp <jordy.rascal@protonmail.com>
2022-07-08 23:02:23 -0500

    Added new method charSearchNumStrParseResultsDtoAtom.equal()

--- strmech/charsearchnumstrparseresultsdtoatom.go
+++ strmech/charsearchnumstrparseresultsdtoatom.go
@@ -83,6 +83,135 @@ func (searchNumStrParseResultsAtom *charSearchNumStrParseResultsDtoAtom) empty(
 	return
 }
 
+// equal - Receives a pointer to two instances of
+// CharSearchNumStrParseResultsDto and proceeds to compare
+// their member variables in order to determine if they are
+// equivalent.
+//
+// A boolean flag showing the result of this comparison is
+// returned. If the member variables for both instances are equal
+// in all respects, this flag is set to 'true'. Otherwise, this
+// method returns 'false'.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  numStrParseResults1    *CharSearchNumStrParseResultsDto
+//     - An instance of CharSearchNumStrParseResultsDto.
+//       Internal member variables from 'numStrParseResults1'
+//       will be compared to those of 'numStrParseResults2' to
+//       determine if both instances are equivalent.
+//
+//
+//  numStrParseResults2    *CharSearchNumStrParseResultsDto
+//     - An instance of CharSearchNumStrParseResultsDto.
+//       Internal member variables from 'numStrParseResults2'
+//       will be compared to those of 'numStrParseResults1' to
+//       determine if both instances are equivalent.
+//
+//
+// ----------------------------------------------------------------
+//
+// Return Values
+//
+//  bool
+//     - If the comparison of 'numStrParseResults1' and
+//       'numStrParseResults2' shows that all internal member
+//       variables are equivalent, this method will return a
+//       boolean value of 'true'.
+//
+//       If the two instances are NOT equal, this method will
+//       return a boolean value of 'false' to the calling function.
+//
+func (searchNumStrParseResultsAtom *charSearchNumStrParseResultsDtoAtom) equal(
+	numStrParseResults1 *CharSearchNumStrParseResultsDto,
+	numStrParseResults2 *CharSearchNumStrParseResultsDto) bool {
+
+	if searchNumStrParseResultsAtom.lock == nil {
+		searchNumStrParseResultsAtom.lock = new(sync.Mutex)
+	}
+
+	searchNumStrParseResultsAtom.lock.Lock()
+
+	defer searchNumStrParseResultsAtom.lock.Unlock()
+
+	if numStrParseResults1.SearchResultsName !=
+		numStrParseResults2.SearchResultsName {
+
+		return false
+	}
+
+	if numStrParseResults1.SearchResultsFunctionChain !=
+		numStrParseResults2.SearchResultsFunctionChain {
+
+		return false
+	}
+
+	if numStrParseResults1.FoundNumericDigits !=
+		numStrParseResults2.FoundNumericDigits {
+
+		return false
+	}
+
+	if numStrParseResults1.FoundNonZeroValue !=
+		numStrParseResults2.FoundNonZeroValue {
+
+		return false
+	}
+
+	if numStrParseResults1.FoundDecimalSeparatorSymbols !=
+		numStrParseResults2.FoundDecimalSeparatorSymbols {
+
+		return false
+	}
+
+	if numStrParseResults1.FoundDecimalDigits !=
+		numStrParseResults2.FoundDecimalDigits {
+
+		return false
+	}
+
+	if numStrParseResults1.NumSignValue !=
+		numStrParseResults2.NumSignValue {
+
+		return false
+	}
+
+	if numStrParseResults1.NumValueType !=
+		numStrParseResults2.NumValueType {
+
+		return false
+	}
+
+	if !numStrParseResults1.RemainderString.Equal(
+		&numStrParseResults2.RemainderString) {
+
+		return false
+	}
+
+	if !numStrParseResults1.DecimalSeparatorSearchResults.Equal(
+		&numStrParseResults2.DecimalSeparatorSearchResults) {
+
+		return false
+	}
+
+	if !numStrParseResults1.NegativeNumberSymbolSearchResults.Equal(
+		&numStrParseResults1.NegativeNumberSymbolSearchResults) {
+
+		return false
+	}
+
+	if !numStrParseResults1.ParsingTerminatorSearchResults.Equal(
+		&numStrParseResults1.ParsingTerminatorSearchResults) {
+
+		return false
+	}
+
+	return true
+}
+
 // ptr - Returns a pointer to a new instance of
 // charSearchNumStrParseResultsDtoAtom.
 //
