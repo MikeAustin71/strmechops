536339ccf0da8ae65b14395a2ad6dca0be484d94
Mike Rapp <jordy.rascal@protonmail.com>
2022-07-11 03:53:26 -0500

    Added new method RuneArrayCollection.DeleteCollectionElement()

--- strmech/runearraycollection.go
+++ strmech/runearraycollection.go
@@ -1806,6 +1806,131 @@ func (runeArrayCol *RuneArrayCollection) CopyOut(
 				"<-runeArrayCol"))
 }
 
+// DeleteCollectionElement - Deletes a member element of the Rune
+// Arrays Collection maintained by the current instance of
+// RuneArrayCollection. The element to be deleted is specified by
+// input parameter 'zeroBasedIndex' and represents the array index
+// for the element to be deleted.
+//
+// If the current Rune Array Collection is empty and has zero
+// elements, an error will be returned.
+//
+// ----------------------------------------------------------------
+//
+// IMPORTANT
+//
+// Be advised, this method WILL DELETE DATA!
+//
+// The Rune Arrays Collection member element specified by input
+// parameter 'zeroBasedIndex' WILL BE DELETED.
+//
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//  zeroBasedIndex             int
+//     - Specifies the index of the member element in the Rune
+//       Arrays Collection which will be deleted.
+//
+//
+//  errorPrefix                interface{}
+//     - This object encapsulates error prefix text which is
+//       included in all returned error messages. Usually, it
+//       contains the name of the calling method or methods
+//       listed as a method or function chain of execution.
+//
+//       If no error prefix information is needed, set this
+//       parameter to 'nil'.
+//
+//       This empty interface must be convertible to one of the
+//       following types:
+//
+//
+//       1. nil - A nil value is valid and generates an empty
+//                collection of error prefix and error context
+//                information.
+//
+//       2. string - A string containing error prefix information.
+//
+//       3. []string A one-dimensional slice of strings containing
+//                   error prefix information
+//
+//       4. [][2]string A two-dimensional slice of strings
+//          containing error prefix and error context information.
+//
+//       5. ErrPrefixDto - An instance of ErrPrefixDto. The
+//                         ErrorPrefixInfo from this object will be
+//                         copied to 'errPrefDto'.
+//
+//       6. *ErrPrefixDto - A pointer to an instance of
+//                          ErrPrefixDto. ErrorPrefixInfo from this
+//                          object will be copied to 'errPrefDto'.
+//
+//       7. IBasicErrorPrefix - An interface to a method generating
+//                              a two-dimensional slice of strings
+//                              containing error prefix and error
+//                              context information.
+//
+//       If parameter 'errorPrefix' is NOT convertible to one of
+//       the valid types listed above, it will be considered
+//       invalid and trigger the return of an error.
+//
+//       Types ErrPrefixDto and IBasicErrorPrefix are included in
+//       the 'errpref' software package,
+//       "github.com/MikeAustin71/errpref".
+//
+//
+// ----------------------------------------------------------------
+//
+// Return Values
+//
+//  err                        error
+//     - If this method completes successfully and no errors are
+//       encountered this return value is set to 'nil'. Otherwise,
+//       if errors are encountered, this return value will contain
+//       an appropriate error message.
+//
+//       If an error message is returned, the text value of input
+//       parameter 'errorPrefix' will be inserted or prefixed at
+//       the beginning of the error message.
+//
+func (runeArrayCol *RuneArrayCollection) DeleteCollectionElement(
+	zeroBasedIndex int,
+	errorPrefix interface{}) (
+	err error) {
+
+	if runeArrayCol.lock == nil {
+		runeArrayCol.lock = new(sync.Mutex)
+	}
+
+	runeArrayCol.lock.Lock()
+
+	defer runeArrayCol.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"RuneArrayCollection."+
+			"PopIndexElement()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	return runeArrayCollectionQuark{}.ptr().
+		deleteCollectionElement(
+			runeArrayCol,
+			zeroBasedIndex,
+			ePrefix.XCpy(
+				fmt.Sprintf(
+					"runeArrayCol.runeArrayDtoCol[%v]",
+					zeroBasedIndex)))
+}
+
 // Empty - Empties the internal RuneArrayDto collection and sets
 // its value to 'nil'. This method will leave the current
 // instance of RuneArrayCollection in an invalid state and
