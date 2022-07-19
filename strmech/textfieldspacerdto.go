98c58d10b53b3a5f1426d16309fde5e74458ab0b
Mike Rapp <jordy.rascal@protonmail.com>
2022-07-19 03:31:38 -0500

    Added member variables for type TextFieldSpacerDto.

--- strmech/textfieldspacerdto.go
+++ strmech/textfieldspacerdto.go
@@ -1,10 +1,20 @@
 package strmech
 
+import "sync"
+
 // TextFieldSpacerDto - This type is used to transmit parameters
 // required for the creation of Text Spacer Fields. Text Spacer
 // Fields consist of one or more white space characters (" ").
 //
 type TextFieldSpacerDto struct {
+	FormatType TextFieldType
+	// Required. This enumeration value specifies the type of Text
+	// Format Operation to be performed.
+	//
+	// For the TextFieldSpacerDto Format Type, this value
+	// should always be set to:
+	//   TxtFieldType.Spacer()       - Valid
+
 	LeftMarginStr string
 	// The contents of the string will be used as the left margin
 	// for the Text Field.
@@ -41,4 +51,34 @@ type TextFieldSpacerDto struct {
 	// a zero length or empty string and no line termination
 	// characters will be created.
 
+	MaxLineLength int
+	// The maximum length of the line on which this label text
+	// will be presented.
+	//
+	// Set this parameter to minus one (-1) to specify an unlimited
+	// line length for this text line.
+	//
+	// 'MaxLineLength' is used in conjunction with parameter
+	// 'TurnAutoLineLengthBreaksOn' to automatically place text
+	// fields on separate text lines when that text exceeds the
+	// maximum text line length ('MaxLineLength'). Therefore,
+	// paramter 'turnAutoLineLengthBreaksOn' controls whether
+	// automatic line breaks using 'MaxLineLength' will be
+	// applied.
+	//
+	// If the value of 'maxLineLength' is less than one (1), it
+	// will be automatically converted to minus one (-1).
+	//
+	// Set this parameter to minus one (-1) to specify an unlimited
+	// line length for this text line.
+
+	TurnAutoLineLengthBreaksOn bool
+	// This parameter controls whether text lines which exceed the
+	// maximum line length ('MaxLineLength') are broken up and
+	// presented on the following line.
+	//
+	// To apply automatic line breaking at the maximum line length,
+	// set the value of this parameter to 'true'.
+
+	lock *sync.Mutex
 }
