72f7568a371270489ff5ee414bdb2483d181d171
Mike Rapp <jordy.rascal@protonmail.com>
2022-08-15 18:27:37 -0500

    Added new method signedNumberFormatSpecAtom.equal()

--- strmech/numstrroundingspec.go
+++ strmech/numstrroundingspec.go
@@ -1,13 +1,15 @@
 package strmech
 
+import "sync"
+
 type NumStrRoundingSpec struct {
-	RoundFractionalDigits bool
+	roundFractionalDigits bool
 	// When set to 'true' this signals that rounding should
 	// be performed on fractional numbers. When set to 'false'
 	// this parameter signals that no rounding should be
 	// performed.
 
-	RoundingType NumberRoundingType
+	roundingType NumberRoundingType
 	// This enumeration parameter is used to specify the type
 	// of rounding algorithm that will be applied in the
 	// rounding operation.
@@ -25,11 +27,64 @@ type NumStrRoundingSpec struct {
 	//	NumRoundType.Ceiling(),
 	//	NumRoundType.Truncate(),
 
-	RoundToFractionalDigits int
+	roundToFractionalDigits int
 	// When set to a positive integer value, this
 	// parameter controls the number of digits to
 	// the right of the decimal separator (a.k.a.
 	// decimal point) which will remain after
 	// completion of the number rounding operation.
 
+	lock *sync.Mutex
+}
+
+func (nStrRoundingSpec *NumStrRoundingSpec) Empty() {
+
+	if nStrRoundingSpec.lock == nil {
+		nStrRoundingSpec.lock = new(sync.Mutex)
+	}
+
+	nStrRoundingSpec.lock.Lock()
+
+	defer nStrRoundingSpec.lock.Unlock()
+
+	nStrRoundingSpec.roundFractionalDigits = false
+
+	nStrRoundingSpec.roundingType =
+		NumRoundType.None()
+
+	nStrRoundingSpec.roundToFractionalDigits = -1
+
+	return
+}
+
+func (nStrRoundingSpec *NumStrRoundingSpec) Equal(
+	incomingNStrRoundingSpec *NumStrRoundingSpec) bool {
+
+	if nStrRoundingSpec.lock == nil {
+		nStrRoundingSpec.lock = new(sync.Mutex)
+	}
+
+	nStrRoundingSpec.lock.Lock()
+
+	defer nStrRoundingSpec.lock.Unlock()
+
+	if nStrRoundingSpec.roundFractionalDigits !=
+		incomingNStrRoundingSpec.roundFractionalDigits {
+
+		return false
+	}
+
+	if nStrRoundingSpec.roundingType !=
+		incomingNStrRoundingSpec.roundingType {
+
+		return false
+	}
+
+	if nStrRoundingSpec.roundToFractionalDigits !=
+		incomingNStrRoundingSpec.roundToFractionalDigits {
+
+		return false
+	}
+
+	return true
 }
--- strmech/signnumberformatspec.go
+++ strmech/signnumberformatspec.go
@@ -46,3 +46,160 @@ type SignedNumberFormatSpec struct {
 
 	lock *sync.Mutex
 }
+
+// signedNumberFormatSpecAtom - This type provides
+// helper methods for SignedNumberFormatSpec
+type signedNumberFormatSpecAtom struct {
+	lock *sync.Mutex
+}
+
+// empty - Receives a pointer to an instance of
+// SignedNumberFormatSpec and proceeds to reset the
+// data values for all member variables to their
+// initial or zero values.
+//
+// ----------------------------------------------------------------
+//
+// # IMPORTANT
+//
+// All the member variable data values contained in input parameter
+// 'SignedNumFmtSpec' will be deleted and reset to their zero values.
+//
+// ------------------------------------------------------------------------
+//
+// Input Parameters
+//
+//	SignedNumFmtSpec           *SignedNumberFormatSpec
+//	   - A pointer to an instance of SignedNumberFormatSpec.
+//	     All the internal member variables contained in this
+//	     instance will be deleted and reset to their zero values.
+//
+// ------------------------------------------------------------------------
+//
+// Return Values
+//
+//	NONE
+func (signedNumFmtSpecAtom *signedNumberFormatSpecAtom) empty(
+	signedNumFmtSpec *SignedNumberFormatSpec) {
+
+	if signedNumFmtSpecAtom.lock == nil {
+		signedNumFmtSpecAtom.lock = new(sync.Mutex)
+	}
+
+	signedNumFmtSpecAtom.lock.Lock()
+
+	defer signedNumFmtSpecAtom.lock.Unlock()
+
+	if signedNumFmtSpec == nil {
+		return
+	}
+
+	signedNumFmtSpec.decSeparator.Empty()
+
+	signedNumFmtSpec.intGroupingType.Empty()
+
+	signedNumFmtSpec.roundingSpec.Empty()
+
+	signedNumFmtSpec.positiveNumberSign.Empty()
+
+	signedNumFmtSpec.negativeNumberSign.Empty()
+
+	signedNumFmtSpec.numberField.Empty()
+
+}
+
+// equal - Receives a pointer to two instances of
+// SignedNumberFormatSpec and proceeds to compare their
+// member variables in order to determine if they are
+// equivalent.
+//
+// A boolean flag showing the result of this comparison is
+// returned. If the member variables for both instances are
+// equal in all respects, this flag is set to 'true'. Otherwise,
+// this method returns 'false'.
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//	signedNumFmtSpec1    *SignedNumberFormatSpec
+//	   - An instance of SignedNumberFormatSpec.
+//	     Internal member variables from 'signedNumFmtSpec1'
+//	     will be compared to those of 'signedNumFmtSpec2' to
+//	     determine if both instances are equivalent.
+//
+//
+//	signedNumFmtSpec2    *SignedNumberFormatSpec
+//	   - An instance of SignedNumberFormatSpec.
+//	     Internal member variables from 'signedNumFmtSpec2'
+//	     will be compared to those of 'signedNumFmtSpec1' to
+//	     determine if both instances are equivalent.
+//
+// ----------------------------------------------------------------
+//
+// Return Values
+//
+//	bool
+//	   - If the comparison of 'signedNumFmtSpec1' and
+//	     'signedNumFmtSpec2' shows that all internal member
+//	     variables are equivalent, this method will return a
+//	     boolean value of 'true'.
+//
+//	     If the two instances are NOT equal, this method will
+//	     return a boolean value of 'false' to the calling
+//	     function.
+func (signedNumFmtSpecAtom *signedNumberFormatSpecAtom) equal(
+	signedNumFmtSpec1 *SignedNumberFormatSpec,
+	signedNumFmtSpec2 *SignedNumberFormatSpec) bool {
+
+	if signedNumFmtSpecAtom.lock == nil {
+		signedNumFmtSpecAtom.lock = new(sync.Mutex)
+	}
+
+	signedNumFmtSpecAtom.lock.Lock()
+
+	defer signedNumFmtSpecAtom.lock.Unlock()
+
+	if signedNumFmtSpec1 == nil ||
+		signedNumFmtSpec2 == nil {
+		return false
+	}
+
+	if !signedNumFmtSpec1.decSeparator.Equal(
+		&signedNumFmtSpec2.decSeparator) {
+
+		return false
+	}
+
+	if !signedNumFmtSpec1.intGroupingType.Equal(
+		&signedNumFmtSpec2.intGroupingType) {
+
+		return false
+	}
+
+	if !signedNumFmtSpec1.roundingSpec.Equal(
+		&signedNumFmtSpec2.roundingSpec) {
+
+		return false
+	}
+
+	if !signedNumFmtSpec1.positiveNumberSign.Equal(
+		&signedNumFmtSpec2.positiveNumberSign) {
+
+		return false
+	}
+
+	if !signedNumFmtSpec1.negativeNumberSign.Equal(
+		&signedNumFmtSpec2.negativeNumberSign) {
+
+		return false
+	}
+
+	if !signedNumFmtSpec1.numberField.Equal(
+		&signedNumFmtSpec2.numberField) {
+
+		return false
+	}
+
+	return true
+}
