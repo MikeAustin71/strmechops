39a78053cc98d564171d4224baa73e662d85acf6
Mike Rapp <jordy.rascal@protonmail.com>
2022-08-22 17:24:44 -0500

    Added new method SignedNumberFormatSpec.GetNumberFieldSpec()

--- strmech/signnumberformatspec.go
+++ strmech/signnumberformatspec.go
@@ -1001,6 +1001,126 @@ func (signedNumFmtSpec *SignedNumberFormatSpec) GetNegativeNumSymSpec(
 			"<-signedNumFmtSpec.negativeNumberSign"))
 }
 
+// GetNumberFieldSpec - Returns the Number Field Specification
+// currently configured for this instance of
+// SignedNumberFormatSpec.
+//
+// The Number Field Specification includes parameters for
+// field length and text justification ('Right, Center,
+// Left').
+//
+// Numeric digits are formatted using the Number Field
+// Specification within a number field specified by field
+// length. The numeric digits string is then justified
+// within the number field according to a text justification
+// specification of 'Right', 'Center' or 'Left'.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	 errorPrefix                interface{}
+//
+//		This object encapsulates error prefix text which is
+//		included in all returned error messages. Usually, it
+//		contains the name of the calling method or methods
+//		listed as a method or function chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1. nil - A nil value is valid and generates an empty
+//		   collection of error prefix and error context
+//		   information.
+//
+//		2. string - A string containing error prefix
+//			information.
+//
+//		3. []string A one-dimensional slice of strings
+//			containing error prefix information.
+//
+//		4. [][2]string A two-dimensional slice of strings
+//		   containing error prefix and error context
+//		   information.
+//
+//		5. ErrPrefixDto - An instance of ErrPrefixDto.
+//			Information from this object will be copied for use
+//			in error and informational messages.
+//
+//		6. *ErrPrefixDto - A pointer to an instance of
+//			ErrPrefixDto. Information from this object will be
+//			copied for use in error and informational messages.
+//
+//		7. IBasicErrorPrefix - An interface to a method
+//			generating a two-dimensional slice of strings
+//			containing error prefix and error context
+//			information.
+//
+//		If parameter 'errorPrefix' is NOT convertible to one
+//		of the valid types listed above, it will be
+//		considered invalid and trigger the return of an
+//		error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are included
+//		in the 'errpref' software package,
+//		"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	NumStrNumberFieldSpec
+//
+//		If this method completes successfully, a copy
+//		of the Number Field Specification configured
+//		for the current SignedNumberFormatSpec instance
+//		will be returned.
+//
+//	error
+//
+//		If this method completes successfully and no errors are
+//		encountered this return value is set to 'nil'. Otherwise,
+//		if errors are encountered, this return value will contain
+//		an appropriate error message.
+//
+//		If an error message is returned, the text value of input
+//		parameter 'errorPrefix' will be inserted or prefixed at
+//		the beginning of the error message.
+func (signedNumFmtSpec *SignedNumberFormatSpec) GetNumberFieldSpec(
+	errorPrefix interface{}) (
+	NumStrNumberFieldSpec,
+	error) {
+
+	if signedNumFmtSpec.lock == nil {
+		signedNumFmtSpec.lock = new(sync.Mutex)
+	}
+
+	signedNumFmtSpec.lock.Lock()
+
+	defer signedNumFmtSpec.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"SignedNumberFormatSpec."+
+			"GetNumberFieldSpec()",
+		"")
+
+	if err != nil {
+		return NumStrNumberFieldSpec{}, err
+	}
+
+	return signedNumFmtSpec.numberFieldSpec.CopyOut(
+		ePrefix.XCpy(
+			"<-signedNumFmtSpec.numberFieldSpec"))
+}
+
 // GetPositiveNumSymSpec - Returns the Positive Number Symbol
 // Specification currently configured for this instance of
 // SignedNumberFormatSpec.
@@ -2460,7 +2580,8 @@ func (signedNumFmtSpec *SignedNumberFormatSpec) SetNegativeNumberSignSpec(
 // SignedNumberFormatSpec.
 //
 // The Number Field Specification includes parameters for
-// field length and text justification ('Right, Center, Left').
+// field length and text justification ('Right, Center,
+// Left').
 //
 // Numeric digits are formatted using the Number Field
 // Specification within a number field specified by field
