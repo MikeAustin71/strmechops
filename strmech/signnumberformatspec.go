00c1140523473fc7ca02e4748edd5a6763b0103e
Mike Rapp <jordy.rascal@protonmail.com>
2022-08-22 17:19:48 -0500

    Added new method SignedNumberFormatSpec.GetRoundingSpec()

--- strmech/signnumberformatspec.go
+++ strmech/signnumberformatspec.go
@@ -1111,6 +1111,122 @@ func (signedNumFmtSpec *SignedNumberFormatSpec) GetPositiveNumSymSpec(
 			"<-signedNumFmtSpec.positiveNumberSign"))
 }
 
+// GetRoundingSpec - Returns the Rounding Specification
+// configured for the current instance of
+// SignedNumberFormatSpec.
+//
+// The Rounding Specification determines if rounding will be
+// applied to the fractional digits in a number string as
+// well as the type of rounding algorithm applied and the
+// number of fractional digits remaining after the rounding
+// operation is completed.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	 errorPrefix                interface{}
+//
+//		This object encapsulates error prefix text which is
+//		included in all returned error messages. Usually, it
+//		contains the name of the calling method or methods
+//		listed as a method or function chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1. nil - A nil value is valid and generates an empty
+//		   collection of error prefix and error context
+//		   information.
+//
+//		2. string - A string containing error prefix
+//			information.
+//
+//		3. []string A one-dimensional slice of strings
+//			containing error prefix information.
+//
+//		4. [][2]string A two-dimensional slice of strings
+//		   containing error prefix and error context
+//		   information.
+//
+//		5. ErrPrefixDto - An instance of ErrPrefixDto.
+//			Information from this object will be copied for use
+//			in error and informational messages.
+//
+//		6. *ErrPrefixDto - A pointer to an instance of
+//			ErrPrefixDto. Information from this object will be
+//			copied for use in error and informational messages.
+//
+//		7. IBasicErrorPrefix - An interface to a method
+//			generating a two-dimensional slice of strings
+//			containing error prefix and error context
+//			information.
+//
+//		If parameter 'errorPrefix' is NOT convertible to one
+//		of the valid types listed above, it will be
+//		considered invalid and trigger the return of an
+//		error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are included
+//		in the 'errpref' software package,
+//		"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	NumStrRoundingSpec
+//
+//		If this method completes successfully, a copy
+//		of the Rounding Specification configured for
+//		the current SignedNumberFormatSpec instance
+//		will be returned.
+//
+//	error
+//
+//		If this method completes successfully and no errors are
+//		encountered this return value is set to 'nil'. Otherwise,
+//		if errors are encountered, this return value will contain
+//		an appropriate error message.
+//
+//		If an error message is returned, the text value of input
+//		parameter 'errorPrefix' will be inserted or prefixed at
+//		the beginning of the error message.
+func (signedNumFmtSpec *SignedNumberFormatSpec) GetRoundingSpec(
+	errorPrefix interface{}) (
+	NumStrRoundingSpec,
+	error) {
+
+	if signedNumFmtSpec.lock == nil {
+		signedNumFmtSpec.lock = new(sync.Mutex)
+	}
+
+	signedNumFmtSpec.lock.Lock()
+
+	defer signedNumFmtSpec.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"SignedNumberFormatSpec."+
+			"GetRoundingSpec()",
+		"")
+
+	if err != nil {
+		return NumStrRoundingSpec{}, err
+	}
+
+	return signedNumFmtSpec.roundingSpec.CopyOut(
+		ePrefix.XCpy(
+			"<-signedNumFmtSpec.roundingSpec"))
+}
+
 // NewSignedNumFmtParams - Creates and returns a new instance of
 // SignedNumberFormatSpec.
 //
