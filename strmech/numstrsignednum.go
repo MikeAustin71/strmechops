52de120dcd2b29ac144bb56eb76219bcc0180afa
Mike Rapp <jordy.rascal@protonmail.com>
2022-08-22 19:39:47 -0500

    Refactored NumStrSignedNum.GetNumStr()

--- strmech/numstrsignednum.go
+++ strmech/numstrsignednum.go
@@ -17,83 +17,89 @@ type NumStrSignedNum struct {
 //
 // Input Parameters
 //
-//		numStrKernel               NumberStrKernel
-//		   - An instance of NumberStrKernel containing the numeric
-//		     digits which will be used to create and format the
-//		     returned number string.
+//	numStrKernel				NumberStrKernel
+//
+//		An instance of NumberStrKernel containing the numeric
+//		digits which will be used to create and format the
+//		returned number string.
 //
 //
-//		signedNumFormatSpec        SignedNumberFormatSpec
-//	    - This instance of SignedNumberFormatSpec contains all
-//	      the parameters required to format a signed number for
-//	      text display.
+//	signedNumFormatSpec			SignedNumberFormatSpec
+//
+//		This instance of SignedNumberFormatSpec contains all
+//		the parameters required to format a signed number for
+//		text display.
 //
 //
-//		errorPrefix                interface{}
-//		   - This object encapsulates error prefix text which is
-//		     included in all returned error messages. Usually, it
-//		     contains the name of the calling method or methods
-//		     listed as a method or function chain of execution.
+//	 errorPrefix                interface{}
 //
-//		     If no error prefix information is needed, set this parameter
-//		     to 'nil'.
+//		This object encapsulates error prefix text which is
+//		included in all returned error messages. Usually, it
+//		contains the name of the calling method or methods
+//		listed as a method or function chain of execution.
 //
-//		     This empty interface must be convertible to one of the
-//		     following types:
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
 //
+//		This empty interface must be convertible to one of
+//		the following types:
 //
-//		     1. nil - A nil value is valid and generates an empty
-//		              collection of error prefix and error context
-//		              information.
+//		1. nil - A nil value is valid and generates an empty
+//		   collection of error prefix and error context
+//		   information.
 //
-//		     2. string - A string containing error prefix information.
+//		2. string - A string containing error prefix
+//			information.
 //
-//		     3. []string A one-dimensional slice of strings containing
-//		                 error prefix information
+//		3. []string A one-dimensional slice of strings
+//			containing error prefix information.
 //
-//		     4. [][2]string A two-dimensional slice of strings containing
-//		                    error prefix and error context information.
+//		4. [][2]string A two-dimensional slice of strings
+//		   containing error prefix and error context
+//		   information.
 //
-//		     5. ErrPrefixDto - An instance of ErrPrefixDto. The
-//		                       ErrorPrefixInfo from this object will be
-//		                       copied to 'errPrefDto'.
+//		5. ErrPrefixDto - An instance of ErrPrefixDto.
+//			Information from this object will be copied for use
+//			in error and informational messages.
 //
-//		     6. *ErrPrefixDto - A pointer to an instance of ErrPrefixDto.
-//		                        ErrorPrefixInfo from this object will be
-//		                       copied to 'errPrefDto'.
+//		6. *ErrPrefixDto - A pointer to an instance of
+//			ErrPrefixDto. Information from this object will be
+//			copied for use in error and informational messages.
 //
-//		     7. IBasicErrorPrefix - An interface to a method generating
-//		                            a two-dimensional slice of strings
-//		                            containing error prefix and error
-//		                            context information.
+//		7. IBasicErrorPrefix - An interface to a method
+//			generating a two-dimensional slice of strings
+//			containing error prefix and error context
+//			information.
 //
-//		     If parameter 'errorPrefix' is NOT convertible to one of
-//		     the valid types listed above, it will be considered
-//		     invalid and trigger the return of an error.
+//		If parameter 'errorPrefix' is NOT convertible to one
+//		of the valid types listed above, it will be
+//		considered invalid and trigger the return of an
+//		error.
 //
-//		     Types ErrPrefixDto and IBasicErrorPrefix are included in
-//		     the 'errpref' software package, "github.com/MikeAustin71/errpref".
+//		Types ErrPrefixDto and IBasicErrorPrefix are included
+//		in the 'errpref' software package,
+//		"github.com/MikeAustin71/errpref".
 //
 // ------------------------------------------------------------------------
 //
 // # Return Values
 //
-// numberStr                   string
+//	numberStr					string
 //
-//   - If this method completes successfully, this parameter
-//     will return a signed numeric value formatted as a
-//     number string.
+//		If this method completes successfully, this parameter
+//		will return a signed numeric value formatted as a
+//		number string.
 //
-//     err                        error
+//	err							error
 //
-//   - If this method completes successfully and no errors are
-//     encountered this return value is set to 'nil'. Otherwise,
-//     if errors are encountered, this return value will contain
-//     an appropriate error message.
+//		If this method completes successfully and no errors
+//		are encountered this return value is set to 'nil'.
+//		Otherwise, if errors are encountered, this return
+//		value will contain an appropriate error message.
 //
-//     If an error message is returned, the text value of input
-//     parameter 'errorPrefix' will be inserted or prefixed at
-//     the beginning of the error message.
+//		If an error message is returned, the text value of
+//		input parameter 'errorPrefix' will be inserted or
+//		prefixed at	the beginning of the error message.
 func (nStrSignedNum *NumStrSignedNum) GetNumStr(
 	numStrKernel NumberStrKernel,
 	signedNumFormatSpec SignedNumberFormatSpec,
