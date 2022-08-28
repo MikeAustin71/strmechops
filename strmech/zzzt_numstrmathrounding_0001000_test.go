50d9975990e64c0999a372423a9a4262f0c90df8
Mike Rapp <jordy.rascal@protonmail.com>
2022-08-28 17:23:14 -0500

    Added more tests for new method numStrMathRoundingAtom.roundHalfAwayFromZero().

--- strmech/zzzt_numstrmathrounding_0001000_test.go
+++ strmech/zzzt_numstrmathrounding_0001000_test.go
@@ -33,6 +33,12 @@ func TestNumStrMathRoundingRoundHalfAwayFromZero_000100(t *testing.T) {
 		ePrefix.XCpy(
 			"intRunes"))
 
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
 	nStrMathRoundAtom := numStrMathRoundingAtom{}
 
 	err = nStrMathRoundAtom.roundHalfAwayFromZero(
@@ -65,6 +71,187 @@ func TestNumStrMathRoundingRoundHalfAwayFromZero_000100(t *testing.T) {
 			actualStr)
 
 		return
+	}
+
+	expectedIntDigits := "1"
+
+	expectedFracDigits := "9999999"
+
+	intRunes,
+		err = RuneArrayDto{}.NewRunes(
+		[]rune(expectedIntDigits),
+		CharSearchType.LinearTargetStartingIndex(),
+		ePrefix.XCpy(
+			"intRunes"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	fracRunes,
+		err = RuneArrayDto{}.NewRunes(
+		[]rune(expectedFracDigits),
+		CharSearchType.LinearTargetStartingIndex(),
+		ePrefix.XCpy(
+			"intRunes"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	err = nStrMathRoundAtom.roundHalfAwayFromZero(
+		&intRunes,
+		&fracRunes,
+		3,
+		ePrefix.XCpy(
+			"roundToFracDigits=3"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	actualStr = intRunes.GetCharacterString() +
+		"." +
+		fracRunes.GetCharacterString()
+
+	expectedStr = "2.000"
+
+	if expectedStr != actualStr {
+
+		t.Errorf("%v\n"+
+			"Error: nStrMathRoundAtom.roundHalfAwayFromZero() #2\n"+
+			"Expected String = '%v'\n"+
+			"  Actual String = '%v'\n",
+			ePrefix.String(),
+			expectedStr,
+			actualStr)
+
+		return
+	}
+
+	expectedIntDigits = "9"
+
+	expectedFracDigits = "9999999"
+
+	intRunes,
+		err = RuneArrayDto{}.NewRunes(
+		[]rune(expectedIntDigits),
+		CharSearchType.LinearTargetStartingIndex(),
+		ePrefix.XCpy(
+			"intRunes"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	fracRunes,
+		err = RuneArrayDto{}.NewRunes(
+		[]rune(expectedFracDigits),
+		CharSearchType.LinearTargetStartingIndex(),
+		ePrefix.XCpy(
+			"intRunes"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	err = nStrMathRoundAtom.roundHalfAwayFromZero(
+		&intRunes,
+		&fracRunes,
+		3,
+		ePrefix.XCpy(
+			"roundToFracDigits=3"))
+
+	if err != nil {
+		t.Errorf("%v\n",
+			err.Error())
+		return
+	}
+
+	actualStr = intRunes.GetCharacterString() +
+		"." +
+		fracRunes.GetCharacterString()
+
+	expectedStr = "10.000"
+
+	if expectedStr != actualStr {
+
+		t.Errorf("%v\n"+
+			"Error: nStrMathRoundAtom.roundHalfAwayFromZero() #2\n"+
+			"Expected String = '%v'\n"+
+			"  Actual String = '%v'\n",
+			ePrefix.String(),
+			expectedStr,
+			actualStr)
+
+		return
+	}
+
+	err = nStrMathRoundAtom.roundHalfAwayFromZero(
+		nil,
+		&fracRunes,
+		3,
+		ePrefix.XCpy(
+			"Error Test #1"))
+
+	if err == nil {
+
+		t.Errorf("%v\n"+
+			"Error: nStrMathRoundAtom.roundHalfAwayFromZero() Error #1\n"+
+			"Expected an error return because 'integerDigits' is nil\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.String())
+
+		return
+
+	}
+
+	err = nStrMathRoundAtom.roundHalfAwayFromZero(
+		&intRunes,
+		nil,
+		3,
+		ePrefix.XCpy(
+			"Error Test #2"))
+
+	if err == nil {
+
+		t.Errorf("%v\n"+
+			"Error: nStrMathRoundAtom.roundHalfAwayFromZero() Error #2\n"+
+			"Expected an error return because 'fractionalDigits' is nil\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.String())
+
+		return
+
+	}
+
+	err = nStrMathRoundAtom.roundHalfAwayFromZero(
+		&intRunes,
+		&fracRunes,
+		-1,
+		ePrefix.XCpy(
+			"Error Test #3"))
+
+	if err == nil {
+
+		t.Errorf("%v\n"+
+			"Error: nStrMathRoundAtom.roundHalfAwayFromZero() Error #2\n"+
+			"Expected an error return because 'roundToFractionalDigits' is minus one (-1)\n"+
+			"HOWEVER, NO ERROR WAS RETURNED!\n",
+			ePrefix.String())
+
+		return
+
 	}
 
 	return
