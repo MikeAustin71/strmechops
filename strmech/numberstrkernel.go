59ea177ed4c60bd3e17c04671861e0bafeda8a6c
Mike Rapp <jordy.rascal@protonmail.com>
2022-09-15 23:23:49 -0500

    Added new method NumberStrKernel.NewFromBigFloatValue().

--- strmech/numberstrkernel.go
+++ strmech/numberstrkernel.go
@@ -1737,6 +1737,135 @@ func (numStrKernel *NumberStrKernel) IsNonZeroValue() bool {
 	return false
 }
 
+//	NewFromBigFloatValue
+//
+//	Creates a new instance of NumberStrKernel based on a
+//	big float numeric value (*big.Float) passed as an
+//	input parameter.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	bigFloatNum					*big.Float
+//
+//		This floating point numeric value which will be
+//		used to create and return a new instance of
+//		NumberStrKernel.
+//
+//
+//	 errorPrefix                interface{}
+//
+//		This object encapsulates error prefix text which is
+//		included in all returned error messages. Usually, it
+//		contains the name of the calling method or methods
+//		listed as a method or function chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1. nil - A nil value is valid and generates an empty
+//		   collection of error prefix and error context
+//		   information.
+//
+//		2. string - A string containing error prefix
+//			information.
+//
+//		3. []string A one-dimensional slice of strings
+//			containing error prefix information.
+//
+//		4. [][2]string A two-dimensional slice of strings
+//		   containing error prefix and error context
+//		   information.
+//
+//		5. ErrPrefixDto - An instance of ErrPrefixDto.
+//			Information from this object will be copied for use
+//			in error and informational messages.
+//
+//		6. *ErrPrefixDto - A pointer to an instance of
+//			ErrPrefixDto. Information from this object will be
+//			copied for use in error and informational messages.
+//
+//		7. IBasicErrorPrefix - An interface to a method
+//			generating a two-dimensional slice of strings
+//			containing error prefix and error context
+//			information.
+//
+//		If parameter 'errorPrefix' is NOT convertible to one
+//		of the valid types listed above, it will be
+//		considered invalid and trigger the return of an
+//		error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are included
+//		in the 'errpref' software package,
+//		"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	NumberStrKernel
+//
+//		If this method completes successfully, a new instance
+//		of NumberStrKernel will be returned configured and
+//		populated with the numeric value passed in paramter,
+//		'bigFloatNum'.
+//
+//	error
+//
+//		If this method completes successfully, the returned
+//		error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error message.
+//	 	This returned error message will incorporate the method
+//	 	chain and text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be attached to the beginning
+//	 	of the error message.
+func (numStrKernel *NumberStrKernel) NewFromBigFloatValue(
+	bigFloatNum *big.Float,
+	errorPrefix interface{}) (
+	NumberStrKernel,
+	error) {
+
+	if numStrKernel.lock == nil {
+		numStrKernel.lock = new(sync.Mutex)
+	}
+
+	numStrKernel.lock.Lock()
+
+	defer numStrKernel.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	newNumStrKernel := NumberStrKernel{}
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"NumberStrKernel."+
+			"NewFromBigFloatValue()",
+		"")
+
+	if err != nil {
+		return newNumStrKernel, err
+	}
+
+	numberSign := NumSignVal.None()
+
+	err = new(numberStrKernelNanobot).setWithNumber(
+		&newNumStrKernel,
+		bigFloatNum,
+		numberSign,
+		ePrefix)
+
+	return newNumStrKernel, err
+}
+
 //	NewFromBigIntValue
 //
 //	Creates a new instance of NumberStrKernel based on a
@@ -1811,7 +1940,7 @@ func (numStrKernel *NumberStrKernel) IsNonZeroValue() bool {
 //		If this method completes successfully, a new instance
 //		of NumberStrKernel will be returned configured and
 //		populated with the numeric value passed in paramter,
-//		'signedIntValue'.
+//		'bigIntNum'.
 //
 //	error
 //
