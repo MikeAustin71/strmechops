a8e55450d19182e6dad96521ccc0ad2c5539e969
Mike Rapp <jordy.rascal@protonmail.com>
2022-09-16 00:30:48 -0500

    Added new method NumberStrKernel.SetBigFloatValue().

--- strmech/numberstrkernel.go
+++ strmech/numberstrkernel.go
@@ -1749,7 +1749,7 @@ func (numStrKernel *NumberStrKernel) IsNonZeroValue() bool {
 //
 //	bigFloatNum					*big.Float
 //
-//		This floating point numeric value which will be
+//		This floating point numeric value will be
 //		used to create and return a new instance of
 //		NumberStrKernel.
 //
@@ -2445,6 +2445,134 @@ func (numStrKernel *NumberStrKernel) NewFromUnsignedIntValue(
 		ePrefix)
 
 	return newNumStrKernel, err
+}
+
+//	SetBigFloatValue
+//
+//	Deletes resets the internal values for the current
+//	instance of	NumberStrKernel using the big floating
+//	point numeric value passed as an instance of type
+//	*big.Float.
+//
+// ----------------------------------------------------------------
+//
+// # IMPORTANT
+//
+//	For the current instance of NumberStrKernel, all
+//	pre-existing data will be deleted and overwritten
+//	with new data generated from the numeric value
+//	passed as input parameter, 'bigFloatNum'.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	bigFloatNum					*big.Float
+//
+//		This floating point numeric value will be used
+//		to configure and populate the current instance
+//		of NumberStrKernel.
+//
+//
+//	 errorPrefix                interface{}
+//
+//		This object encapsulates error prefix text which is
+//		included in all returned error messages. Usually, it
+//		contains the name of the calling method or methods
+//		listed as a method or function chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1. nil - A nil value is valid and generates an empty
+//		   collection of error prefix and error context
+//		   information.
+//
+//		2. string - A string containing error prefix
+//			information.
+//
+//		3. []string A one-dimensional slice of strings
+//			containing error prefix information.
+//
+//		4. [][2]string A two-dimensional slice of strings
+//		   containing error prefix and error context
+//		   information.
+//
+//		5. ErrPrefixDto - An instance of ErrPrefixDto.
+//			Information from this object will be copied for use
+//			in error and informational messages.
+//
+//		6. *ErrPrefixDto - A pointer to an instance of
+//			ErrPrefixDto. Information from this object will be
+//			copied for use in error and informational messages.
+//
+//		7. IBasicErrorPrefix - An interface to a method
+//			generating a two-dimensional slice of strings
+//			containing error prefix and error context
+//			information.
+//
+//		If parameter 'errorPrefix' is NOT convertible to one
+//		of the valid types listed above, it will be
+//		considered invalid and trigger the return of an
+//		error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are included
+//		in the 'errpref' software package,
+//		"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the returned
+//		error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error message.
+//	 	This returned error message will incorporate the method
+//	 	chain and text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be attached to the beginning
+//	 	of the error message.
+func (numStrKernel *NumberStrKernel) SetBigFloatValue(
+	bigFloatNum *big.Float,
+	errorPrefix interface{}) error {
+
+	if numStrKernel.lock == nil {
+		numStrKernel.lock = new(sync.Mutex)
+	}
+
+	numStrKernel.lock.Lock()
+
+	defer numStrKernel.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"NumberStrKernel."+
+			"SetBigFloatValue()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	numberSign := NumSignVal.None()
+
+	err = new(numberStrKernelNanobot).setWithNumber(
+		numStrKernel,
+		bigFloatNum,
+		numberSign,
+		ePrefix)
+
+	return err
 }
 
 // RationalizeFractionalIntegerDigits - If fractional digits are
