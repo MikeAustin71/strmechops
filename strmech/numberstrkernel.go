8e0f1fb37cc58e7da3d9b5713705de74ce934d78
Mike Rapp <jordy.rascal@protonmail.com>
2022-09-20 23:17:32 -0500

    Added new method NumberStrKernel.NewFromRuneDto()

--- strmech/numberstrkernel.go
+++ strmech/numberstrkernel.go
@@ -3210,6 +3210,166 @@ func (numStrKernel *NumberStrKernel) NewFromFloatValue(
 	return newNumStrKernel, err
 }
 
+//	NewFromRuneDto
+//
+//	Creates and returns a new instance of NumberStrKernel
+//	generated from input parameters containing integer
+//	digit and fractional digit Rune Array Data Transfer
+//	Objects (RuneArrayDto).
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	integerDigits				*RuneArrayDto
+//
+//		A pointer to a rune array data transfer object used to
+//		configure the integer digits array contained within
+//		the returned instance of NumberStrKernel.
+//
+//	fractionalDigits			*RuneArrayDto
+//
+//		A pointer to a rune array data transfer object used to
+//		configure the fractional digits array contained within
+//		the returned instance of NumberStrKernel.
+//
+//	numberSign					NumericSignValueType
+//
+//		The Number Sign is specified by means of a
+//		NumericSignValueType enumeration value.
+//
+//		Possible values are listed as follows:
+//
+//			NumSignVal.None()     = Ignored. Numeric Value
+//									will be set to positive.
+//
+//			NumSignVal.Negative() = Valid Value
+//
+//			NumSignVal.Zero()     = Zero numeric values are
+//									automatically assigned
+//									NumSignVal.Zero().
+//
+//			NumSignVal.Positive() = Default value for
+//									non-zero numeric values.
+//
+//		If 'numberSign' is set to any value other than
+//		NumSignVal.Negative(), it will be ignored and
+//		the final number sign will be automatically
+//		assigned.
+//
+//	 errorPrefix                interface{}
+//
+//		This object encapsulates error prefix text which is
+//		included in all returned error messages. Usually, it
+//		contains the name of the calling method or methods
+//		listed as a method or function chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1. nil - A nil value is valid and generates an empty
+//		   collection of error prefix and error context
+//		   information.
+//
+//		2. string - A string containing error prefix
+//			information.
+//
+//		3. []string A one-dimensional slice of strings
+//			containing error prefix information.
+//
+//		4. [][2]string A two-dimensional slice of strings
+//		   containing error prefix and error context
+//		   information.
+//
+//		5. ErrPrefixDto - An instance of ErrPrefixDto.
+//			Information from this object will be copied for use
+//			in error and informational messages.
+//
+//		6. *ErrPrefixDto - A pointer to an instance of
+//			ErrPrefixDto. Information from this object will be
+//			copied for use in error and informational messages.
+//
+//		7. IBasicErrorPrefix - An interface to a method
+//			generating a two-dimensional slice of strings
+//			containing error prefix and error context
+//			information.
+//
+//		If parameter 'errorPrefix' is NOT convertible to one
+//		of the valid types listed above, it will be
+//		considered invalid and trigger the return of an
+//		error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are included
+//		in the 'errpref' software package,
+//		"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	NumberStrKernel
+//
+//		If this method completes successfully, a new instance
+//		of NumberStrKernel will be returned configured with
+//		the numeric value extracted from input parameters
+//		'integerDigits' and 'fractionalDigits'.
+//
+//	error
+//
+//		If this method completes successfully, the returned
+//		error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error message.
+//	 	This returned error message will incorporate the method
+//	 	chain and text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be attached to the beginning
+//	 	of the error message.
+func (numStrKernel *NumberStrKernel) NewFromRuneDto(
+	integerDigits *RuneArrayDto,
+	fractionalDigits *RuneArrayDto,
+	numberSign NumericSignValueType,
+	errorPrefix interface{}) (
+	NumberStrKernel,
+	error) {
+
+	if numStrKernel.lock == nil {
+		numStrKernel.lock = new(sync.Mutex)
+	}
+
+	numStrKernel.lock.Lock()
+
+	defer numStrKernel.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	newNumStrKernel := NumberStrKernel{}
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"NumberStrKernel."+
+			"NewFromRuneDto()",
+		"")
+
+	if err != nil {
+		return newNumStrKernel, err
+	}
+
+	err = new(numberStrKernelNanobot).setWithRuneArrayDto(
+		&newNumStrKernel,
+		integerDigits,
+		fractionalDigits,
+		numberSign,
+		ePrefix)
+
+	return newNumStrKernel, err
+}
+
 //	NewFromRuneDigits
 //
 //	Creates and returns a new instance of NumberStrKernel
