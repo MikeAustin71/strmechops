6f8247f40867d18663e862d708037cb731a0795b
Mike Rapp <jordy.rascal@protonmail.com>
2022-10-06 16:18:58 -0500

    Added new method NumStrFormatSpec.NumStrFormatSpec()

--- strmech/numstrformatspec.go
+++ strmech/numstrformatspec.go
@@ -2420,6 +2420,231 @@ func (numStrFmtSpec *NumStrFormatSpec) NewNumFmtParamsRunes(
 	return newSignedNumFmtSpec, err
 }
 
+//	NewSignedNumFmtUS
+//
+//	Returns a new instance of NumStrFormatSpec configured
+//	for a Signed Number using US (United States)
+//	Number String formatting conventions.
+//
+// ----------------------------------------------------------------
+//
+// # Defaluts
+//
+//	The radix point or decimal seprator is set to the
+//	period character ('.').
+//
+//	The integer group specification is set to 'thousands'.
+//	This means that integer digits will be separated into
+//	'thousands' with each group containing three digits each
+//	(Example: 1,000,000,000).
+//
+//	The negative number sign is set to a leading minus sign
+//	('-').
+//
+//	The positive number sign is set to a blank or empty
+//	string ("").
+//
+//	The zero number format is set to a blank or empty
+//	string ("").
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	roundingSpec 				NumStrRoundingSpec
+//
+//		Numeric Value Rounding Specification. This
+//		specification contains all the parameters
+//		required to configure and apply a rounding
+//		algorithm for floating point Number Strings.
+//
+//	numberFieldSpec			NumStrNumberFieldSpec
+//
+//		This Number Field Specification contains all
+//		parameters necessary to format a Number String
+//		within a larger Number Field. In addition to
+//		specifying the length of number field, this
+//		object contains justification specifications
+//		for centering, left justifying or right
+//		justifying a Number String within a Number
+//		Field.
+//
+//	 errorPrefix                interface{}
+//
+//		This object encapsulates error prefix text which is
+//		included in all returned error messages. Usually, it
+//		contains the name of the calling method or methods
+//		listed as a method or function chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1. nil - A nil value is valid and generates an empty
+//		   collection of error prefix and error context
+//		   information.
+//
+//		2. string - A string containing error prefix
+//			information.
+//
+//		3. []string A one-dimensional slice of strings
+//			containing error prefix information.
+//
+//		4. [][2]string A two-dimensional slice of strings
+//		   containing error prefix and error context
+//		   information.
+//
+//		5. ErrPrefixDto - An instance of ErrPrefixDto.
+//			Information from this object will be copied for use
+//			in error and informational messages.
+//
+//		6. *ErrPrefixDto - A pointer to an instance of
+//			ErrPrefixDto. Information from this object will be
+//			copied for use in error and informational messages.
+//
+//		7. IBasicErrorPrefix - An interface to a method
+//			generating a two-dimensional slice of strings
+//			containing error prefix and error context
+//			information.
+//
+//		If parameter 'errorPrefix' is NOT convertible to one
+//		of the valid types listed above, it will be
+//		considered invalid and trigger the return of an
+//		error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are included
+//		in the 'errpref' software package,
+//		"github.com/MikeAustin71/errpref".
+//
+// -----------------------------------------------------------------
+//
+// # Return Values
+//
+//	newSignedNumFmtSpec			NumStrFormatSpec
+//
+//		If this method completes successfully, this parameter
+//		will return a new, fully populated instance of
+//		NumStrFormatSpec.
+//
+//	err							error
+//
+//		If this method completes successfully, the returned error
+//		Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the returned
+//		error Type will encapsulate an error message. This
+//		returned error message will incorporate the method chain
+//		and text passed by input parameter, 'errorPrefix'. The
+//		'errorPrefix' text will be attached to the beginning of
+//		the error message.
+func (numStrFmtSpec *NumStrFormatSpec) NewSignedNumFmtUS(
+	roundingSpec NumStrRoundingSpec,
+	numberFieldSpec NumStrNumberFieldSpec,
+	errorPrefix interface{}) (
+	newSignedNumFmtSpec NumStrFormatSpec,
+	err error) {
+
+	if numStrFmtSpec.lock == nil {
+		numStrFmtSpec.lock = new(sync.Mutex)
+	}
+
+	numStrFmtSpec.lock.Lock()
+
+	defer numStrFmtSpec.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"NumStrFormatSpec."+
+			"NewSignedNumFmtUS()",
+		"")
+
+	if err != nil {
+		return newSignedNumFmtSpec, err
+	}
+
+	var decSeparator DecimalSeparatorSpec
+
+	decSeparator = DecimalSeparatorSpec{
+		decimalSeparatorChars: RuneArrayDto{
+			CharsArray:     []rune{'.'},
+			Description1:   "",
+			Description2:   "",
+			charSearchType: CharSearchType.LinearTargetStartingIndex(),
+			lock:           nil,
+		},
+
+		lock: nil,
+	}
+
+	var intGroupingSpec NumStrIntegerGroupingSpec
+
+	intGroupingSpec = NumStrIntegerGroupingSpec{
+		integerSeparatorChars: RuneArrayDto{
+			CharsArray:     []rune{','},
+			Description1:   "",
+			Description2:   "",
+			charSearchType: CharSearchType.LinearTargetStartingIndex(),
+			lock:           nil,
+		},
+		intGroupingType: IntGroupingType.Thousands(),
+		lock:            nil,
+	}
+
+	var negativeNumberSign NumStrNumberSymbolSpec
+
+	negativeNumberSign = NumStrNumberSymbolSpec{
+		leadingNumberSymbols: RuneArrayDto{
+			CharsArray:     []rune{'-'},
+			Description1:   "",
+			Description2:   "",
+			charSearchType: CharSearchType.LinearTargetStartingIndex(),
+			lock:           nil,
+		},
+		leadingNumberFieldSymbolPosition:  NumFieldSymPos.InsideNumField(),
+		trailingNumberSymbols:             RuneArrayDto{},
+		trailingNumberFieldSymbolPosition: 0,
+		lock:                              nil,
+	}
+
+	var positiveNumberSign NumStrNumberSymbolSpec
+
+	positiveNumberSign = NumStrNumberSymbolSpec{
+		leadingNumberSymbols:              RuneArrayDto{},
+		leadingNumberFieldSymbolPosition:  0,
+		trailingNumberSymbols:             RuneArrayDto{},
+		trailingNumberFieldSymbolPosition: 0,
+		lock:                              nil,
+	}
+
+	var zeroNumberSign NumStrNumberSymbolSpec
+
+	zeroNumberSign = NumStrNumberSymbolSpec{
+		leadingNumberSymbols:              RuneArrayDto{},
+		leadingNumberFieldSymbolPosition:  0,
+		trailingNumberSymbols:             RuneArrayDto{},
+		trailingNumberFieldSymbolPosition: 0,
+		lock:                              nil,
+	}
+
+	err = new(numStrFmtSpecNanobot).setNStrFmtComponents(
+		&newSignedNumFmtSpec,
+		decSeparator,
+		intGroupingSpec,
+		roundingSpec,
+		negativeNumberSign,
+		positiveNumberSign,
+		zeroNumberSign,
+		numberFieldSpec,
+		ePrefix.XCpy("newSignedNumFmtSpec<-"))
+
+	return newSignedNumFmtSpec, err
+}
+
 // SetDecimalSeparatorSpec - Deletes and replaces the Decimal
 // Separator Specification for the current instance of
 // NumStrFormatSpec.
