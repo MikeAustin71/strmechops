a8a0e2212d0e1e754813d32039184f1dcae30239
Mike Rapp <jordy.rascal@protonmail.com>
2022-10-07 18:32:18 -0500

    Added new method IntegerSeparatorDto.SetGermanDefaults()

--- strmech/integerseparatordto.go
+++ strmech/integerseparatordto.go
@@ -4882,6 +4882,143 @@ func (nStrIntSep *IntegerSeparatorDto) SetFrenchDefaults(
 			ePrefix)
 }
 
+//	SetGermanDefaults
+//
+//	This method will overwrite and set the all the
+//	internal member variable data values for the
+//	current instance of IntegerSeparatorDto to
+//	default values used in Germany. Integer
+//	separator values used in Germany consist of the
+//	period character ('.'), an integer grouping of
+//	three ('3') and unlimited repetitions of this
+//	sequence.
+//
+//		German Integer Separation Example
+//		(One-Million): '1.000.000'
+//
+//	While Germany is a member of the European Union,
+//	various European Union member countries apply
+//	different characters for integer separation.
+//
+//	Many countries follow the German integer separation
+//	convention but other European Union member countries
+//	follow the integer separation convention used in
+//	France. For information on German integer separation
+//	usage, see method:
+//
+//		IntegerSeparatorDto.SetFrenchDefaults()
+//
+//	If custom integer separator algorithms are required, see
+//	method:
+//
+//		IntegerSeparatorDto.SetComponents()
+//
+// ----------------------------------------------------------------
+//
+// # IMPORTANT
+//
+//	This method will overwrite all pre-existing data
+//	values in the current IntegerSeparatorDto instance.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which is
+//		included in all returned error messages. Usually, it
+//		contains the name of the calling method or methods
+//		listed as a method or function chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil - A nil value is valid and generates an empty
+//		   	collection of error prefix and error context
+//		   	information.
+//
+//		2.	string - A string containing error prefix
+//			information.
+//
+//		3.	[]string A one-dimensional slice of strings
+//			containing error prefix information.
+//
+//		4.	[][2]string A two-dimensional slice of strings
+//		   	containing error prefix and error context
+//		   	information.
+//
+//		5.	ErrPrefixDto - An instance of ErrPrefixDto.
+//			Information from this object will be copied for
+//			use in error and informational messages.
+//
+//		6.	*ErrPrefixDto - A pointer to an instance of
+//			ErrPrefixDto. Information from this object will
+//			be copied for use in error and informational
+//			messages.
+//
+//		7.	IBasicErrorPrefix - An interface to a method
+//			generating a two-dimensional slice of strings
+//			containing error prefix and error context
+//			information.
+//
+//		If parameter 'errorPrefix' is NOT convertible to one
+//		of the valid types listed above, it will be
+//		considered invalid and trigger the return of an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are included
+//		in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref"
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the returned
+//		error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error message.
+//		This returned error message will incorporate the method
+//		chain and text passed by input parameter, 'errorPrefix'.
+//		The 'errorPrefix' text will be attached to the beginning
+//		of the error message.
+func (nStrIntSep *IntegerSeparatorDto) SetGermanDefaults(
+	errorPrefix interface{}) error {
+
+	if nStrIntSep.lock == nil {
+		nStrIntSep.lock = new(sync.Mutex)
+	}
+
+	nStrIntSep.lock.Lock()
+
+	defer nStrIntSep.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"IntegerSeparatorDto."+
+			"SetGermanDefaults()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	return new(integerSeparatorDtoMechanics).
+		setToGermanDefaults(
+			nStrIntSep,
+			ePrefix)
+}
+
 // SetIndiaNumbering - Overwrites all the member variable
 // data values for the current instance of IntegerSeparatorDto.
 //
