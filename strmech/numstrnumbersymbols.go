f18a967e7e252ef173160f5777c78a69937490ea
Mike Rapp <jordy.rascal@protonmail.com>
2022-10-21 01:06:04 -0500

    Added new method NumStrNumberSymbols.SetSymbolSpecs().

--- strmech/numstrnumbersymbols.go
+++ strmech/numstrnumbersymbols.go
@@ -152,7 +152,8 @@ type NumStrNumberSymbols struct {
 //	Creates and returns a new instance of
 //	NumStrNumberSymbols. The new instance is generated
 //	from the Positive, Negative and Zero Number Sign
-//	Symbol objects passed as input parameters.
+//	Symbol Specification objects passed as input
+//	parameters.
 //
 // ----------------------------------------------------------------
 //
@@ -248,7 +249,8 @@ type NumStrNumberSymbols struct {
 //		parameter will return a new, fully populated
 //		instance of NumStrNumberSymbols configured
 //		with the Positive, Negative and Zero Number
-//		Sign Symbol objects passed as input parameters.
+//		Sign Symbol Specification objects passed as
+//		input parameters.
 //
 //	error
 //
@@ -305,6 +307,163 @@ func (nStrNumSym *NumStrNumberSymbols) NewSymbolSpecs(
 				"newNumberSymbols"))
 
 	return newNumberSymbols, err
+}
+
+//	SetSymbolSpecs
+//
+//	Reconfigures the current instance of
+//	NumStrNumberSymbols based on the Positive, Negative
+//	and Zero Number	Sign Symbol Specification objects
+//	passed as input parameters.
+//
+// ----------------------------------------------------------------
+//
+// # IMPORTANT
+//
+//	Be advised that the data fields contained in the
+//	current instance of NumStrNumberSymbols will be
+//	deleted and replaced with the Positive, Negative
+//	and Zero Number	Sign Symbol Specifications passed
+//	as input parameters.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	positiveNumberSign			NumStrNumberSymbolSpec
+//
+//		This Positive Number Sign Symbol Specification
+//		will be copied to the corresponding Positive
+//		Symbol Specification in the new, returned
+//		instance of NumStrNumberSymbols.
+//
+//	negativeNumberSign			NumStrNumberSymbolSpec
+//
+//		This Negative Number Sign Symbol Specification
+//		will be copied to the corresponding Negative
+//		Symbol Specification in the new, returned
+//		instance of NumStrNumberSymbols.
+//
+//	zeroNumberSign			NumStrNumberSymbolSpec
+//
+//		This Zero Number Sign Symbol Specification
+//		will be copied to the corresponding Zero
+//		Symbol Specification in the new, returned
+//		instance of NumStrNumberSymbols.
+//
+//	 errorPrefix                interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it	contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (nStrNumSym *NumStrNumberSymbols) SetSymbolSpecs(
+	positiveNumberSign NumStrNumberSymbolSpec,
+	negativeNumberSign NumStrNumberSymbolSpec,
+	zeroNumberSign NumStrNumberSymbolSpec,
+	errorPrefix interface{}) error {
+
+	if nStrNumSym.lock == nil {
+		nStrNumSym.lock = new(sync.Mutex)
+	}
+
+	nStrNumSym.lock.Lock()
+
+	defer nStrNumSym.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"NumStrNumberSymbols."+
+			"SetSymbolSpecs()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	return new(numStrNumberSymbolsMechanics).
+		setNumSymbolSpecs(
+			nStrNumSym,
+			positiveNumberSign,
+			negativeNumberSign,
+			zeroNumberSign,
+			ePrefix.XCpy(
+				"nStrNumSym"))
+
 }
 
 type numStrNumberSymbolsMechanics struct {
