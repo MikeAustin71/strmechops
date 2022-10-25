9c2dfc04323725232d0ea8bbf8ebfd900b132bf9
Mike Rapp <jordy.rascal@protonmail.com>
2022-10-25 17:17:07 -0500

    Added new method NumStrNumberSymbolsSpec.GetPositiveNumberSignSpec().

--- strmech/numstrnumbersymbolsspec.go
+++ strmech/numstrnumbersymbolsspec.go
@@ -843,6 +843,133 @@ func (nStrNumSymSpec *NumStrNumberSymbolsSpec) GetNegativeNumberSignSpec(
 	return negativeNumSignSpec, err
 }
 
+//	GetPositiveNumberSignSpec
+//
+//	Returns a deep copy of the NumStrNumberSymbolSpec
+//	configured for positive number signs.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	 errorPrefix                interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it	contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	positiveNumSignSpec			NumStrNumberSymbolSpec
+//
+//		If this method completes successfully, this
+//		parameter will return a new, fully populated
+//		instance of NumStrNumberSymbolSpec configured
+//		with the Positive Number Sign Symbol configured
+//		for the current instance of
+//		NumStrNumberSymbolsSpec.
+//
+//	err							error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (nStrNumSymSpec *NumStrNumberSymbolsSpec) GetPositiveNumberSignSpec(
+	errorPrefix interface{}) (
+	positiveNumSignSpec NumStrNumberSymbolSpec,
+	err error) {
+
+	if nStrNumSymSpec.lock == nil {
+		nStrNumSymSpec.lock = new(sync.Mutex)
+	}
+
+	nStrNumSymSpec.lock.Lock()
+
+	defer nStrNumSymSpec.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"NumStrNumberSymbolsSpec."+
+			"GetPositiveNumberSignSpec()",
+		"")
+
+	if err != nil {
+		return positiveNumSignSpec, err
+	}
+
+	positiveNumSignSpec,
+		err = nStrNumSymSpec.positiveNumberSign.CopyOut(
+		ePrefix.XCpy(
+			"positiveNumSignSpec<-"))
+
+	return positiveNumSignSpec, err
+}
+
 // IsNOP
 //
 //	'NOP' stands for 'No Operation'. This method signals
