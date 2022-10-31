38c97b67459f57888b590d60f31ae4e72e5ec402
Mike Rapp <jordy.rascal@protonmail.com>
2022-10-31 02:31:30 -0500

    Added new method NumStrFmtCountryCultureSpec.NewUS().

--- strmech/numstrfmtcountryculturespec.go
+++ strmech/numstrfmtcountryculturespec.go
@@ -727,6 +727,138 @@ func (nStrFmtCountryCultureSpec *NumStrFmtCountryCultureSpec) New(
 	return newCountryCultureSpec, err
 }
 
+//	NewUS
+//
+//	Returns a new instance of NumStrFmtCountryCultureSpec
+//	formatted with country and Number String formatting
+//	parameters commonly used in the US (United States).
+//
+// ----------------------------------------------------------------
+//
+//	# Input Parameters
+//
+//	 errorPrefix                interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it	contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	NumStrFmtCountryCultureSpec
+//
+//		If this method completes successfully, a new
+//		instance of NumStrFmtCountryCultureSpec
+//		will be returned configured with country and
+//		Number String formatting specifications
+//		commonly used in the US (United States).
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (nStrFmtCountryCultureSpec *NumStrFmtCountryCultureSpec) NewUS(
+	errorPrefix interface{}) (
+	NumStrFmtCountryCultureSpec,
+	error) {
+
+	if nStrFmtCountryCultureSpec.lock == nil {
+		nStrFmtCountryCultureSpec.lock = new(sync.Mutex)
+	}
+
+	nStrFmtCountryCultureSpec.lock.Lock()
+
+	defer nStrFmtCountryCultureSpec.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	var err error
+
+	var newCountryCultureSpec NumStrFmtCountryCultureSpec
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"NumStrFmtCountryCultureSpec."+
+			"NewUS()",
+		"")
+
+	if err != nil {
+		return newCountryCultureSpec, err
+	}
+
+	err = new(numStrFmtCountryCultureSpecMech).
+		setCountryUS(
+			&newCountryCultureSpec,
+			ePrefix.XCpy(
+				"newCountryCultureSpec<-"))
+
+	return newCountryCultureSpec, err
+}
+
 //	SetCurrencyNumberFieldSpec
 //
 //	Deletes and resets the Currency Number Field
