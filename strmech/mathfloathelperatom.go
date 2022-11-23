f79d7a15e90406ed550cab9e0cbe277165bd0a0e
Mike Rapp <jordy.rascal@protonmail.com>
2022-11-23 09:52:37 -0600

    Added new method mathFloatHelperAtom.precisionBitsFromRequiredDigits()

--- strmech/mathfloathelperatom.go
+++ strmech/mathfloathelperatom.go
@@ -200,3 +200,179 @@ skipNumStrConversion:
 
 	return signedPureNumStr, err
 }
+
+//	precisionBitsFromRequiredDigits
+//
+//	Generates the number of precision bits in the
+//	mantissa of a big.Float number based on the
+//	number of numerical digits required to produce
+//	an accurate calculation result.
+//
+//	Be advised that the number of mantissa precision bits
+//	required to store a process an accurate numeric value
+//	includes both integer and fractional numeric digits.
+//
+// ----------------------------------------------------------------
+//
+//	# Input Parameters
+//
+//
+//	requiredIntegerDigits		int64
+//
+//		The number of integer digits required for the
+//		pending calculation.
+//
+//		If this parameter has a value less than zero,
+//		an error will be returned.
+//
+//		If the sum of parameters 'requiredIntegerDigits'
+//		and 'requiredFractionalDigits' is equal to zero,
+//		an error will be returned.
+//
+//	requiredFractionalDigits	int64
+//
+//		The number of fractional digits required to
+//		ensure accuracy for the pending calculation.
+//
+//		If this parameter has a value less than zero,
+//		an error will be returned.
+//
+//		If the sum of parameters 'requiredIntegerDigits'
+//		and 'requiredFractionalDigits' is equal to zero,
+//		an error will be returned.
+//
+//	requestedBufferDigits		int64
+//
+//		The number of extra numerical digits required to
+//		ensure accuracy for the pending calculation. It
+//		is generally a good idea to add space for extra
+//		numerical digits to accommodate rounding and/or
+//		complex numerical calculations.
+//
+//		If this parameter has a value less than zero,
+//		an error will be returned.
+//
+//	errPrefDto					*ePref.ErrPrefixDto
+//
+//		This object encapsulates an error prefix string
+//		which is included in all returned error
+//		messages. Usually, it contains the name of the
+//		calling method or methods listed as a function
+//		chain.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		Type ErrPrefixDto is included in the 'errpref'
+//		software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	precisionBits				uint
+//
+//		If this method completes successfully, this
+//		parameter will return the number of precision
+//		bits required to store and accurately process
+//		the number of numerical digits identified by
+//		input parameters, 'requiredIntegerDigits',
+//		'requiredFractionalDigits' and
+//		'requestedBufferDigits'.
+//
+//	err							error
+//
+//		If this method completes successfully, this
+//		returned error Type is set equal to 'nil'. If
+//		errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message.
+//
+//		If an error message is returned, the text value
+//		for input parameter 'errPrefDto' (error prefix)
+//		will be prefixed or attached at the beginning of
+//		the error message.
+func (mathFloatHelpAtom *mathFloatHelperAtom) precisionBitsFromRequiredDigits(
+	requiredIntegerDigits,
+	requiredFractionalDigits,
+	requestedBufferDigits int64,
+	errPrefDto *ePref.ErrPrefixDto) (
+	precisionBits uint,
+	err error) {
+
+	mathFloatHelpAtom.lock.Lock()
+
+	defer mathFloatHelpAtom.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
+		errPrefDto,
+		"mathFloatHelperAtom."+
+			"precisionBitsFromRequiredDigits()",
+		"")
+
+	if err != nil {
+
+		return precisionBits, err
+	}
+
+	if requiredIntegerDigits < 0 {
+
+		err = fmt.Errorf("\n%v\n"+
+			"Error: Input parameter 'requiredIntegerDigits' is invalid!\n"+
+			"'requiredIntegerDigits' is less than zero and negative.\n"+
+			"requiredIntegerDigits = '%v'\n",
+			ePrefix.String(),
+			requiredIntegerDigits)
+
+		return precisionBits, err
+	}
+
+	if requiredFractionalDigits < 0 {
+
+		err = fmt.Errorf("\n%v\n"+
+			"Error: Input parameter 'requiredFractionalDigits' is invalid!\n"+
+			"'requiredFractionalDigits' is less than zero and negative.\n"+
+			"requiredFractionalDigits = '%v'\n",
+			ePrefix.String(),
+			requiredFractionalDigits)
+
+		return precisionBits, err
+	}
+
+	if requestedBufferDigits < 0 {
+
+		err = fmt.Errorf("\n%v\n"+
+			"Error: Input parameter 'requestedBufferDigits' is invalid!\n"+
+			"'requestedBufferDigits' is less than zero and negative.\n"+
+			"requestedBufferDigits = '%v'\n",
+			ePrefix.String(),
+			requestedBufferDigits)
+
+		return precisionBits, err
+	}
+
+	if requiredIntegerDigits == 0 &&
+		requiredFractionalDigits == 0 {
+
+		err = fmt.Errorf("\n%v\n"+
+			"Error: The combination of 'requiredIntegerDigits' and 'requiredFractionalDigits' is invalid!\n"+
+			"Both 'requiredIntegerDigits' and 'requiredFractionalDigits' are equal to zero.\n",
+			ePrefix.String())
+
+		return precisionBits, err
+	}
+
+	totalRequiredNumericalDigits :=
+		requiredIntegerDigits +
+			requiredFractionalDigits +
+			requestedBufferDigits
+
+	precisionBits = new(mathFloatHelperPreon).
+		estimateDigitsToPrecision(totalRequiredNumericalDigits)
+
+	return precisionBits, err
+}
