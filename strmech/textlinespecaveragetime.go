32a5014918ef82340006668491db7c4defb817d1
Mike Rapp <jordy.rascal@protonmail.com>
2022-12-02 00:48:27 -0600

    Added new method TextLineSpecAverageTime.CopyOutPtr()

--- strmech/textlinespecaveragetime.go
+++ strmech/textlinespecaveragetime.go
@@ -287,6 +287,137 @@ func (txtLineAvgTime *TextLineSpecAverageTime) CopyOut(
 	return deepCopyTxtLineAvgTimer, err
 }
 
+//	CopyOutPtr
+//
+//	Returns a pointer to a deep copy of the current
+//	TextLineSpecAverageTime instance.
+//
+//	If the current TextLineSpecAverageTime instance
+//	contains invalid member variable data values, this
+//	method will return an error.
+//
+// ----------------------------------------------------------------
+//
+//	# Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it	contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	*TextLineSpecAverageTime
+//
+//		If this method completes successfully, this
+//		parameter will return a pointer to a deep copy of
+//		the current TextLineSpecAverageTime instance.
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (txtLineAvgTime *TextLineSpecAverageTime) CopyOutPtr(
+	errorPrefix interface{}) (
+	*TextLineSpecAverageTime,
+	error) {
+
+	if txtLineAvgTime.lock == nil {
+		txtLineAvgTime.lock = new(sync.Mutex)
+	}
+
+	txtLineAvgTime.lock.Lock()
+
+	defer txtLineAvgTime.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	deepCopyTxtLineAvgTimer := TextLineSpecAverageTime{}
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextLineSpecAverageTime.CopyOutPtr()",
+		"")
+
+	if err != nil {
+		return &deepCopyTxtLineAvgTimer, err
+	}
+
+	err = new(textLineSpecAverageTimeNanobot).copy(
+		&deepCopyTxtLineAvgTimer,
+		txtLineAvgTime,
+		ePrefix.XCpy("deepCopyTxtLineAvgTimer<-"+
+			"txtLineAvgTime"))
+
+	return &deepCopyTxtLineAvgTimer, err
+}
+
 //	New
 //
 //	Returns an initialized instance of
