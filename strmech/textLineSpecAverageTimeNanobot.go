18a086552cc36c91147d17977c6ea799c567d01c
Mike Rapp <jordy.rascal@protonmail.com>
2022-12-01 22:53:44 -0600

    Added new method TextLineSpecAverageTime.CopyIn()

--- strmech/textLineSpecAverageTimeNanobot.go
+++ strmech/textLineSpecAverageTimeNanobot.go
@@ -13,6 +13,80 @@ type textLineSpecAverageTimeNanobot struct {
 	lock *sync.Mutex
 }
 
+//	copy
+//
+//	Copies all data from input parameter 'sourceAvgTimer'
+//	to input parameter 'destinationAvgTimer'.
+//
+//	Be advised that the pre-existing data fields in input
+//	parameter 'destinationAvgTimer' will be overwritten and
+//	deleted.
+//
+// ----------------------------------------------------------------
+//
+// Input Parameters
+//
+//	destinationAvgTimer			*TextLineSpecAverageTime
+//
+//		A pointer to an instance of
+//		TextLineSpecAverageTime. Data extracted from input
+//		parameter 'incomingTimerLines' will be copied to
+//		this input parameter, 'destinationAvgTimer'. If
+//		this method completes successfully, all member
+//		data variables encapsulated in 'destinationAvgTimer'
+//		will be identical to those contained in input
+//		parameter 'sourceAvgTimer'.
+//
+//		Be advised that the pre-existing data fields in
+//		input parameter 'destinationAvgTimer' will be
+//		overwritten and deleted.
+//
+//	sourceAvgTimer				*TextLineSpecAverageTime
+//
+//		A pointer to an instance of TextLineSpecTimerLines.
+//
+//		All data values in this TextLineSpecAverageTime
+//		instance will be copied to input parameter
+//		'destinationAvgTimer'.
+//
+//		The original member variable data values
+//		encapsulated in 'sourceAvgTimer' will remain
+//		unchanged and will NOT be overwritten or deleted.
+//
+//		If 'sourceAvgTimer' contains invalid member data
+//		variables, this method will return an error.
+//
+//	errPrefDto					*ePref.ErrPrefixDto
+//
+//		This object encapsulates an error prefix string
+//		which is included in all returned error
+//		messages. Usually, it contains the name of the
+//		calling method or methods listed as a function
+//		chain.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		Type ErrPrefixDto is included in the 'errpref'
+//		software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'. If
+//		errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message.
+//
+//		If an error message is returned, the text value
+//		for input parameter 'errPrefDto' (error prefix)
+//		will be prefixed or attached at the beginning of
+//		the error message.
 func (txtLineAvgTimeNanobot *textLineSpecAverageTimeNanobot) copy(
 	destinationAvgTimer *TextLineSpecAverageTime,
 	sourceAvgTimer *TextLineSpecAverageTime,
--- strmech/textlinespecaveragetime.go
+++ strmech/textlinespecaveragetime.go
@@ -1,6 +1,7 @@
 package strmech
 
 import (
+	ePref "github.com/MikeAustin71/errpref"
 	"math/big"
 	"sync"
 )
@@ -21,6 +22,138 @@ type TextLineSpecAverageTime struct {
 	numberOfDurationEvents big.Int
 	totalDurationNanoSecs  big.Int
 	lock                   *sync.Mutex
+}
+
+//	CopyIn
+//
+//	Copies all the data fields from an incoming instance
+//	of TextLineSpecAverageTime ('incomingAvgTimer') to
+//	the data fields of the current TextLineSpecAverageTime
+//	instance ('txtLineAvgTime').
+//
+// ----------------------------------------------------------------
+//
+// # IMPORTANT
+//
+//	This method will delete and overwrite all pre-existing
+//	data values in the current instance of
+//	TextLineSpecAverageTime.
+//
+// ----------------------------------------------------------------
+//
+//	# Input Parameters
+//
+//	incomingAvgTimer			*TextLineSpecAverageTime
+//
+//		A pointer to an instance of TextLineSpecAverageTime.
+//		All data value contained in this instance will be
+//		copied to current instance of
+//		TextLineSpecAverageTime, 'txtLineAvgTime'.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it	contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (txtLineAvgTime *TextLineSpecAverageTime) CopyIn(
+	incomingAvgTimer *TextLineSpecAverageTime,
+	errorPrefix interface{}) error {
+
+	if txtLineAvgTime.lock == nil {
+		txtLineAvgTime.lock = new(sync.Mutex)
+	}
+
+	txtLineAvgTime.lock.Lock()
+
+	defer txtLineAvgTime.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextLineSpecAverageTime.CopyIn()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	return new(textLineSpecAverageTimeNanobot).copy(
+		txtLineAvgTime,
+		incomingAvgTimer,
+		ePrefix.XCpy("txtLineAvgTime<-"))
 }
 
 //	New
