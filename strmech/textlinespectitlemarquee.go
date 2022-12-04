ed6ea62d52f21073b8f7da0d9741646aabb4a9a0
Mike Rapp <jordy.rascal@protonmail.com>
2022-12-04 15:13:51 -0600

    Added new method TextLineSpecTitleMarquee.CopyOut()

--- strmech/textlinespectitlemarquee.go
+++ strmech/textlinespectitlemarquee.go
@@ -163,6 +163,139 @@ func (txtLineSpecTitleMarquee *TextLineSpecTitleMarquee) CopyIn(
 					"incomingTitleMarquee"))
 }
 
+//	CopyOut
+//
+//	Returns a deep copy of the current
+//	TextLineSpecTitleMarquee instance.
+//
+//	If the current TextLineSpecTitleMarquee instance
+//	contains invalid member variables, this method will
+//	return an error.
+//
+// ----------------------------------------------------------------
+//
+//	# Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it	contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	TextLineSpecTitleMarquee
+//
+//		If this method completes successfully, a deep
+//		copy of the current TextLineSpecTitleMarquee
+//		instance will be returned through this parameter.
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (txtLineSpecTitleMarquee *TextLineSpecTitleMarquee) CopyOut(
+	errorPrefix interface{}) (
+	TextLineSpecTitleMarquee,
+	error) {
+
+	if txtLineSpecTitleMarquee.lock == nil {
+		txtLineSpecTitleMarquee.lock = new(sync.Mutex)
+	}
+
+	txtLineSpecTitleMarquee.lock.Lock()
+
+	defer txtLineSpecTitleMarquee.lock.Unlock()
+
+	deepCopyTitleMarquee := TextLineSpecTitleMarquee{}
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextLineSpecTitleMarquee."+
+			"CopyOut()",
+		"")
+
+	if err != nil {
+		return deepCopyTitleMarquee, err
+	}
+
+	err = new(textLineSpecTitleMarqueeNanobot).
+		copy(&deepCopyTitleMarquee,
+			txtLineSpecTitleMarquee,
+			ePrefix.XCpy(
+				"deepCopyTitleMarquee<-"+
+					"txtLineSpecTitleMarquee"))
+
+	return deepCopyTitleMarquee, err
+}
+
 //	NewAllParams
 //
 //	Creates and returns a new instance of
@@ -239,6 +372,13 @@ func (txtLineSpecTitleMarquee *TextLineSpecTitleMarquee) CopyIn(
 //		A collection of text line objects containing all
 //		specifications necessary to display the Text
 //		Title Lines.
+//
+//		If this parameter is empty with zero Text Line
+//		member elements, no error will be returned.
+//		However, the user will be responsible for
+//		populating the title lines using the 'Add'
+//		methods on the returned instance of
+//		TextLineSpecTitleMarquee.
 //
 //	numBottomTitleBlankLines	int
 //
