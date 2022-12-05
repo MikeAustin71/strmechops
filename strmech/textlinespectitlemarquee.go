87105b7481461f68e2d600fea1d23257339c3db6
Mike Rapp <jordy.rascal@protonmail.com>
2022-12-04 20:30:48 -0600

    Added new method TextLineSpecTitleMarquee.IsValidInstanceError()

--- strmech/textlinespectitlemarquee.go
+++ strmech/textlinespectitlemarquee.go
@@ -869,6 +869,183 @@ func (txtLineSpecTitleMarquee *TextLineSpecTitleMarquee) GetFormattedText(
 	return strBuilder.String(), err
 }
 
+//	IsValidInstance
+//
+//	Performs a diagnostic review of the data values
+//	encapsulated in the current TextLineSpecTitleMarquee
+//	instance to determine if they are valid.
+//
+//	If all data element evaluate as valid, this method
+//	returns 'true'. If any data element is invalid, this
+//	method returns 'false'.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	--- NONE ---
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	isValid						bool
+//
+//		If all data elements encapsulated by the current
+//		instance of TextLineSpecTitleMarquee are valid,
+//		this returned boolean value is set to 'true'. If
+//		any data values are invalid, this return
+//		parameter is set to 'false'.
+func (txtLineSpecTitleMarquee *TextLineSpecTitleMarquee) IsValidInstance() (
+	isValid bool) {
+
+	if txtLineSpecTitleMarquee.lock == nil {
+		txtLineSpecTitleMarquee.lock = new(sync.Mutex)
+	}
+
+	txtLineSpecTitleMarquee.lock.Lock()
+
+	defer txtLineSpecTitleMarquee.lock.Unlock()
+
+	isValid,
+		_ = new(textLineSpecTitleMarqueeElectron).
+		testValidityTitleMarquee(
+			txtLineSpecTitleMarquee,
+			nil)
+
+	return isValid
+}
+
+//	IsValidInstanceError
+//
+//	Performs a diagnostic review of the data values
+//	encapsulated in the current TextLineSpecTitleMarquee
+//	instance to determine if they are valid.
+//
+//	If any data element evaluates as invalid, this method
+//	will return an error.
+//
+//	This method fulfills requirements of
+//	ITextLineSpecification interface.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it	contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//
+//		If the current instance of
+//		TextLineSpecTitleMarquee is judged to be valid in
+//		all respects, the returned error Type is set
+//		equal to 'nil'.
+//
+//		If input parameter 'TextLineSpecTitleMarquee' is
+//		found to be invalid, the returned error Type will
+//		encapsulate an appropriate error message.
+//
+//		If an error message is returned, the text value
+//		for input parameter 'errorPrefix' (error prefix)
+//		will be prefixed or attached at the beginning of
+//		the error message.
+func (txtLineSpecTitleMarquee *TextLineSpecTitleMarquee) IsValidInstanceError(
+	errorPrefix interface{}) error {
+
+	if txtLineSpecTitleMarquee.lock == nil {
+		txtLineSpecTitleMarquee.lock = new(sync.Mutex)
+	}
+
+	txtLineSpecTitleMarquee.lock.Lock()
+
+	defer txtLineSpecTitleMarquee.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextLineSpecTitleMarquee."+
+			"IsValidInstanceError()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	_,
+		err = new(textLineSpecTitleMarqueeElectron).
+		testValidityTitleMarquee(
+			txtLineSpecTitleMarquee,
+			ePrefix.XCpy(
+				"txtLineSpecTitleMarquee"))
+
+	return err
+}
+
 //	NewAllParams
 //
 //	Creates and returns a new instance of
