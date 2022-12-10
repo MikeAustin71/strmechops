88d6295c4273f9335c9b64dd67bc3749775e9307
Mike Rapp <jordy.rascal@protonmail.com>
2022-12-10 14:47:39 -0600

    Added new method TextBigFloatFieldFormatDto.IsValidInstanceError()

--- strmech/textbigfloatfieldformatdto.go
+++ strmech/textbigfloatfieldformatdto.go
@@ -1149,6 +1149,134 @@ func (textBigFloatFieldFmtDto *TextBigFloatFieldFormatDto) IsValidInstance() (
 	return isValid
 }
 
+// IsValidInstanceError
+//
+// Performs a diagnostic review of the data values
+// encapsulated in the current TextBigFloatFieldFormatDto
+// instance to determine if they are valid.
+//
+// If any data element evaluates as invalid, this method
+// will return an error.
+//
+// This method is required in order to implement the
+// ITextFieldFormatDto interface.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it	contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If any of the internal member data variables
+//		contained in the current instance of
+//		TextBigFloatFieldFormatDto are found to be
+//		invalid, this method will return an error
+//		containing an appropriate error message.
+//
+//		If an error message is returned, the returned
+//		error message will incorporate the method chain
+//		and text passed by input parameter, 'errorPrefix'.
+//		The 'errorPrefix' text will be attached to the
+//		beginning of the error message.
+func (textBigFloatFieldFmtDto *TextBigFloatFieldFormatDto) IsValidInstanceError(
+	errorPrefix interface{}) error {
+
+	if textBigFloatFieldFmtDto.lock == nil {
+		textBigFloatFieldFmtDto.lock = new(sync.Mutex)
+	}
+
+	textBigFloatFieldFmtDto.lock.Lock()
+
+	defer textBigFloatFieldFmtDto.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextBigFloatFieldFormatDto."+
+			"CopyIn()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	_,
+		err = new(textBigFloatFieldFormatDtoAtom).
+		testValidityOfTxtBigFloatFieldFmtDto(
+			textBigFloatFieldFmtDto,
+			ePrefix.XCpy(
+				"textBigFloatFieldFmtDto"))
+
+	return err
+}
+
 // textBigFloatFieldFormatDtoNanobot
 //
 // Provides helper methods for TextBigFloatFieldFormatDto.
