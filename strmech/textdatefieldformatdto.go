304e7999ce4d4f5a4e9e3eeb76d56a7663d8cab5
Mike Rapp <jordy.rascal@protonmail.com>
2022-12-09 21:13:22 -0600

    Added new method TextDateFieldFormatDto.GetFormattedTextFieldStr()

--- strmech/textdatefieldformatdto.go
+++ strmech/textdatefieldformatdto.go
@@ -708,6 +708,145 @@ func (textDateFieldFormatDto *TextDateFieldFormatDto) GetFieldFormatDtoType() st
 	return "TextDateFieldFormatDto"
 }
 
+// GetFormattedTextFieldStr
+//
+// Returns a string containing the formatted text field
+// generated from the current instance of
+// TextDateFieldFormatDto.
+//
+// The returned formatted text field string contains the
+// left margin, field contents and right margin.
+//
+// This method is required in order to implement the
+// ITextFieldFormatDto interface.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it	contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	string
+//
+//		If this method completes successfully, the text
+//		field specifications contained in the current
+//		instance of TextDateFieldFormatDto will be
+//		converted to, and returned as, a formatted text
+//		field string.
+//
+//		The returned text field string will contain the
+//		left margin, text field contents and right margin
+//		as those elements are defined in the current
+//		instance of TextDateFieldFormatDto.
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (textDateFieldFormatDto *TextDateFieldFormatDto) GetFormattedTextFieldStr(
+	errorPrefix interface{}) (
+	string,
+	error) {
+
+	if textDateFieldFormatDto.lock == nil {
+		textDateFieldFormatDto.lock = new(sync.Mutex)
+	}
+
+	textDateFieldFormatDto.lock.Lock()
+
+	defer textDateFieldFormatDto.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextDateFieldFormatDto."+
+			"GetFormattedTextFieldStr()",
+		"")
+
+	if err != nil {
+		return "", err
+	}
+
+	return new(textDateFieldFormatDtoNanobot).
+		getFormattedTextFieldStr(
+			textDateFieldFormatDto,
+			ePrefix.XCpy(
+				"textDateFieldFormatDto"))
+}
+
 // textDateFieldFormatDtoNanobot - Provides helper
 // methods for TextDateFieldFormatDto.
 type textDateFieldFormatDtoNanobot struct {
