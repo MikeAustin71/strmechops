7a65797223b37da86dbfb5e67beb456c5b684356
Mike Rapp <jordy.rascal@protonmail.com>
2022-12-09 19:53:32 -0600

    Added new method TextDateFieldFormatDto.CopyIn()

--- strmech/textdatefieldformatdto.go
+++ strmech/textdatefieldformatdto.go
@@ -155,6 +155,153 @@ type TextDateFieldFormatDto struct {
 	lock *sync.Mutex
 }
 
+// CopyIn
+//
+// Copies all the data fields from an incoming instance
+// of TextDateFieldFormatDto
+// ('incomingTxtDateFieldFmtDto') to the corresponding
+// data fields of the current TextDateFieldFormatDto
+// instance ('textDateFieldFormatDto').
+//
+// ----------------------------------------------------------------
+//
+// # IMPORTANT
+//
+//	This method will delete and overwrite all
+//	pre-existing data values contained within the
+//	current instance of TextDateFieldFormatDto
+//	('textDateFieldFormatDto').
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	incomingTxtDateFieldFmtDto		*TextDateFieldFormatDto
+//
+//		A pointer to an instance of
+//		TextDateFieldFormatDto.
+//
+//		All the internal data field values in this
+//		instance will be copied to corresponding data
+//		fields of the current TextDateFieldFormatDto
+//		instance.
+//
+//		The data fields contained in
+//		'incomingTxtDateFieldFmtDto' will NOT be
+//		changed or modified.
+//
+//		If 'incomingTxtDateFieldFmtDto' contains
+//		invalid data values, an error will be returned.
+//
+//	errorPrefix						interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it	contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (textDateFieldFormatDto *TextDateFieldFormatDto) CopyIn(
+	incomingTxtDateFieldFmtDto *TextDateFieldFormatDto,
+	errorPrefix interface{}) error {
+
+	if textDateFieldFormatDto.lock == nil {
+		textDateFieldFormatDto.lock = new(sync.Mutex)
+	}
+
+	textDateFieldFormatDto.lock.Lock()
+
+	defer textDateFieldFormatDto.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextDateFieldFormatDto."+
+			"CopyIn()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	return new(textDateFieldFormatDtoNanobot).
+		copy(textDateFieldFormatDto,
+			incomingTxtDateFieldFmtDto,
+			ePrefix.XCpy(
+				"textDateFieldFormatDto"+
+					"<-incomingTxtDateFieldFmtDto"))
+}
+
 // textDateFieldFormatDtoNanobot - Provides helper
 // methods for TextDateFieldFormatDto.
 type textDateFieldFormatDtoNanobot struct {
