7b8c535ce048af9007e6d6cfce35dc61ff3faf14
Mike Rapp <jordy.rascal@protonmail.com>
2022-12-10 13:26:05 -0600

    Added new method TextBigFloatFieldFormatDto.CopyIn()

--- strmech/textbigfloatfieldformatdto.go
+++ strmech/textbigfloatfieldformatdto.go
@@ -163,6 +163,154 @@ type TextBigFloatFieldFormatDto struct {
 	lock *sync.Mutex
 }
 
+// CopyIn
+//
+// Copies all the data fields from an incoming instance
+// of TextBigFloatFieldFormatDto
+// ('incomingTxtBigFloatFieldFmtDto') to the corresponding
+// data fields of the current TextBigFloatFieldFormatDto
+// instance ('textBigFloatFieldFmtDto').
+//
+// ----------------------------------------------------------------
+//
+// # IMPORTANT
+//
+//	This method will delete and overwrite all
+//	pre-existing data values contained within the
+//	current instance of TextBigFloatFieldFormatDto
+//	('textBigFloatFieldFmtDto').
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	incomingTxtBigFloatFieldFmtDto		*TextBigFloatFieldFormatDto
+//
+//		A pointer to an instance of
+//		TextBigFloatFieldFormatDto.
+//
+//		All the internal data field values in this
+//		instance will be copied to corresponding data
+//		fields of the current TextBigFloatFieldFormatDto
+//		instance.
+//
+//		The data fields contained in
+//		'incomingTxtFieldFmtDto' will NOT be changed or
+//		modified.
+//
+//		If 'incomingTxtFieldFmtDto' contains invalid data
+//		values, an error will be returned.
+//
+//	errorPrefix						interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it	contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (textBigFloatFieldFmtDto *TextBigFloatFieldFormatDto) CopyIn(
+	incomingTxtBigFloatFieldFmtDto *TextBigFloatFieldFormatDto,
+	errorPrefix interface{}) error {
+
+	if textBigFloatFieldFmtDto.lock == nil {
+		textBigFloatFieldFmtDto.lock = new(sync.Mutex)
+	}
+
+	textBigFloatFieldFmtDto.lock.Lock()
+
+	defer textBigFloatFieldFmtDto.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextFillerFieldFormatDto."+
+			"CopyIn()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	return new(textBigFloatFieldFormatDtoNanobot).copy(
+		textBigFloatFieldFmtDto,
+		incomingTxtBigFloatFieldFmtDto,
+		ePrefix.XCpy(
+			"textBigFloatFieldFmtDto<-"+
+				"incomingTxtBigFloatFieldFmtDto"))
+}
+
 // GetPureNumberStr
 //
 // Returns a pure number string representing the floating
