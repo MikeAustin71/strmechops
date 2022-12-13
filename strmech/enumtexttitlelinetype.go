dbfb71d6ccbf63fd547c58e1e3eb21391167fb89
Mike Rapp <jordy.rascal@protonmail.com>
2022-12-12 19:30:21 -0600

    Added new maps to enum type TextTileLineType.

--- strmech/enumtexttitlelinetype.go
+++ strmech/enumtexttitlelinetype.go
@@ -86,3 +86,123 @@ func (txtTitleLineType TextTileLineType) TrailingMarqueeLine() TextTileLineType 
 	return TextTileLineType(3)
 
 }
+
+// String
+//
+// Returns a string with the name of the enumeration
+// associated with this instance of TextTileLineType.
+//
+// This is a standard utility method and is not part
+// of the valid enumerations for this type.
+//
+// ----------------------------------------------------------------
+//
+// Usage
+//
+//	t:= TextTileLineType(0).TrailingMarqueeLine()
+//	str := t.String()
+//	   str is now equal to 'TrailingMarqueeLine'
+func (txtTitleLineType TextTileLineType) String() string {
+
+	lockTextTitleLineType.Lock()
+
+	defer lockTextTitleLineType.Unlock()
+
+	result, ok :=
+		mapTextTitleLineTypeCodeToString[txtTitleLineType]
+
+	if !ok {
+		return "Error: TextTileLineType code UNKNOWN!"
+	}
+
+	return result
+}
+
+// XIsValid
+//
+// Returns a boolean value signaling whether the current
+// TextTileLineType value is valid.
+//
+// Be advised, the enumeration value "None" is considered
+// NOT VALID.
+//
+// This is a standard utility method and is not part of the valid
+// enumerations for this type.
+//
+// ------------------------------------------------------------------------
+//
+// Usage
+//
+//	titleLineVal := TextTileLineType(0).TrailingMarqueeLine()
+//
+//	isValid := titleLineVal.XIsValid() // isValid == true
+//
+//	titleLineVal = TextTileLineType(0).None()
+//
+//	isValid = titleLineVal.XIsValid() // isValid == false
+func (txtTitleLineType TextTileLineType) XIsValid() bool {
+
+	lockTextTitleLineType.Lock()
+
+	defer lockTextTitleLineType.Unlock()
+
+	return new(textTileLineTypeNanobot).
+		isValidTextTitleLineType(
+			txtTitleLineType)
+}
+
+// TitleLineType
+//
+// A public global constant of type TextTileLineType.
+//
+// This variable serves as an easier, shorthand technique
+// for accessing TextTileLineType values.
+//
+// Usage:
+// TitleLineType.None(),
+// TitleLineType.LeadingMarqueeLine(),
+// TitleLineType.TitleLine(),
+// TitleLineType.TrailingMarqueeLine(),
+const TitleLineType = TextTileLineType(0)
+
+// textTileLineTypeNanobot
+//
+// Provides helper methods for enumeration
+// TextTileLineType.
+type textTileLineTypeNanobot struct {
+	lock *sync.Mutex
+}
+
+// isValidTextTitleLineType
+//
+// Receives an instance of TextTileLineType and returns a
+// boolean value signaling whether that TextTileLineType
+// instance is valid.
+//
+// If the passed instance of TextTileLineType is valid,
+// this method returns 'true'.
+//
+// Be advised, the enumeration value "None" is considered
+// NOT VALID. "None" represents an error condition.
+//
+// This is a standard utility method and is not part of
+// the valid TextTileLineType enumeration.
+func (txtTileLineTypeNanobot *textTileLineTypeNanobot) isValidTextTitleLineType(
+	txtTileLineType TextTileLineType) bool {
+
+	if txtTileLineTypeNanobot.lock == nil {
+		txtTileLineTypeNanobot.lock = new(sync.Mutex)
+	}
+
+	txtTileLineTypeNanobot.lock.Lock()
+
+	defer txtTileLineTypeNanobot.lock.Unlock()
+
+	if txtTileLineType < 1 ||
+		txtTileLineType > 3 {
+
+		return false
+	}
+
+	return true
+}
