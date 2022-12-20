5d56f6f5378097ce237c7367631e3ae673f0e6e2
Mike Rapp <jordy.rascal@protonmail.com>
2022-12-19 23:55:01 -0600

    Add new method TextFieldFormatDtoFloat64.CopyOut()

--- strmech/textfieldformatdtofloat64.go
+++ strmech/textfieldformatdtofloat64.go
@@ -575,6 +575,149 @@ func (txtFieldFmtDtoFloat64 *TextFieldFormatDtoFloat64) CopyIn(
 			"txtFieldFmtDtoFloat64<-incomingTxtFloat64FmtDto"))
 }
 
+// CopyOut
+//
+// Returns a deep copy of the current
+// TextFieldFormatDtoFloat64 instance.
+//
+// If the current TextFieldFormatDtoFloat64 instance
+// contains invalid member variable data values, this
+// method will return an error.
+//
+// ----------------------------------------------------------------
+//
+// # BE ADVISED
+//
+//	The original member variable data values encapsulated
+//	within the current TextFieldFormatDtoFloat64
+//	instance will NOT BE changed or modified.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of
+//		the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	TextFieldFormatDtoFloat64
+//
+//		If this method completes successfully and no
+//		errors are encountered, this parameter will
+//		return a deep copy of the current
+//		TextFieldFormatDtoFloat64 instance.
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (txtFieldFmtDtoFloat64 *TextFieldFormatDtoFloat64) CopyOut(
+	errorPrefix interface{}) (
+	TextFieldFormatDtoFloat64,
+	error) {
+
+	if txtFieldFmtDtoFloat64.lock == nil {
+		txtFieldFmtDtoFloat64.lock = new(sync.Mutex)
+	}
+
+	txtFieldFmtDtoFloat64.lock.Lock()
+
+	defer txtFieldFmtDtoFloat64.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	var err error
+
+	newTxtFloat64FmtDto :=
+		TextFieldFormatDtoFloat64{}
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"TextFieldFormatDtoFloat64."+
+			"CopyOut()",
+		"")
+
+	if err != nil {
+		return newTxtFloat64FmtDto, err
+	}
+
+	err = new(textFieldFormatDtoFloat64Nanobot).copy(
+		&newTxtFloat64FmtDto,
+		txtFieldFmtDtoFloat64,
+		ePrefix.XCpy(
+			"newTxtFloat64FmtDto<-txtFieldFmtDtoFloat64"))
+
+	return newTxtFloat64FmtDto, err
+}
+
 // textFieldFormatDtoFloat64Nanobot
 //
 // Provides helper methods for TextFieldFormatDtoFloat64.
