5103fe68ca3a0b57e62dae99e34dc6f3ea332bf4
Mike Rapp <jordy.rascal@protonmail.com>
2023-01-12 17:27:17 -0600

    Added new method numStrNumberSymbolSpecMechanics.setUSSignedNumMinusDefaults()

--- strmech/numstrnumbersymbolspec.go
+++ strmech/numstrnumbersymbolspec.go
@@ -2976,23 +2976,12 @@ func (nStrNumberSymbolSpec *NumStrNumberSymbolSpec) NewUSSignedNumMinusDefaults(
 
 	}
 
-	numStrNumSymSpecNanobot := numStrNumberSymbolSpecNanobot{}
-
-	numStrNumSymSpecNanobot.empty(
-		&positiveSignedNumberSymbols)
-
-	numStrNumSymSpecNanobot.empty(
-		&zeroSignedNumberSymbols)
-
-	numStrNumSymSpecNanobot.empty(
-		&negativeSignedNumberSymbols)
-
-	err = numStrNumSymSpecNanobot.setLeadingNStrNumSymbolSpec(
-		&negativeSignedNumberSymbols,
-		[]rune{'-'},
-		NumFieldSymPos.InsideNumField(),
-		ePrefix.XCpy(
-			"negativeSignedNumberSymbols"))
+	err = new(numStrNumberSymbolSpecMechanics).
+		setUSSignedNumMinusDefaults(
+			&positiveSignedNumberSymbols,
+			&zeroSignedNumberSymbols,
+			&negativeSignedNumberSymbols,
+			ePrefix)
 
 	return positiveSignedNumberSymbols,
 		zeroSignedNumberSymbols,
@@ -4990,6 +4979,181 @@ func (nStrNumberSymbolSpec *NumStrNumberSymbolSpec) SetTrailingNumberSymbolRunes
 			ePrefix.XCpy(
 				"nStrNumberSymbolSpec<-"+
 					"trailingNumberSymbol"))
+
+	return err
+}
+
+// numStrNumberSymbolSpecMechanics - This type provides
+// helper methods for NumStrNumberSymbolSpec
+type numStrNumberSymbolSpecMechanics struct {
+	lock *sync.Mutex
+}
+
+// setUSSignedNumMinusDefaults
+//
+// Reconfigures three NumStrNumberSymbolSpec instances
+// with default signed number symbols commonly applied in
+// the United States (US).
+//
+// This method applies default number symbol
+// specifications configuring negative numeric values
+// with leading minus signs ('-').
+//
+//	Example: -123.34
+//
+// These number symbol specifications are designed to
+// format number strings containing signed numeric
+// values. Currency symbols are not supported or
+// included in the configured number symbol
+// specifications.
+//
+// The three configured NumStrNumberSymbolSpec instances
+// are therefore configured with US specifications for
+// positive signed number symbols, zero value symbols,
+// and negative signed number symbols.
+//
+// The positive signed number symbol is configured as
+// empty or blank because under United States formatting
+// standards, positive number signs are implied and not
+// specifically displayed. Therefore, no leading plus
+// ('+') symbol is required.
+//
+// Likewise, the zero signed number symbol is also
+// configured as empty or blank because under United
+// States formatting standards, zero numeric values have
+// no number sign symbols.
+//
+// The negative signed number symbol is configured with a
+// leading minus sign ('-') meaning that all negative
+// numeric values will be prefixed with a leading minus
+// sign ('-'). The negative number sign will be
+// positioned inside the number field:
+//
+//	NumFieldSymPos.InsideNumField()
+//		Example:
+//			Number Field Length: 9
+//			Numeric Value: 123.45
+//			Number Symbol: leading minus sign ('-')
+//			Number Symbol Position: Inside Number Field
+//			Formatted Number String: "  -123.45"
+//			Number Field Index:       012345678
+//			Total Number String Length: 9
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	positiveSignedNumberSymbols *NumStrNumberSymbolSpec
+//
+//		This instance of NumStrNumberSymbolSpec will be
+//		configured with signed number symbols associated
+//		with positive numeric values.
+//
+//		The positive signed number symbol is configured
+//		as empty or blank because under United States
+//		formatting standards, positive number signs are
+//		implied and not specifically displayed.
+//		Therefore, no leading plus ('+') symbol is
+//		required.
+//
+//	zeroSignedNumberSymbols		*NumStrNumberSymbolSpec
+//
+//		This instance of NumStrNumberSymbolSpec will be
+//		configured with signed number symbols associated
+//		with zero numeric values.
+//
+//		The zero signed number symbol is configured as
+//		empty or blank because under United States
+//		formatting standards, zero numeric values have
+//		no number sign symbols.
+//
+//	negativeSignedNumberSymbols *NumStrNumberSymbolSpec
+//
+//		This instance of NumStrNumberSymbolSpec will be
+//		configured with signed number symbols associated
+//		with negative numeric values.
+//
+//		Negative numeric values will be	configured with
+//		leading minus signs ('-') in accordance with US
+//		number string formatting standards.
+//
+//	errPrefDto					*ePref.ErrPrefixDto
+//
+//		This object encapsulates an error prefix string
+//		which is included in all returned error
+//		messages. Usually, it contains the name of the
+//		calling method or methods listed as a function
+//		chain.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		Type ErrPrefixDto is included in the 'errpref'
+//		software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'. If
+//		errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message.
+//
+//		If an error message is returned, the text value
+//		for input parameter 'errPrefDto' (error prefix)
+//		will be prefixed or attached at the beginning of
+//		the error message.
+func (nStrNumSymSpecMech *numStrNumberSymbolSpecMechanics) setUSSignedNumMinusDefaults(
+	positiveSignedNumberSymbols *NumStrNumberSymbolSpec,
+	zeroSignedNumberSymbols *NumStrNumberSymbolSpec,
+	negativeSignedNumberSymbols *NumStrNumberSymbolSpec,
+	errPrefDto *ePref.ErrPrefixDto) error {
+
+	if nStrNumSymSpecMech.lock == nil {
+		nStrNumSymSpecMech.lock = new(sync.Mutex)
+	}
+
+	nStrNumSymSpecMech.lock.Lock()
+
+	defer nStrNumSymSpecMech.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
+		errPrefDto,
+		"numStrNumberSymbolSpecMechanics."+
+			"setUSSignedNumMinusDefaults()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	numStrNumSymSpecNanobot := numStrNumberSymbolSpecNanobot{}
+
+	numStrNumSymSpecNanobot.empty(
+		positiveSignedNumberSymbols)
+
+	numStrNumSymSpecNanobot.empty(
+		zeroSignedNumberSymbols)
+
+	numStrNumSymSpecNanobot.empty(
+		negativeSignedNumberSymbols)
+
+	err = numStrNumSymSpecNanobot.setLeadingNStrNumSymbolSpec(
+		negativeSignedNumberSymbols,
+		[]rune{'-'},
+		NumFieldSymPos.InsideNumField(),
+		ePrefix.XCpy(
+			"negativeSignedNumberSymbols"))
 
 	return err
 }
