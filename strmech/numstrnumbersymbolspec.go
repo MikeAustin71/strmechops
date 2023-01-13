48ff16843615c1ca7d2e7ae8d309d086e74ab554
Mike Rapp <jordy.rascal@protonmail.com>
2023-01-13 00:50:48 -0600

    Added new method NumStrNumberSymbolSpec.NewEUCurrencyDefaults()

--- strmech/numstrnumbersymbolspec.go
+++ strmech/numstrnumbersymbolspec.go
@@ -1019,6 +1019,136 @@ func (nStrNumberSymbolSpec *NumStrNumberSymbolSpec) IsNOP() bool {
 	return false
 }
 
+// NewEUCurrencyDefaults
+//
+// Creates and returns a new instance of
+// NumStrNumberSymbolSpec configured with the default
+// currency symbol used by the European Union (EU).
+//
+// The default EU currency symbol is a trailing Euro
+// sign ('€').
+//
+//	Example:
+//		123.45 €
+//
+// The Number String Number Symbol Specification type
+// (NumStrNumberSymbolSpec) is designed to assist in
+// formatting numeric values as number strings for
+// screen displays, printing or file output.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix						interface{}
+//
+//		This object encapsulates error prefix text which is
+//		included in all returned error messages. Usually, it
+//		contains the name of the calling method or methods
+//		listed as a method or function chain of execution.
+//
+//		If no error prefix information is needed, set this
+//		parameter to 'nil'.
+//
+//		This empty interface must be convertible to one of the
+//		following types:
+//
+//		1. nil - A nil value is valid and generates an empty
+//		   collection of error prefix and error context
+//		   information.
+//
+//		2. string - A string containing error prefix information.
+//
+//		3. []string A one-dimensional slice of strings containing
+//		   error prefix information
+//
+//		4. [][2]string A two-dimensional slice of strings
+//		   containing error prefix and error context information.
+//
+//		5. ErrPrefixDto - An instance of ErrPrefixDto. Information
+//		   from this object will be copied for use in error and
+//		   informational messages.
+//
+//		6. *ErrPrefixDto - A pointer to an instance of ErrPrefixDto.
+//		   Information from this object will be copied for use in
+//		   error and informational messages.
+//
+//		7. IBasicErrorPrefix - An interface to a method generating
+//		   a two-dimensional slice of strings containing error
+//		   prefix and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible to one of
+//		the valid types listed above, it will be considered
+//		invalid and trigger the return of an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are included in
+//		the 'errpref' software package,
+//		"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	euCurrencySymbols			NumStrNumberSymbolSpec
+//
+//		If this method completes successfully, an
+//		instance of NumStrNumberSymbolSpec will be
+//		returned configured with the default EU
+//		(European Union) Currency Symbol.
+//
+//
+//		The default EU currency symbol is a trailing Euro
+//		sign ('€').
+//
+//			Example:
+//				123.45 €
+//
+//	err								error
+//
+//		If this method completes successfully and no errors are
+//		encountered this return value is set to 'nil'. Otherwise,
+//		if errors are encountered, this return value will contain
+//		an appropriate error message.
+//
+//		If an error message is returned, the text value of input
+//		parameter 'errorPrefix' will be inserted or prefixed at
+//		the beginning of the error message.
+func (nStrNumberSymbolSpec *NumStrNumberSymbolSpec) NewEUCurrencyDefaults(
+	errorPrefix interface{}) (
+	euCurrencySymbols NumStrNumberSymbolSpec,
+	err error) {
+
+	if nStrNumberSymbolSpec.lock == nil {
+		nStrNumberSymbolSpec.lock = new(sync.Mutex)
+	}
+
+	nStrNumberSymbolSpec.lock.Lock()
+
+	defer nStrNumberSymbolSpec.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"NumStrNumberSymbolSpec."+
+			"NewEUCurrencyDefaults()",
+		"")
+
+	if err != nil {
+		return euCurrencySymbols, err
+
+	}
+
+	err = new(numStrNumberSymbolSpecMechanics).
+		setCurrencyEUDefaults(
+			&euCurrencySymbols,
+			ePrefix.XCpy(
+				"euCurrencySymbols"))
+
+	return euCurrencySymbols, err
+}
+
 // NewLeadingNumberSymbol - Creates and returns a new instance
 // of NumStrNumberSymbolSpec configured with a leading
 // number symbol character or characters.
