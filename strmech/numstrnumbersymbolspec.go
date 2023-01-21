a13d1f0dd8f7804acddcac95f09553ee69611b8b
Mike Rapp <jordy.rascal@protonmail.com>
2023-01-21 03:25:28 -0600

    Added new method NumStrNumberSymbolSpec.SetCurrencyDefaultsUS().

--- strmech/numstrnumbersymbolspec.go
+++ strmech/numstrnumbersymbolspec.go
@@ -7703,6 +7703,143 @@ func (nStrNumberSymbolSpec *NumStrNumberSymbolSpec) SetCurrencyDefaultsUKMinusOu
 				"nStrNumberSymbolSpec"))
 }
 
+// SetCurrencyDefaultsUS
+//
+// Deletes and reconfigures the data values in the
+// current instance of NumStrNumberSymbolSpec with
+// default currency specifications commonly applied
+// in the United States (US).
+//
+// The currency symbol used in the United States is
+// the leading Dollar sign symbol ('$').
+//
+//	Example:
+//		$ 125
+//
+// ----------------------------------------------------------------
+//
+// # IMPORTANT
+//
+//	This method will delete, overwrite and reset all
+//	pre-existing data values in the current instance of
+//	NumStrNumberSymbolSpec. All data values will be
+//	configured with default currency specifications
+//	commonly applied in the United States.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (nStrNumberSymbolSpec *NumStrNumberSymbolSpec) SetCurrencyDefaultsUS(
+	errorPrefix interface{}) error {
+
+	if nStrNumberSymbolSpec.lock == nil {
+		nStrNumberSymbolSpec.lock = new(sync.Mutex)
+	}
+
+	nStrNumberSymbolSpec.lock.Lock()
+
+	defer nStrNumberSymbolSpec.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"NumStrNumberSymbolSpec."+
+			"SetCurrencyDefaultsUS()",
+		"")
+
+	if err != nil {
+		return err
+
+	}
+
+	return new(numStrNumberSymbolSpecMechanics).
+		setCurrencyDefaultsUS(
+			nStrNumberSymbolSpec,
+			ePrefix.XCpy(
+				"nStrNumberSymbolSpec"))
+
+}
+
 // SetCurrencyLeadingSymbol
 //
 // Resets and configures a Leading Currency Symbol
