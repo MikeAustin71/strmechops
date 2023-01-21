a8644ef8ded4f899bf461239702cc5aba306fd55
Mike Rapp <jordy.rascal@protonmail.com>
2023-01-21 02:09:19 -0600

    Added new method NumStrNumberSymbolSpec.SetCurrencyDefaultsUKMinusInside().

--- strmech/numstrnumbersymbolspec.go
+++ strmech/numstrnumbersymbolspec.go
@@ -1873,7 +1873,7 @@ func (nStrNumberSymbolSpec *NumStrNumberSymbolSpec) NewCurrencyDefaultsEU(
 // formatting numeric values as number strings for
 // screen displays, printing or file output.
 //
-// The default Uk currency symbol is a leading pound
+// The default UK currency symbol is a leading pound
 // sign ('£').
 //
 //	Example:
@@ -7421,6 +7421,144 @@ func (nStrNumberSymbolSpec *NumStrNumberSymbolSpec) SetCurrencyDefaultEU(
 
 	return new(numStrNumberSymbolSpecMechanics).
 		setCurrencyDefaultsEU(
+			nStrNumberSymbolSpec,
+			ePrefix.XCpy(
+				"nStrNumberSymbolSpec"))
+}
+
+//	SetCurrencyDefaultsUKMinusInside
+//
+//	Deletes and reconfigures the data values in the
+//	current instance of NumStrNumberSymbolSpec with
+//	the default Currency Symbol specification applied
+//	in the United Kingdom (UK).
+//
+//	The default UK currency symbol is a leading pound
+//	sign ('£').
+//
+//		Example:
+//			£ 123.45	Positive Value
+//
+//	The phrase "MinusInside" in the method name signals
+//	that the minus sign will be placed to the right of,
+//	or inside, the pound sign ('£').
+//
+//		Example:
+//			£ -123.45	Negative Value
+//
+//	To generate a default configuration where the minus
+//	sign is outside the pound sign ('£'), use method:
+//
+//	NumStrNumberSymbolSpec.SetCurrencyDefaultsUKMinusOutside()
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (nStrNumberSymbolSpec *NumStrNumberSymbolSpec) SetCurrencyDefaultsUKMinusInside(
+	errorPrefix interface{}) error {
+
+	if nStrNumberSymbolSpec.lock == nil {
+		nStrNumberSymbolSpec.lock = new(sync.Mutex)
+	}
+
+	nStrNumberSymbolSpec.lock.Lock()
+
+	defer nStrNumberSymbolSpec.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"NumStrNumberSymbolSpec."+
+			"SetCurrencyDefaultsUKMinusInside()",
+		"")
+
+	if err != nil {
+		return err
+
+	}
+
+	return new(numStrNumberSymbolSpecMechanics).
+		setCurrencyDefaultsUKMinusInside(
 			nStrNumberSymbolSpec,
 			ePrefix.XCpy(
 				"nStrNumberSymbolSpec"))
