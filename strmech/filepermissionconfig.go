f0965dc0618f16f68c522b95fb09074eaa29caf0
Mike Rapp <jordy.rascal@protonmail.com>
2023-02-14 17:54:48 -0600

    Added new method FilePermissionConfig.GetIsRegularFile().

--- strmech/filepermissionconfig.go
+++ strmech/filepermissionconfig.go
@@ -355,7 +355,9 @@ func (fPerm *FilePermissionConfig) Equal(fPerm2 *FilePermissionConfig) bool {
 //		text will be attached to the beginning of the
 //		error message.
 func (fPerm *FilePermissionConfig) GetIsDir(
-	errorPrefix interface{}) (bool, error) {
+	errorPrefix interface{}) (
+	bool,
+	error) {
 
 	if fPerm.lock == nil {
 		fPerm.lock = new(sync.Mutex)
@@ -390,6 +392,147 @@ func (fPerm *FilePermissionConfig) GetIsDir(
 	}
 
 	return fPerm.fileMode.IsDir(), nil
+}
+
+// GetIsRegularFile
+//
+// Returns a boolean value signaling whether the
+// FileMode contained in the current instance of
+// FilePermissionConfig is a regular file or not.
+//
+// A returned value of 'true' signals that the
+// FileMode represents a file.
+//
+// This method serves as a wrapper for method
+// os.FileMode.IsRegular()
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	bool
+//
+//		This returned boolean value signals whether the
+//		FileMode contained in the current instance of
+//		FilePermissionConfig is a regular file or not.
+//
+//		A returned value of 'true' signals that the
+//		FileMode represents a file.
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (fPerm *FilePermissionConfig) GetIsRegularFile(
+	errorPrefix interface{}) (
+	bool,
+	error) {
+
+	if fPerm.lock == nil {
+		fPerm.lock = new(sync.Mutex)
+	}
+
+	fPerm.lock.Lock()
+
+	defer fPerm.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"FilePermissionConfig."+
+			"GetIsRegularFile()",
+		"")
+
+	if err != nil {
+		return false, err
+	}
+
+	_,
+		err = new(filePermissionConfigElectron).
+		testValidityOfFilePermissionConfig(
+			fPerm,
+			ePrefix)
+
+	if err != nil {
+
+		return false, err
+	}
+
+	return fPerm.fileMode.IsRegular(), nil
 }
 
 // GetEntryTypeComponent
