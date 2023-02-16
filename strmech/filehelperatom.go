1a09d392d7f636b6d0d107b8b82611bb6bce4327
Mike Rapp <jordy.rascal@protonmail.com>
2023-02-15 21:54:00 -0600

    Refactored method FileHelper.ChangeWorkingDir().

--- strmech/filehelperatom.go
+++ strmech/filehelperatom.go
@@ -1,6 +1,8 @@
 package strmech
 
 import (
+	"fmt"
+	ePref "github.com/MikeAustin71/errpref"
 	"os"
 	fp "path/filepath"
 	"strings"
@@ -78,4 +80,163 @@ func (fHelperAtom *fileHelperAtom) adjustPathSlash(
 	}
 
 	return fp.FromSlash(path)
+}
+
+// changeWorkingDir
+//
+// Changes the current working directory to the named
+// directory passed by input parameter, 'dirPath'.
+//
+// If there is an error, it will be of type *PathError.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	dirPath						string
+//
+//		The name of the new current working directory.
+//		The existing current working directory will be
+//		changed to this directory path specification.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an error
+//		message. This returned error message will
+//		incorporate the method chain and text passed by
+//		input parameter, 'errorPrefix'. The 'errorPrefix'
+//		text will be attached to the beginning of the
+//		error message.
+func (fHelperAtom *fileHelperAtom) changeWorkingDir(
+	dirPath string,
+	errorPrefix interface{}) error {
+
+	if fHelperAtom.lock == nil {
+		fHelperAtom.lock = new(sync.Mutex)
+	}
+
+	fHelperAtom.lock.Lock()
+
+	defer fHelperAtom.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"fileHelperAtom."+
+			"changeWorkingDir()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	errCode := 0
+
+	errCode, _, dirPath =
+		new(fileHelperElectron).isStringEmptyOrBlank(dirPath)
+
+	if errCode == -1 {
+
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'dirPath' is an empty string!\n",
+			ePrefix.String())
+
+		return err
+	}
+
+	if errCode == -2 {
+
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter 'dirPath' consists of blank spaces!\n",
+			ePrefix.String())
+
+		return err
+	}
+
+	var err2 error
+
+	err2 = os.Chdir(dirPath)
+
+	if err2 != nil {
+
+		err = fmt.Errorf("%v\n"+
+			"Error returned by os.Chdir(dirPath).\n"+
+			"dirPath='%v'\n"+
+			"Error='%v'\n",
+			ePrefix.String(),
+			dirPath,
+			err2.Error())
+	}
+
+	return err
 }
