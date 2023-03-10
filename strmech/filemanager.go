f3290cadb07035e69e5666d26bbbf15c7102b5f0
Mike Rapp <jordy.rascal@protonmail.com>
2023-03-09 21:54:02 -0600

    Refactored method FileMgr.CreateDirAndFile()

--- strmech/filemanager.go
+++ strmech/filemanager.go
@@ -3694,19 +3694,104 @@ func (fMgr *FileMgr) CreateDir(
 	return err
 }
 
-// CreateDirAndFile - Performs two operations:
+// CreateDirAndFile
 //
-//	If the home directory does not currently exist, this method
-//	will first create the directory tree containing the file.
+// Performs two operations:
 //
-// Next, the file will be created. If the file previously exists,
-// it will be truncated.
+// If the home directory does not currently exist, this
+// method will first create the directory tree containing
+// the file.
 //
-// Note that if this method successfully creates the file, a
-// File Pointer (*File) will be stored in the FileMgr field
-// fMgr.filePtr. Be sure to close the File Pointer when
-// finished with it. See FileMgr.CloseThisFile().
-func (fMgr *FileMgr) CreateDirAndFile() error {
+// Next, the file will be created. If the file previously
+// exists, it will be truncated.
+//
+// Note that if this method successfully creates the
+// file, a File Pointer (*File) will be stored in the
+// FileMgr field fMgr.filePtr. Be sure to close the
+// File Pointer when finished with it. See
+// FileMgr.CloseThisFile().
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
+func (fMgr *FileMgr) CreateDirAndFile(
+	errorPrefix interface{}) error {
 
 	if fMgr.lock == nil {
 		fMgr.lock = new(sync.Mutex)
@@ -3716,17 +3801,24 @@ func (fMgr *FileMgr) CreateDirAndFile() error {
 
 	defer fMgr.lock.Unlock()
 
-	ePrefix := "FileMgr:CreateDirAndFile() "
+	var ePrefix *ePref.ErrPrefixDto
 	var err error
 
-	fMgrHlpr := fileMgrHelper{}
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"FileMgr."+
+			"CreateDirAndFile()",
+		"")
 
-	err = fMgrHlpr.createFile(
+	if err != nil {
+		return err
+	}
+
+	return new(fileMgrHelper).createFile(
 		fMgr,
 		true,
 		ePrefix)
-
-	return err
 }
 
 // CreateThisFile - Creates the File identified by FileMgr
