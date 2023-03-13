7d04761658f69c13d1d94a659f5e99606820c166
Mike Rapp <jordy.rascal@protonmail.com>
2023-03-12 22:42:54 -0500

    Refactored method FileMgr.MoveFileToFileStr()

--- strmech/filemanager.go
+++ strmech/filemanager.go
@@ -7156,20 +7156,137 @@ func (fMgr *FileMgr) MoveFileToFileMgr(
 				"fMgr<-destinationFMgr"))
 }
 
-// MoveFileToFileStr - This method will move the file identified
-// by the current FileMgr to a new path and file name contained
-// in the input parameter 'pathFileNameExt' which is of type
-// string.
+// MoveFileToFileStr
 //
-// IMPORTANT:
+// This method will move the file identified by the
+// current FileMgr instance to a new path and file name
+// specified by the input parameter string,
+// 'pathFileNameExt'.
 //
-// The file identified by the current FileMgr object will be
-// DELETED!
+// ----------------------------------------------------------------
 //
-// The new file is defined by input parameter 'pathFileNameExt'.
+// # IMPORTANT
 //
-// If the input parameter 'pathFileNameExt' contains a directory path
-// which currently does NOT exist, it will be created.
+// If this method completes successfully, the following
+// outcomes will be generated:
+//
+//	(1)	The file identified by the current instance of
+//		FileMgr will be copied to the path and file name
+//		defined by input parameter 'pathFileNameExt'.
+//
+//	(2) The original file identified by the current
+//		FileMgr object will be DELETED!
+//
+//	(3)	A new FileMgr instance containing the file name
+//		of the new created file specified by input
+//		parameter 'pathFileNameExt' will be returned.
+//
+//	(4) The original current instance of FileMgr will be
+//		rendered invalid because its file no longer
+//		exists on disk.
+//
+//	(5)	If the input parameter 'pathFileNameExt' contains
+//		a directory path which currently does NOT exist,
+//		this method will attempt to create it.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	pathFileNameExt				string
+//
+//		Contains the destination path, file name and file
+//		extension. The file identified by the current
+//		instance of FileMgr will be moved to the location
+//		identified by input parameter 'pathFileNameExt'.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	FileMgr
+//
+//		If this method completes successfully, the
+//		file identified by the current instance of
+//		FileMgr will be moved the path, file name and
+//		file extension specified by input parameter
+//		'pathFileNameExt'.
+//
+//		This returned instance of FileMgr will define
+//		the newly moved file identified by
+//		'pathFileNameExt'.
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
 func (fMgr *FileMgr) MoveFileToFileStr(
 	pathFileNameExt string,
 	errorPrefix interface{}) (
