ea87e958b5bf4e8099af65b41ffdc0b9614cd858
Mike Rapp <jordy.rascal@protonmail.com>
2023-03-15 21:52:22 -0500

    Refactored method FileMgr.ResetFileInfo()

--- strmech/filemanager.go
+++ strmech/filemanager.go
@@ -10339,10 +10339,110 @@ func (fMgr *FileMgr) ReadFileString(
 //
 // Acquires the current os.FileInfo data associated with
 // the file identified by the current FileMgr instance.
+// This new information is then stored in an internal
+// member variable contained in the current instance of
+// FileMgr. Any previous os.FileInfo data stored in this
+// internal member variable will be deleted, overwritten
+// and reset.
 //
-// An error will be triggered if the file path does NOT
-// exist!
-func (fMgr *FileMgr) ResetFileInfo() error {
+// The internal member variable used to store
+// FileInfoPlus data is FileMgr.fMgr.actualFileInfo.
+//
+// FileInfo data for the current instance of FileMgr
+// can be retrieved using one of the following methods:
+//
+//	FileMgr.GetFileInfo()
+//	FileMgr.GetFileInfoPlus()
+//
+// ----------------------------------------------------------------
+//
+// # BE ADVISED
+//
+// If the directory, file name and file extension
+// specified by the current FileMgr instance does not
+// exist, an error will be returned.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
+func (fMgr *FileMgr) ResetFileInfo(
+	errorPrefix interface{}) error {
 
 	if fMgr.lock == nil {
 		fMgr.lock = new(sync.Mutex)
@@ -10359,7 +10459,7 @@ func (fMgr *FileMgr) ResetFileInfo() error {
 
 	ePrefix,
 		err = ePref.ErrPrefixDto{}.NewIEmpty(
-		nil,
+		errorPrefix,
 		"FileMgr."+
 			"ResetFileInfo()",
 		"")
@@ -10368,6 +10468,7 @@ func (fMgr *FileMgr) ResetFileInfo() error {
 		return err
 	}
 
+	// Sets fMgr.actualFileInfo
 	filePathDoesExist,
 		err = new(fileMgrHelperAtom).
 		doesFileMgrPathFileExist(
