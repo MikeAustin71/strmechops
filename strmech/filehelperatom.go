7fcd193da11aa17f249cd7dd08ac0de0305fcc92
Mike Rapp <jordy.rascal@protonmail.com>
2023-03-16 21:53:58 -0500

    Refactored method fileHelperAtom.changeWorkingDir()

--- strmech/filehelperatom.go
+++ strmech/filehelperatom.go
@@ -292,63 +292,19 @@ func (fHelperAtom *fileHelperAtom) createFile(
 //		The existing current working directory will be
 //		changed to this directory path specification.
 //
-//	errorPrefix					interface{}
+//	errPrefDto					*ePref.ErrPrefixDto
 //
-//		This object encapsulates error prefix text which
-//		is included in all returned error messages.
-//		Usually, it contains the name of the calling
-//		method or methods listed as a method or function
-//		chain of execution.
+//		This object encapsulates an error prefix string
+//		which is included in all returned error
+//		messages. Usually, it contains the name of the
+//		calling method or methods listed as a function
+//		chain.
 //
 //		If no error prefix information is needed, set
 //		this parameter to 'nil'.
 //
-//		This empty interface must be convertible to one
-//		of the following types:
-//
-//		1.	nil
-//				A nil value is valid and generates an
-//				empty collection of error prefix and
-//				error context information.
-//
-//		2.	string
-//				A string containing error prefix
-//				information.
-//
-//		3.	[]string
-//				A one-dimensional slice of strings
-//				containing error prefix information.
-//
-//		4.	[][2]string
-//				A two-dimensional slice of strings
-//		   		containing error prefix and error
-//		   		context information.
-//
-//		5.	ErrPrefixDto
-//				An instance of ErrPrefixDto.
-//				Information from this object will
-//				be copied for use in error and
-//				informational messages.
-//
-//		6.	*ErrPrefixDto
-//				A pointer to an instance of
-//				ErrPrefixDto. Information from
-//				this object will be copied for use
-//				in error and informational messages.
-//
-//		7.	IBasicErrorPrefix
-//				An interface to a method
-//				generating a two-dimensional slice
-//				of strings containing error prefix
-//				and error context information.
-//
-//		If parameter 'errorPrefix' is NOT convertible
-//		to one of the valid types listed above, it will
-//		be considered invalid and trigger the return of
-//		an error.
-//
-//		Types ErrPrefixDto and IBasicErrorPrefix are
-//		included in the 'errpref' software package:
+//		Type ErrPrefixDto is included in the 'errpref'
+//		software package:
 //			"github.com/MikeAustin71/errpref".
 //
 // ----------------------------------------------------------------
@@ -369,7 +325,7 @@ func (fHelperAtom *fileHelperAtom) createFile(
 //		error message.
 func (fHelperAtom *fileHelperAtom) changeWorkingDir(
 	dirPath string,
-	errorPrefix interface{}) error {
+	errPrefDto *ePref.ErrPrefixDto) error {
 
 	if fHelperAtom.lock == nil {
 		fHelperAtom.lock = new(sync.Mutex)
@@ -384,7 +340,7 @@ func (fHelperAtom *fileHelperAtom) changeWorkingDir(
 
 	ePrefix,
 		err = ePref.ErrPrefixDto{}.NewIEmpty(
-		errorPrefix,
+		errPrefDto,
 		"fileHelperAtom."+
 			"changeWorkingDir()",
 		"")
