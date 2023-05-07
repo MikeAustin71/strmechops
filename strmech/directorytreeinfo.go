df4e8d89191625062810f4c2be2ea48e3c2f8e5b
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-07 17:15:54 -0500

    Refactored DirectoryTreeInfo.CopyToDirectoryTree() again.

--- strmech/directorytreeinfo.go
+++ strmech/directorytreeinfo.go
@@ -31,6 +31,144 @@ type DirectoryTreeInfo struct {
 //
 // The copy operation includes all files and all directories in the designated directory
 // tree.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	baseDir						DirMgr
+//
+//		A concrete instance of DirMgr. The directory path
+//		contained in this instance will be used as the source
+//		for the directory tree which will be copied to the
+//		directory path contained in 'newBaseDir'.
+//
+//	newBaseDir					DirMgr
+//
+//		A concrete instance of DirMgr. The directory path
+//		contained in this instance will be used as the
+//		destination path for the directory tree from the
+//		'baseDir' directory path.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	DirectoryTreeInfo
+//
+//		If this method completes successfully, without
+//		errors, a fully populated instance of
+//		DirectoryTreeInfo is returned.
+//
+//		This structure contains detailed information on
+//		files processed during copy operation.
+//
+//	          type DirectoryTreeInfo struct {
+//
+//	            StartPath             string
+//					The starting path or directory for the file
+//	                search.
+//
+//	            Directories           DirMgrCollection
+//					Directory Managers found during directory tree
+//					search.
+//
+//					This collection will ALWAYS return the parent
+//					directory ('DirMgr') as the first entry in the
+//					collection.
+//
+//	            FoundFiles            FileMgrCollection
+//					Found Files matching file selection criteria
+//
+//	            ErrReturns            []error
+//					Internal System errors encountered
+//
+//	            FileSelectCriteria    FileSelectionCriteria
+//	            	The File Selection Criteria submitted as an
+//					input parameter to this method.
+//	           }
+//
+//	        If successful, files matching the file selection criteria input
+//	        parameter shown above will be returned in a 'DirectoryTreeInfo'
+//	        object. The field 'DirectoryTreeInfo.FoundFiles' contains information
+//	        on all the files in the specified directory tree which match the file selection
+//	        criteria.
+//
+//	        Note: It is a good idea to check the returned field 'DirectoryTreeInfo.ErrReturns'
+//	              to determine if any internal system errors were encountered while processing
+//	              the directory tree.
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
 func (dirTree *DirectoryTreeInfo) CopyToDirectoryTree(
 	baseDir DirMgr,
 	newBaseDir DirMgr,
