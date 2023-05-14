2aa0786ff3f133d50e89a93e2508d426490c9040
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-13 22:40:48 -0500

    Refactored method FileOps.GetDestination().

--- strmech/fileoperations.go
+++ strmech/fileoperations.go
@@ -568,10 +568,131 @@ func (fops *FileOps) GetSource(
 			"fops.source"))
 }
 
-// GetDestination - Returns a deep copy of the
-// destination FileMgr instance.
-func (fops *FileOps) GetDestination() FileMgr {
-	return fops.destination.CopyOut()
+// GetDestination
+//
+// Returns a deep copy of the destination FileMgr
+// instance.
+//
+// The internal member variable which is copied and
+// returned is identified as:
+//
+//	FileOps.destination
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	FileMgr
+//
+//		If this method completes successfully, this
+//		parameter will return a deep copy of the
+//	 	'destination' File Manager encapsulated in the
+//	 	current instance of FileOps.
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
+func (fops *FileOps) GetDestination(
+	errorPrefix interface{}) (
+	FileMgr,
+	error) {
+
+	if fops.lock == nil {
+		fops.lock = new(sync.Mutex)
+	}
+
+	fops.lock.Lock()
+
+	defer fops.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"FileOps."+
+			"GetDestination()",
+		"")
+
+	if err != nil {
+		return FileMgr{}, err
+	}
+
+	return fops.destination.CopyOut(
+		ePrefix.XCpy("fops.destination"))
 }
 
 // NewByFileMgrs - Creates and returns a new FileOps
