52f7acdde2ac88febde9e5ad32e995d125d1f469
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-14 01:47:17 -0500

    Refactored method FileOps.NewByFileMgrs().

--- strmech/fileoperations.go
+++ strmech/fileoperations.go
@@ -700,9 +700,112 @@ func (fops *FileOps) GetDestination(
 // Creates and returns a new FileOps instance based on
 // input parameters 'source' and 'destination' File
 // Managers.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	sourceFMgr					FileMgr
+//
+//		A concrete instance of FileMgr which is used
+//		to configure the source File Manager contained
+//		in the returned instance of FileOps.
+//
+//	destinationFMgr				FileMgr
+//
+//		A concrete instance of FileMgr which is used
+//		to configure the destination File Manager
+//		contained in the returned instance of FileOps.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	FileOps
+//
+//		If this method completes successfully, this
+//		parameter will return a new, and properly
+//		initialized instance, of FileOps.
+//
+//		Data values for this new instance will be
+//		constructed from the source and destination File
+//		Managers passed as input parameters 'sourceFMgr'
+//		and 'destinationFMgr'.
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
 func (fops FileOps) NewByFileMgrs(
-	source FileMgr,
-	destination FileMgr,
+	sourceFMgr FileMgr,
+	destinationFMgr FileMgr,
 	errorPrefix interface{}) (
 	FileOps,
 	error) {
@@ -734,8 +837,8 @@ func (fops FileOps) NewByFileMgrs(
 	err = new(FileOperationsAtom).
 		setFileOps(
 			&fOpsNew,
-			source,
-			destination,
+			sourceFMgr,
+			destinationFMgr,
 			ePrefix.XCpy("fOpsNew"))
 
 	return fOpsNew, nil
