5bae16ae11c4f98b3253c52e92079d9b2d5cf1b8
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-15 22:22:27 -0500

    Added new method FileOps.CopyIn().

--- strmech/fileoperations.go
+++ strmech/fileoperations.go
@@ -49,6 +49,133 @@ type FileOps struct {
 	lock *sync.Mutex
 }
 
+// CopyIn
+//
+// This method receives a pointer to an incoming instance
+// of FileOps and proceeds to copy all the encapsulated
+// data values to corresponding data elements contained
+// in the current instance of FileOps.
+//
+// The copy operation performed is a "deep" copy.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	incomingFOps				FileOps
+//
+//		A concrete instance of FileOps. All the internal
+//		member variable data values will be copied to the
+//		corresponding data elements contained in the
+//		current instance of FileOps.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
+func (fops *FileOps) CopyIn(
+	incomingFOps FileOps,
+	errorPrefix interface{}) error {
+
+	if fops.lock == nil {
+		fops.lock = new(sync.Mutex)
+	}
+
+	fops.lock.Lock()
+
+	defer fops.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"FileOps."+
+			"CopyIn()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	return new(FileOperationsNanobot).copyIn(
+		fops,
+		&incomingFOps,
+		ePrefix.XCpy("fops<-incomingFOps"))
+}
+
 // CopyOut
 //
 // Returns a deep copy of the current FileOps instance.
