b27fc71790a8d84204f4bb5857caf196411264e6
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-16 22:52:54 -0500

    Refactored method FileOperationsElectron.testValidityOfFileOps().

--- strmech/fileoperationselectron.go
+++ strmech/fileoperationselectron.go
@@ -320,6 +320,193 @@ func (fOpsElectron *FileOperationsElectron) empty(
 	return
 }
 
+// testValidityOfFileOps
+//
+// Receives a pointer to an instance of FileOps and
+// performs a diagnostic analysis to determine if that
+// instance is valid in all respects.
+//
+// If the input parameter 'fOps' is determined to be
+// invalid, this method will return a boolean flag
+// ('isValid') of 'false'. In addition, an instance of
+// type error ('err') will be returned configured with an
+// appropriate error message.
+//
+// If the input parameter 'fOps' is valid, this method
+// will return a boolean flag ('isValid') of 'true' and
+// the returned error type ('err') will be set to 'nil'.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	fOps						*FileOps
+//
+//		A pointer to an instance of FileOps. This object
+//		will be subjected to diagnostic analysis in order
+//		to determine if all the member variables contain
+//		valid values.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	isValid						bool
+//
+//		If input parameter 'fOps' is judged to be valid
+//		in all respects, this return parameter will be
+//		set to 'true'.
+//
+//		If input parameter 'fOps' is found to be invalid,
+//		this return parameter will be set to 'false'.
+//
+//	error
+//
+//		If input parameter 'fOps' is judged to be valid
+//		in all respects, this return parameter will be
+//		set to 'nil'.
+//
+//		If input parameter, 'fOps' is found to be
+//		invalid, this return parameter will be configured
+//		with an appropriate error message.
+//
+//		If an error message is returned, the text value
+//		for input parameter 'errPrefDto' (error prefix)
+//		will be prefixed or attached at the beginning of
+//		the error message.
+func (fOpsElectron *FileOperationsElectron) testValidityOfFileOps(
+	fOps *FileOps,
+	errPrefDto *ePref.ErrPrefixDto) (
+	bool,
+	error) {
+
+	if fOpsElectron.lock == nil {
+		fOpsElectron.lock = new(sync.Mutex)
+	}
+
+	fOpsElectron.lock.Lock()
+
+	defer fOpsElectron.lock.Unlock()
+
+	var err error
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	funcName := "FileOperationsElectron." +
+		"testValidityOfFileOps()"
+
+	var isValid bool
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
+		errPrefDto,
+		funcName,
+		"")
+
+	if err != nil {
+		return isValid, err
+	}
+
+	if fOps == nil {
+		err = fmt.Errorf("%v\n"+
+			"Error: FileOps instance is invalid!\n"+
+			"Input parameter 'fOps' is a nil pointer.\n",
+			ePrefix.String())
+
+		return isValid, err
+	}
+
+	fMgrHelperAtom := new(fileMgrHelperAtom)
+
+	err = fMgrHelperAtom.isFileMgrValid(
+		&fOps.source,
+		ePrefix.XCpy("fOps.source,"))
+
+	if err != nil {
+		return isValid, err
+	}
+
+	err = fMgrHelperAtom.isFileMgrValid(
+		&fOps.destination,
+		ePrefix.XCpy("fOps.source,"))
+
+	if err != nil {
+		return isValid, err
+	}
+
+	err = fOps.opToExecute.IsValid()
+
+	if err != nil {
+
+		err = nil
+
+		fOps.opToExecute = FileOpCode.None()
+	}
+
+	isValid = true
+
+	return isValid, err
+}
+
 // setFileOpsDestination
 //
 // Reconfigures the 'destination' File Manager
