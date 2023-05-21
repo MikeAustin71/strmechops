14557a007ddf22570bc60655e832dff4dd918e07
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-20 23:22:18 -0500

    Added new method DirMgrCollection.CopyIn().

--- strmech/dirmgrcollection.go
+++ strmech/dirmgrcollection.go
@@ -896,6 +896,149 @@ func (dMgrs *DirMgrCollection) AddDirMgrCollection(
 	return err
 }
 
+//	CopyIn
+//
+//	Copies the Directory Manager Collection from an
+//	incoming instance of DirMgrCollection
+//	('incomingDMgrCollection')	to the Directory Manager
+//	Collection maintained by the current DirMgrCollection
+//	instance ('dMgrs').
+//
+// ----------------------------------------------------------------
+//
+// # IMPORTANT
+//
+//	All the member variable data values in the current
+//	DirMgrCollection instance ('dMgrs') will be deleted
+//	and overwritten.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	incomingDMgrCollection 		*DirMgrCollection
+//
+//		A pointer to an instance of DirMgrCollection.
+//		This method will NOT change the values of
+//		internal member variables contained in this
+//		instance.
+//
+//		The Directory Manager Collection contained in
+//		this DirMgrCollection instance will be copied to
+//		the current DirMgrCollection instance ('dMgrs').
+//
+//		If 'incomingDMgrCollection' contains an empty
+//		Directory Manager Collection, an error will be
+//		returned.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
+func (dMgrs *DirMgrCollection) CopyIn(
+	incomingDMgrCollection *DirMgrCollection,
+	errorPrefix interface{}) error {
+
+	if dMgrs.lock == nil {
+		dMgrs.lock = new(sync.Mutex)
+	}
+
+	dMgrs.lock.Lock()
+
+	defer dMgrs.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"DirMgrCollection.CopyIn()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	return new(dirMgrCollectionHelper).
+		copyCollection(
+			dMgrs,
+			incomingDMgrCollection,
+			ePrefix.XCpy(
+				"dMgrs<-incomingDMgrCollection"))
+}
+
 // CopyOut
 //
 // Returns an DirMgrCollection which is an exact
@@ -1018,10 +1161,10 @@ func (dMgrs *DirMgrCollection) CopyOut(
 
 	err = new(dirMgrCollectionHelper).
 		copyCollection(
-			dMgrs,
 			&dMgrs2,
+			dMgrs,
 			ePrefix.XCpy(
-				"dMgrs<-dMgrs2"))
+				"dMgrs2<-dMgrs"))
 
 	return dMgrs2, err
 }
