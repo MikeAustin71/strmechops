7e86cb1e59eca17dd15605f95d27ba2be0f7c5da
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-24 22:45:03 -0500

    Refactored method DirMgr.DeleteDirFilesStats()

--- strmech/dirmanager.go
+++ strmech/dirmanager.go
@@ -1026,23 +1026,118 @@ func (dMgr *DirMgr) DeleteAll(
 			ePrefix)
 }
 
-// DeleteAllFilesInDir - Deletes all the files in the current directory.
-// ONLY files in the current directory are deleted. Subdirectories are
-// NOT deleted and files in subdirectories are NOT deleted.
+// DeleteAllFilesInDir
+//
+// ----------------------------------------------------------------
+//
+// # WARNING
+//
+// This method deletes all the files in the directory
+// identified by the current instance of DirMgr.
+//
+// ONLY files in the top level directory identified
+// by the current instance of DirNgr are deleted.
+// Subdirectories are NOT deleted and files in
+// subdirectories are NOT deleted.
 //
 // Reference:
 // https://stackoverflow.com/questions/33450980/golang-remove-all-contents-of-a-directory
 //
-// ---------------------------------------------------------------------------
+// ----------------------------------------------------------------
 //
-// Return Value:
+// # Input Parameters
 //
-//	errs     []error  - An array of errors is returned. If the method completes
-//	                    successfully with no errors, a ZERO-length array is
-//	                    returned.
+//	errorPrefix					interface{}
 //
-//	                    If errors are encountered they are stored in the error
-//	                    array and returned to the caller.
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	deleteDirStats				DeleteDirFilesStats
+//
+//		If this method completes successfully, this
+//		return parameter will be populated with
+//		information and statistics on the file deletion
+//		operation. This information includes the number
+//		of files deleted.
+//
+//			type DeleteDirFilesStats struct {
+//				TotalFilesProcessed        uint64
+//				FilesDeleted               uint64
+//				FilesDeletedBytes          uint64
+//				FilesRemaining             uint64
+//				FilesRemainingBytes        uint64
+//				TotalSubDirectories        uint64
+//				TotalDirsScanned           uint64
+//				NumOfDirsWhereFilesDeleted uint64
+//				DirectoriesDeleted         uint64
+//			}
+//
+//	errs						[]error
+//
+//	An array of errors is returned. If this method
+//	completes successfully with no errors, a
+//	ZERO-length array is returned.
+//
+//	If errors are encountered they are stored in the
+//	error array and returned to the caller.
 func (dMgr *DirMgr) DeleteAllFilesInDir(
 	errorPrefix interface{}) (
 	deleteDirStats DeleteDirFilesStats,
