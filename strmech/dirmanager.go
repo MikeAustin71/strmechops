a29ac90f1b739dc6b8550b1dddb50284d7bbe896
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-28 00:41:51 -0500

    Refactored method DirMgr.GetNumberOfAbsPathElements().

--- strmech/dirmanager.go
+++ strmech/dirmanager.go
@@ -7775,9 +7775,121 @@ func (dMgr *DirMgr) GetDirPermissionCodes(
 	return fPermCfg, err
 }
 
-// GetNumberOfAbsPathElements - Returns the number of elements
-// or path components in the absolute path of the current
-// Directory Manager instance.
+// GetNumberOfAbsPathElements
+//
+// Returns the number of elements or path components in
+// the absolute path of the current Directory Manager
+// instance (DirMgr).
+//
+// ----------------------------------------------------------------
+//
+// # Definition of Terms
+//
+// An absolute or full path points to the same location
+// in a file system, regardless of the current working
+// directory. To do that, it must include the root
+// directory.
+//
+//	https://en.wikipedia.org/wiki/Path_(computing)#Absolute_and_relative_paths
+//
+// ----------------------------------------------------------------
+//
+// # Example
+//
+//					Number of Path Elements
+//	                1   2    3    4    5
+//		DirMgr Path = "D:\ADir\BDir\CDir\EDir"
+//
+//		The number of path elements returned by this method
+//		given the absolute path specified above is '5'.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	int
+//
+//		This method returns the number of path elements
+//		in the absolute path specified by the current
+//		instance of DirMgr.
+//
+//		See the example shown above.
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
 func (dMgr *DirMgr) GetNumberOfAbsPathElements(
 	errorPrefix interface{}) (
 	int,
