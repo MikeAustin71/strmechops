08fc980deb55735b4725c6be70934d6ad55e6af7
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-29 01:35:25 -0500

    Refactored method ValidPathStrDto.SetPathDoesExist().

--- strmech/filevalidpathstrdto.go
+++ strmech/filevalidpathstrdto.go
@@ -586,38 +586,174 @@ func (vpDto *ValidPathStrDto) SetAbsPathDoesExistStatus(
 // SetAbsPathFileInfo - "setter" method for internal data field
 // vpDto.absPathFInfoPlus .
 func (vpDto *ValidPathStrDto) SetAbsPathFileInfo(absFInfo FileInfoPlus) {
+
+	if vpDto.lock == nil {
+		vpDto.lock = new(sync.Mutex)
+	}
+
+	vpDto.lock.Lock()
+
+	defer vpDto.lock.Unlock()
+
 	vpDto.absPathFInfoPlus = absFInfo.CopyOut()
 }
 
 // SetOriginalPathStr - "setter" method for internal data field
 // vpDto.originalPathStr .
 func (vpDto *ValidPathStrDto) SetOriginalPathStr(originalPathStr string) {
+
+	if vpDto.lock == nil {
+		vpDto.lock = new(sync.Mutex)
+	}
+
+	vpDto.lock.Lock()
+
+	defer vpDto.lock.Unlock()
+
 	vpDto.originalPathStr = originalPathStr
 }
 
-// SetPathDoesExist - "setter" method for internal data field
+// SetPathDoesExist
+//
+// This is a "setter" method for internal data field
 // vpDto.pathDoesExist which is of type PathExistsStatusCode.
 //
-//	                                      Int
-//	   Enumeration                       Value         Definition
-//	----------------------------------------------------------------------------
-//	PathExistsStatusCode(0).Unknown()     -1    Path file existence has NOT been
-//	                                            tested and status is 'Unknown'.
+// ----------------------------------------------------------------
 //
-//	PathExistsStatusCode(0).DoesNotExist() 0    Path file existence HAS been tested
-//	                                            and path file name does NOT exist on
-//	                                            disk.
+// # Input Parameters
 //
-//	PathExistsStatusCode(0).Exists()      +1    Path file existence HAD been tested
-//	                                            and path file name DOES exist on
-//	                                            disk.
+//	pathDoesExist				PathExistsStatusCode
+//
+//		This instance of PathExistsStatusCode is an enumeration
+//		which must be set to one of the following values:
+//
+//		                                      Int
+//		   Enumeration                       Value         Definition
+//		----------------------------------------------------------------------------
+//		PathExistsStatusCode(0).Unknown()     -1    Path file existence has NOT been
+//		                                            tested and status is 'Unknown'.
+//
+//		PathExistsStatusCode(0).DoesNotExist() 0    Path file existence HAS been tested
+//		                                            and path file name does NOT exist on
+//		                                            disk.
+//
+//		PathExistsStatusCode(0).Exists()      +1    Path file existence HAD been tested
+//		                                            and path file name DOES exist on
+//		                                            disk.
+//
+//		If 'pathDoesExist' is set to an invalid value, an
+//		error will be returned.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If input parameter 'pathDoesExist' is set to a
+//		valid value, the returned error Type is set equal
+//		to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
 func (vpDto *ValidPathStrDto) SetPathDoesExist(
-	pathDoesExist PathExistsStatusCode) error {
+	pathDoesExist PathExistsStatusCode,
+	errorPrefix interface{}) error {
 
-	err := pathDoesExist.StatusIsValid()
+	if vpDto.lock == nil {
+		vpDto.lock = new(sync.Mutex)
+	}
+
+	vpDto.lock.Lock()
+
+	defer vpDto.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"ValidPathStrDto."+
+			"SetPathDoesExist()",
+		"")
 
 	if err != nil {
-		return fmt.Errorf("ValidPathStrDto.SetPathDoesExist() %v",
+		return err
+	}
+
+	err = pathDoesExist.StatusIsValid()
+
+	if err != nil {
+
+		return fmt.Errorf("%v\n"+
+			"Error Return: pathDoesExist.StatusIsValid()\n"+
+			"Error= \n%v\n",
+			ePrefix.String(),
 			err.Error())
 	}
 
