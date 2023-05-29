77c94f6799d45c8ee095be0f9912e59ee3713b4b
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-29 01:41:16 -0500

    Refactored method ValidPathStrDto.SetPathIsValid().

--- strmech/filevalidpathstrdto.go
+++ strmech/filevalidpathstrdto.go
@@ -762,8 +762,20 @@ func (vpDto *ValidPathStrDto) SetPathDoesExist(
 	return nil
 }
 
-// SetPathIsValid - "setter" method for internal data field vpDto.pathIsValid
-// which is of type PathValidityStatusCode.
+// SetPathIsValid
+//
+// This is a "setter" method for the internal data field
+// vpDto.pathIsValid which is of type PathValidityStatusCode.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	pathIsValid					PathValidityStatusCode
+//
+//	This instance of 'PathValidityStatusCode' is an
+//	enumeration which must be set to one of the following
+//	valid values:
 //
 //	                  Path Validity
 //	 Method            Status Code
@@ -779,13 +791,116 @@ func (vpDto *ValidPathStrDto) SetPathDoesExist(
 //
 //	PathValidityStatusCode(0).Valid()       +1  Tests have verified that the Path/file
 //	                                            name is 'Valid'.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If input parameter 'pathIsValid' is set to a
+//		valid value, the returned error Type is set equal
+//		to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
 func (vpDto *ValidPathStrDto) SetPathIsValid(
-	pathIsValid PathValidityStatusCode) error {
+	pathIsValid PathValidityStatusCode,
+	errorPrefix interface{}) error {
 
-	err := pathIsValid.StatusIsValid()
+	if vpDto.lock == nil {
+		vpDto.lock = new(sync.Mutex)
+	}
+
+	vpDto.lock.Lock()
+
+	defer vpDto.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"ValidPathStrDto.IsDtoValid()",
+		"")
 
 	if err != nil {
-		return fmt.Errorf("ValidPathStrDto.SetPathIsValid() %v",
+		return err
+	}
+
+	err = pathIsValid.StatusIsValid()
+
+	if err != nil {
+
+		return fmt.Errorf("%v\n"+
+			"Error Return: pathIsValid.StatusIsValid()\n"+
+			"Error= \n%v\n",
+			ePrefix.String(),
 			err.Error())
 	}
 
