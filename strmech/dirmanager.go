9c2e54939c1dea468473ce8523680d46811214da
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-31 03:26:18 -0500

    Refactored method DirMgr.NewFromKnownPathDirectoryName().

--- strmech/dirmanager.go
+++ strmech/dirmanager.go
@@ -10656,7 +10656,7 @@ func (dMgr *DirMgr) New(
 //
 //	fInfo						os.FileInfo
 //
-//		An os.FileInfo structure containing the directory
+//		An os.FileInfo object containing the directory
 //		name which will be added to the parent directory,
 //		specified by input parameter 'parentDirectoryPath',
 //		to create the new directory path.
@@ -11159,20 +11159,122 @@ func (dMgr *DirMgr) NewFromFileMgr(
 		ePrefix.XCpy("fMgr.dMgr->"))
 }
 
-// NewFromKnownPathDirectoryName - Configures and returns
-// a new 'DirMgr' instance using a parent path name and
-// directory name. The parent path and directory name are
-// combined to form the full path for the new 'DirMgr'
-// instance.
+// NewFromKnownPathDirectoryName
+//
+// Configures and returns a new 'DirMgr' instance using
+// a parent path name and directory name. The parent path
+// and directory name are combined to form the full path
+// for the new 'DirMgr' instance.
 //
 // This method will populate all internal field values
-// with new values based on input parameters 'parentPathName'
-// and 'directoryName'.
+// with new values based on input parameters
+// 'parentPathName' and 'directoryName'.
 //
-// This method differs from similar methods in that it assumes
-// the input parameters are known values and do not require
-// the usual analysis and validation screening applied by
-// other methods.
+// This method differs from similar methods in that it
+// assumes the input parameters are known values and do
+// not require the usual analysis and validation
+// screening applied by other methods.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	parentPathName				string
+//
+//		This string contains the parent directory which
+//		will be added to the directory name supplied by
+//		input parameter 'directoryName'. These two
+//		elements are combined to create the new directory
+//		path.
+//
+//	directoryName				string
+//
+//		This string contains the directory name which
+//		will be added to the parent directory, specified
+//		by input parameter 'parentPathName', to create
+//		the new directory path.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	DirMgr
+//
+//		If this method completes successfully, this
+//		parameter will return a new, fully populated
+//		instance of Directory Manager (DirMgr).
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
 func (dMgr *DirMgr) NewFromKnownPathDirectoryName(
 	parentPathName string,
 	directoryName string,
