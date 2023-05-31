97cb69a3c86dff55782d52e47e5914a1c3d06634
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-30 22:32:13 -0500

    Refactored method DirMgr.MakeDir().

--- strmech/dirmanager.go
+++ strmech/dirmanager.go
@@ -9387,8 +9387,6 @@ func (dMgr *DirMgr) IsVolumeNamePopulated() bool {
 
 	defer dMgr.lock.Unlock()
 
-	isDMgrVolumePopulated := false
-
 	_,
 		_,
 		_ = new(dirMgrHelperAtom).
@@ -9401,15 +9399,11 @@ func (dMgr *DirMgr) IsVolumeNamePopulated() bool {
 	if len(dMgr.volumeName) == 0 ||
 		!dMgr.isInitialized {
 
-		isDMgrVolumePopulated = false
-
-	} else {
-
-		isDMgrVolumePopulated = true
+		return false
 
 	}
 
-	return isDMgrVolumePopulated
+	return true
 }
 
 // MakeDirWithPermission
@@ -9610,15 +9604,100 @@ func (dMgr *DirMgr) MakeDirWithPermission(
 	return err
 }
 
-// MakeDir - If the directory path identified by the current DirMgr
-// object does not exist, this method will create that directory path.
-// The permission specification used to create the directory is
-// 'drwxrwxrwx' which is equivalent to octal value, '020000000777'
+// MakeDir
 //
-// MakeDir creates a directory named path, along with any necessary
-// parent directories.
+// If the directory path identified by the current DirMgr
+// object does not exist, this method will create that
+// directory path.
+//
+// The permission specification used to create the
+// directory is 'drwxrwxrwx' which is equivalent to octal
+// value, '020000000777'
+//
+// MakeDir creates a directory named path, along with any
+// necessary parent directories.
 //
 // If the directory creation fails, an error is returned.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
 func (dMgr *DirMgr) MakeDir(
 	errorPrefix interface{}) error {
 
