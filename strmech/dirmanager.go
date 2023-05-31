7ffbf7bc4dff75eaf585cb46bf80ebed6394163c
Mike Rapp <jordy.rascal@protonmail.com>
2023-05-30 22:56:18 -0500

    Refactored method DirMgr.NewFromFileInfo().

--- strmech/dirmanager.go
+++ strmech/dirmanager.go
@@ -10632,12 +10632,125 @@ func (dMgr *DirMgr) New(
 	return newDirMgr, err
 }
 
-// NewFromFileInfo - Returns a new DirMgr object based on two input parameters:
-//   - A parent directory path string
-//   - An os.FileInfo object containing the directory name.
+// NewFromFileInfo
+//
+// Returns a new DirMgr object based on two input
+// parameters:
+//
+//	(1)	A parent directory path string
+//
+//	(2)	An os.FileInfo object containing the directory
+//		name.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	parentDirectoryPath			string
+//
+//		This string contains the parent directory which
+//		will be added to the directory name supplied by
+//		input parameter 'fInfo'. These two elements are
+//		combined to create the new directory path.
+//
+//
+//	fInfo						os.FileInfo
+//
+//		An os.FileInfo structure containing the directory
+//		name which will be added to the parent directory,
+//		specified by input parameter 'parentDirectoryPath',
+//		to create the new directory path.
+//
+//		Note:
+//
+//			An instance of FileInfoPlus may be submitted
+//			for this parameter because FileInfoPlus
+//			implements the os.FileInfo interface.
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	DirMgr
+//
+//		If this method completes successfully, this
+//		parameter will return a new, fully populated
+//		instance of Directory Manager (DirMgr).
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errorPrefix'.
+//	 	The 'errorPrefix' text will be prefixed or
+//	 	attached to the	beginning of the error message.
 func (dMgr *DirMgr) NewFromFileInfo(
 	parentDirectoryPath string,
-	info os.FileInfo,
+	fInfo os.FileInfo,
 	errorPrefix interface{}) (
 	DirMgr,
 	error) {
@@ -10665,10 +10778,10 @@ func (dMgr *DirMgr) NewFromFileInfo(
 		return newDirMgr, err
 	}
 
-	if info == nil {
+	if fInfo == nil {
 		return newDirMgr,
 			fmt.Errorf("%v\n"+
-				"ERROR: Input parameter 'info' is 'nil' and INVALID!\n",
+				"ERROR: Input parameter 'fInfo' is 'nil' and INVALID!\n",
 				ePrefix.String())
 	}
 
@@ -10679,7 +10792,7 @@ func (dMgr *DirMgr) NewFromFileInfo(
 		setDirMgrWithPathDirectoryName(
 			&newDirMgr,
 			parentDirectoryPath,
-			info.Name(),
+			fInfo.Name(),
 			"newDirMgr",
 			"parentDirectoryPath",
 			"FileInfo.Name()",
@@ -10698,7 +10811,7 @@ func (dMgr *DirMgr) NewFromFileInfo(
 				"FileInfo.Name()= '%v'\n",
 				ePrefix.String(),
 				parentDirectoryPath,
-				info.Name())
+				fInfo.Name())
 	}
 
 	return newDirMgr, err
