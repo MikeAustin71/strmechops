dac7d07b34bf7b80133a016cf9cab51f007c1afb
Mike Rapp <jordy.rascal@protonmail.com>
2023-06-05 01:36:04 -0500

    Added new method strMechHelperErrors.validateInputPointer()

--- strmech/strmechhelpererrors.go
+++ strmech/strmechhelpererrors.go
@@ -10,6 +10,131 @@ type strMechHelperErrors struct {
 	lock *sync.Mutex
 }
 
+// validateInputPointer
+//
+// This method is designed to screen for a nil pointer
+// passed to a function as an input parameter. If the
+// input parameter is identified as a nil pointer, a
+// suitable error message is generated and returned in
+// an instance of type error.
+//
+// This method will facilitate use of
+// "If with assignment" if statements when testing for
+// input errors.
+//
+//	Example "If with assignment"
+//
+//	if assignment-statement; condition {
+//
+//	}
+//
+//	Example Usage
+//
+//	if err = new(strMechHelperErrors).validateInputPointer(
+//				inputPtr, "dirMgrPointer", ePrefix); err != nil {
+//
+//		return err
+//
+//	}
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	inputPtr					interface{}
+//
+//		A pointer to an object passed as an input
+//		parameter for a function. If this parameter,
+//		is nil, an error will be returned.
+//
+//	inputPtrLabel	string
+//
+//		The name or label associated with input parameter
+//		'inputPtr' which will be used in error messages
+//		returned by this method.
+//
+//		If this parameter is submitted as an empty
+//		string, a default value of "inputPtr" will be
+//		automatically applied.
+//
+//	errPrefDto					*ePref.ErrPrefixDto
+//
+//		This object encapsulates an error prefix string
+//		which is included in all returned error
+//		messages. Usually, it contains the name of the
+//		calling method or methods listed as a function
+//		chain.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		Type ErrPrefixDto is included in the 'errpref'
+//		software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If input parameter 'inputPtr' non-nil, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If input parameter 'inputPtr' is nil, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errPrefDto'.
+//	 	The 'errPrefDto' text will be prefixed or
+//	 	attached to the	beginning of the error message.
+func (strMechHlprErrors *strMechHelperErrors) validateInputPointer(
+	inputPtr interface{},
+	inputPtrLabel string,
+	errPrefDto *ePref.ErrPrefixDto) (
+	err error) {
+
+	if strMechHlprErrors.lock == nil {
+		strMechHlprErrors.lock = new(sync.Mutex)
+	}
+
+	strMechHlprErrors.lock.Lock()
+
+	defer strMechHlprErrors.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
+		errPrefDto,
+		"strMechHelperErrors."+
+			"validateInputPointer()",
+		"")
+
+	if err != nil {
+
+		return err
+	}
+
+	if len(inputPtrLabel) == 0 {
+
+		inputPtrLabel = "inputPtr"
+	}
+
+	if inputPtr == nil {
+
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter '%v' is invalid!\n"+
+			"Input parameter '%v' a 'nil' pointer.\n",
+			ePrefix.String(),
+			inputPtrLabel,
+			inputPtrLabel)
+
+	}
+
+	return err
+}
+
 // validateInputString
 //
 // This method is designed to validate input strings
@@ -89,13 +214,11 @@ func (strMechHlprErrors *strMechHelperErrors) validateInputString(
 
 	var ePrefix *ePref.ErrPrefixDto
 
-	funcName := "strMechHelperErrors." +
-		"validateInputString()"
-
 	ePrefix,
 		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
 		errPrefDto,
-		funcName,
+		"strMechHelperErrors."+
+			"validateInputString()",
 		"")
 
 	if err != nil {
