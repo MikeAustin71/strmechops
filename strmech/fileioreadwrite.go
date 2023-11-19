4580ebbd43a5a051386ed209f229a08d5bdc09fa
Mike Rapp <jordy.rascal@protonmail.com>
2023-11-19 01:16:48 -0600

    Added new method fileIoReadWriteElectron.readerCloseRelease().

--- strmech/fileioreadwrite.go
+++ strmech/fileioreadwrite.go
@@ -261,7 +261,211 @@ type fileIoReadWriteElectron struct {
 	lock *sync.Mutex
 }
 
-// writerFlushCloseRelease
+// readerCloseRelease
+//
+// This method will perform Clean-Up operations on the
+// internal io.Reader object encapsulated in the
+// FileIoReadWrite instance passed as input parameter
+// 'fIoReadWrite':
+//
+//	fIoReadWrite.reader
+//
+// Upon completion, this method will effectively render
+// the 'fIoReadWrite' instance invalid and unusable for
+// any future 'read' operations.
+//
+// This Clean-Up operation is accomplished by and closing
+// the internal io.Reader object before finally releasing
+// the memory resources contained in that object:
+//
+//	fIoReadWrite.reader
+//
+// The 'release memory resources' actions are implemented
+// independently based on the values passed for input
+// parameters 'releaseReaderMemResources', and
+// 'releaseFBuffReaderLocalMemRes'.
+//
+// 'releaseReaderMemResources' controls the release of
+// memory resources associated with the internal io.Reader
+// object.
+//
+// 'releaseFBuffReaderLocalMemRes' controls the release of
+// FileIoReadWrite memory resources associated with the
+// io.Reader object.
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	fIoReadWrite						*FileIoReadWrite
+//
+//		A pointer to an instance of FileBufferReader.
+//
+//		This method will perform Clean-Up operations on
+//		the internal io.Reader object encapsulated in
+//		this FileIoReadWrite instance:
+//
+//			fIoReadWrite.reader
+//
+//		Upon completion, this method will effectively
+//		render the FileIoReadWrite instance,
+//		'fIoReadWrite', invalid and unusable for any
+//		future 'read' operations.
+//
+//	fIoReadWriteLabel					string
+//
+//		The name or label associated with input parameter
+//		'fIoReadWrite' which will be used in error
+//		messages returned by this method.
+//
+//		If this parameter is submitted as an empty
+//		string, a default value of "fIoReadWrite" will
+//		be automatically applied.
+//
+//	releaseReaderMemResources			bool
+//
+//		If 'releaseReaderMemResources' is set to 'true',
+//		this method	will release the memory resources
+//		contained in the internal io.reader object
+//		encapsulated by	'fIoReadWrite':
+//
+//			fIoReadWrite.reader
+//
+//		Releasing internal memory resources synchronizes
+//		internal flags and prevents multiple calls to the
+//		'close' method. Calling the 'close' method more
+//		than once may produce unexpected results.
+//
+//	releaseFIoReaderLocalMemRes		bool
+//
+//		If 'releaseFIoReaderLocalMemRes' is set to
+//		'true', this method will release the local memory
+//		resources for the FileIoReadWrite reader object
+//		(fIoReadWrite):
+//
+//			fIoReadWrite.reader = nil
+//			fIoReadWrite.readerFilePathName = ""
+//
+//		Releasing internal memory resources synchronizes
+//		internal flags and prevents multiple calls to the
+//		'close' method. Calling the 'close' method more
+//		than once may produce unexpected results.
+//
+//	errPrefDto							*ePref.ErrPrefixDto
+//
+//		This object encapsulates an error prefix string
+//		which is included in all returned error
+//		messages. Usually, it contains the name of the
+//		calling method or methods listed as a function
+//		chain.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		Type ErrPrefixDto is included in the 'errpref'
+//		software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If this method completes successfully, the
+//		returned error Type is set equal to 'nil'.
+//
+//		If errors are encountered during processing, the
+//		returned error Type will encapsulate an
+//		appropriate error message. This returned error
+//	 	message will incorporate the method chain and
+//	 	text passed by input parameter, 'errPrefDto'.
+//	 	The 'errPrefDto' text will be prefixed or
+//	 	attached to the	beginning of the error message.
+func (fIoReadWriteElectron *fileIoReadWriteElectron) readerCloseRelease(
+	fIoReadWrite *FileIoReadWrite,
+	fBufReadWriteLabel string,
+	releaseReaderMemResources bool,
+	releaseFBuffWriterLocalMemRes bool,
+	errPrefDto *ePref.ErrPrefixDto) error {
+
+	if fIoReadWriteElectron.lock == nil {
+		fIoReadWriteElectron.lock = new(sync.Mutex)
+	}
+
+	fIoReadWriteElectron.lock.Lock()
+
+	defer fIoReadWriteElectron.lock.Unlock()
+
+	var err error
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	funcName := "fileIoReadWriteElectron." +
+		"readerCloseRelease()"
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
+		errPrefDto,
+		funcName,
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	if len(fBufReadWriteLabel) == 0 {
+
+		fBufReadWriteLabel = "fIoReadWrite"
+	}
+
+	if fIoReadWrite == nil {
+
+		err = fmt.Errorf("%v\n"+
+			"Error: Input parameter '%v' is a nil pointer!\n"+
+			"%v is invalid.\n",
+			ePrefix.String(),
+			fBufReadWriteLabel,
+			fBufReadWriteLabel)
+
+		return err
+	}
+
+	var err2 error
+
+	if fIoReadWrite.reader != nil {
+
+		err2 = new(fileIoReaderMolecule).
+			closeAndRelease(
+				fIoReadWrite.reader,
+				fBufReadWriteLabel+".reader",
+				releaseReaderMemResources,
+				ePrefix)
+
+		if err2 != nil {
+
+			err = fmt.Errorf("%v\n"+
+				"Error occurred while closing the %v.\n"+
+				"Error:\n%v\n",
+				funcName,
+				fBufReadWriteLabel+".reader",
+				err2.Error())
+
+			return err
+		}
+	}
+
+	if releaseFBuffWriterLocalMemRes == true {
+
+		fIoReadWrite.reader = nil
+		fIoReadWrite.readerFilePathName = ""
+
+	}
+
+	return err
+}
+
+// writerCloseRelease
 //
 // This method will perform Clean-Up operations on the
 // internal io.Writer object encapsulated in the
@@ -270,22 +474,28 @@ type fileIoReadWriteElectron struct {
 //
 //	fIoReadWrite.writer
 //
-// Upon completion, this method will effectively render the
-// 'fIoReadWrite' instance invalid and unusable for any
-// future 'write' operations.
+// Upon completion, this method will effectively render
+// the 'fIoReadWrite' instance invalid and unusable for
+// any future 'write' operations.
 //
-// This operation is accomplished by flushing and closing
-// the internal bufio.Writer object before finally
-// releasing the memory resources associated with that
-// object:
+// This Clean-Up operation is accomplished by and closing
+// the internal io.Writer object before finally releasing
+// the memory resources contained in that object:
 //
 //	fIoReadWrite.writer
 //
-// The 'flush' and 'release memory resources' actions are
-// implemented independently based on the values passed
-// for input parameters 'flushWriteBuffer',
-// 'releaseWriterMemResources', and
+// The 'release memory resources' actions are implemented
+// independently based on the values passed for input
+// parameters 'releaseWriterMemResources', and
 // 'releaseFBuffWriterLocalMemRes'.
+//
+// 'releaseWriterMemResources' controls the release of
+// memory resources associated with the internal io.Writer
+// object.
+//
+// 'releaseFBuffWriterLocalMemRes' controls the release of
+// FileIoReadWrite memory resources associated with the
+// io.Writer object.
 //
 // ----------------------------------------------------------------
 //
@@ -296,7 +506,7 @@ type fileIoReadWriteElectron struct {
 //		A pointer to an instance of FileBufferWriter.
 //
 //		This method will perform Clean-Up operations on
-//		the internal bufio.Writer object encapsulated in
+//		the internal io.Writer object encapsulated in
 //		this FileIoReadWrite instance:
 //
 //			fIoReadWrite.writer
@@ -319,8 +529,8 @@ type fileIoReadWriteElectron struct {
 //	releaseWriterMemResources			bool
 //
 //		If 'releaseWriterMemResources' is set to 'true',
-//		this method	will release the internal memory
-//		resources for the internal io.writer object
+//		this method	will release the memory resources
+//		contained in the internal io.writer object
 //		encapsulated by	'fIoReadWrite':
 //
 //			fIoReadWrite.writer
@@ -396,7 +606,7 @@ func (fIoReadWriteElectron *fileIoReadWriteElectron) writerCloseRelease(
 	var ePrefix *ePref.ErrPrefixDto
 
 	funcName := "fileIoReadWriteElectron." +
-		"writerFlushCloseRelease()"
+		"writerCloseRelease()"
 
 	ePrefix,
 		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
