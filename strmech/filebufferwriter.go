6401101cb006895d39f5db7aa1d5ec16b2a96adc
Mike Rapp <jordy.rascal@protonmail.com>
2023-12-02 21:43:36 -0600

    Added new method FileBufferWriter.IsValidInstanceError()

--- strmech/filebufferwriter.go
+++ strmech/filebufferwriter.go
@@ -855,6 +855,137 @@ func (fBufWriter *FileBufferWriter) IsClosed() bool {
 	return true
 }
 
+// IsValidInstanceError
+//
+// Analyzes the current FileBufferWriter instance to
+// determine if is invalid.
+//
+// If the current FileBufferWriter instance is found
+// to be invalid, an error is returned.
+//
+// If the current FileBufferWriter instance is valid,
+// this method returns 'nil'
+//
+// ----------------------------------------------------------------
+//
+// # Input Parameters
+//
+//	errorPrefix					interface{}
+//
+//		This object encapsulates error prefix text which
+//		is included in all returned error messages.
+//		Usually, it contains the name of the calling
+//		method or methods listed as a method or function
+//		chain of execution.
+//
+//		If no error prefix information is needed, set
+//		this parameter to 'nil'.
+//
+//		This empty interface must be convertible to one
+//		of the following types:
+//
+//		1.	nil
+//				A nil value is valid and generates an
+//				empty collection of error prefix and
+//				error context information.
+//
+//		2.	string
+//				A string containing error prefix
+//				information.
+//
+//		3.	[]string
+//				A one-dimensional slice of strings
+//				containing error prefix information.
+//
+//		4.	[][2]string
+//				A two-dimensional slice of strings
+//		   		containing error prefix and error
+//		   		context information.
+//
+//		5.	ErrPrefixDto
+//				An instance of ErrPrefixDto.
+//				Information from this object will
+//				be copied for use in error and
+//				informational messages.
+//
+//		6.	*ErrPrefixDto
+//				A pointer to an instance of
+//				ErrPrefixDto. Information from
+//				this object will be copied for use
+//				in error and informational messages.
+//
+//		7.	IBasicErrorPrefix
+//				An interface to a method
+//				generating a two-dimensional slice
+//				of strings containing error prefix
+//				and error context information.
+//
+//		If parameter 'errorPrefix' is NOT convertible
+//		to one of the valid types listed above, it will
+//		be considered invalid and trigger the return of
+//		an error.
+//
+//		Types ErrPrefixDto and IBasicErrorPrefix are
+//		included in the 'errpref' software package:
+//			"github.com/MikeAustin71/errpref".
+//
+// ----------------------------------------------------------------
+//
+// # Return Values
+//
+//	error
+//
+//		If any of the internal member data variables
+//		contained in the current instance of
+//		FileBufferWriter are found to be invalid,
+//		this method will return an error configured
+//		with an appropriate message identifying the
+//		invalid	member data variable.
+//
+//		If all internal member data variables evaluate
+//		as valid, this returned error value will be set
+//		to 'nil'.
+//
+//		If errors are encountered during processing or if
+//		any FileBufferWriter internal member data
+//	 	values are found to be invalid, the returned error
+//	 	will encapsulate an appropriate error message.
+//	 	This returned error message will incorporate the
+//	 	method chain and text passed by input parameter,
+//	 	'errorPrefix'. The 'errorPrefix' text will be
+//	 	prefixed to the beginning of the error message.
+func (fBufWriter *FileBufferWriter) IsValidInstanceError(
+	errorPrefix interface{}) error {
+
+	if fBufWriter.lock == nil {
+		fBufWriter.lock = new(sync.Mutex)
+	}
+
+	fBufWriter.lock.Lock()
+
+	defer fBufWriter.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewIEmpty(
+		errorPrefix,
+		"FileBufferWriter."+
+			"IsValidInstanceError()",
+		"")
+
+	if err != nil {
+		return err
+	}
+
+	return new(fileBufferWriterElectron).
+		isFileBufferWriterValid(
+			fBufWriter,
+			"fBufWriter",
+			ePrefix)
+}
+
 // New
 //
 // This method returns a pointer to a new, empty instance
